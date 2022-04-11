29d37860611461afd2d77e5bc0bf2104b1a80e64
SDKAuto <sdkautomation@microsoft.com>
2022-04-11 03:05:22 +0000

    CodeGen from PR 18568 in Azure/azure-rest-api-specs Merge 18668fbb7f64e724e550db1dea2a43cca027a11c into 69f5ef7b44bd354a790d9594c96fa379de653824

--- sdk/containerservice/arm-containerservice/src/containerServiceClient.ts
+++ sdk/containerservice/arm-containerservice/src/containerServiceClient.ts
@@ -62,7 +62,7 @@ export class ContainerServiceClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-containerservice/15.2.1`;
+    const packageDetails = `azsdk-js-arm-containerservice/16.0.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -77,8 +77,7 @@ export class ContainerServiceClient extends coreClient.ServiceClient {
       userAgentOptions: {
         userAgentPrefix
       },
-      baseUri:
-        options.endpoint ?? options.baseUri ?? "https://management.azure.com"
+      baseUri: options.endpoint || "https://management.azure.com"
     };
     super(optionsWithDefaults);
     // Parameter assignments
@@ -86,7 +85,7 @@ export class ContainerServiceClient extends coreClient.ServiceClient {
 
     // Assigning values to Constant parameters
     this.$host = options.$host || "https://management.azure.com";
-    this.apiVersion = options.apiVersion || "2022-02-01";
+    this.apiVersion = options.apiVersion || "2022-03-01";
     this.operations = new OperationsImpl(this);
     this.managedClusters = new ManagedClustersImpl(this);
     this.maintenanceConfigurations = new MaintenanceConfigurationsImpl(this);
