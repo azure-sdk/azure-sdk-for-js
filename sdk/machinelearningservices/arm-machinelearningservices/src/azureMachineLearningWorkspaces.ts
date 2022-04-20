98d056d647c9238cc5aa838a59680053518595bd
SDKAuto <sdkautomation@microsoft.com>
2022-04-20 20:16:16 +0000

    CodeGen from PR 18690 in Azure/azure-rest-api-specs Merge a4a0acf68cbce3821897b7cecff8e46a28086710 into b5d8eef5bf187dc99f1c18e8323b19e0c37a7124

--- sdk/machinelearningservices/arm-machinelearningservices/src/azureMachineLearningWorkspaces.ts
+++ sdk/machinelearningservices/arm-machinelearningservices/src/azureMachineLearningWorkspaces.ts
@@ -68,7 +68,7 @@ export class AzureMachineLearningWorkspaces extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-machinelearningservices/5.0.0`;
+    const packageDetails = `azsdk-js-arm-machinelearningservices/5.0.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
