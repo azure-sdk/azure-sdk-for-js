984a19eed31237bbdc0812a6f0390a2f3965ad4a
SDKAuto <sdkautomation@microsoft.com>
2022-05-10 03:19:51 +0000

    CodeGen from PR 18885 in Azure/azure-rest-api-specs Merge 64818d96c4dcac2c51656cd2c1e27c75e577478a into fd4dd3abc8bf0cdaebdf74215d0dbe3ec705fe9c

--- sdk/monitor/arm-monitor/src/operations/privateLinkScopes.ts
+++ sdk/monitor/arm-monitor/src/operations/privateLinkScopes.ts
@@ -222,12 +222,10 @@ export class PrivateLinkScopesImpl implements PrivateLinkScopes {
       { resourceGroupName, scopeName, options },
       deleteOperationSpec
     );
-    const poller = new LroEngine(lro, {
+    return new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
       intervalInMs: options?.updateIntervalInMs
     });
-    await poller.poll();
-    return poller;
   }
 
   /**
