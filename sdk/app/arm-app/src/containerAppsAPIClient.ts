c79de1260f51cd97f1ada2439e1a6a31b3c7b1fd
SDKAuto <sdkautomation@microsoft.com>
2022-06-03 00:35:10 +0000

    CodeGen from PR 19054 in Azure/azure-rest-api-specs Merge b2e12070842f8d6e8e145fd37bd33c87c9e26a18 into 0fd5e268b79330edc4f9d3193f2336fc6984ff1b

--- sdk/app/arm-app/src/containerAppsAPIClient.ts
+++ sdk/app/arm-app/src/containerAppsAPIClient.ts
@@ -7,30 +7,44 @@
  */
 
 import * as coreClient from "@azure/core-client";
+import * as coreRestPipeline from "@azure/core-rest-pipeline";
+import {
+  PipelineRequest,
+  PipelineResponse,
+  SendRequest
+} from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
 import {
+  ContainerAppsAuthConfigsImpl,
   ContainerAppsImpl,
   ContainerAppsRevisionsImpl,
   ContainerAppsRevisionReplicasImpl,
+  DaprComponentsImpl,
+  ContainerAppsDiagnosticsImpl,
+  ManagedEnvironmentDiagnosticsImpl,
+  ManagedEnvironmentsDiagnosticsImpl,
+  OperationsImpl,
   ManagedEnvironmentsImpl,
   CertificatesImpl,
-  OperationsImpl,
-  ContainerAppsSourceControlsImpl,
-  DaprComponentsImpl,
-  ContainerAppsAuthConfigsImpl,
-  ManagedEnvironmentsStoragesImpl
+  NamespacesImpl,
+  ManagedEnvironmentsStoragesImpl,
+  ContainerAppsSourceControlsImpl
 } from "./operations";
 import {
+  ContainerAppsAuthConfigs,
   ContainerApps,
   ContainerAppsRevisions,
   ContainerAppsRevisionReplicas,
+  DaprComponents,
+  ContainerAppsDiagnostics,
+  ManagedEnvironmentDiagnostics,
+  ManagedEnvironmentsDiagnostics,
+  Operations,
   ManagedEnvironments,
   Certificates,
-  Operations,
-  ContainerAppsSourceControls,
-  DaprComponents,
-  ContainerAppsAuthConfigs,
-  ManagedEnvironmentsStorages
+  Namespaces,
+  ManagedEnvironmentsStorages,
+  ContainerAppsSourceControls
 } from "./operationsInterfaces";
 import { ContainerAppsAPIClientOptionalParams } from "./models";
 
@@ -66,7 +80,7 @@ export class ContainerAppsAPIClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-app/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-app/1.0.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -85,38 +99,102 @@ export class ContainerAppsAPIClient extends coreClient.ServiceClient {
         options.endpoint ?? options.baseUri ?? "https://management.azure.com"
     };
     super(optionsWithDefaults);
+
+    if (options?.pipeline && options.pipeline.getOrderedPolicies().length > 0) {
+      const pipelinePolicies: coreRestPipeline.PipelinePolicy[] = options.pipeline.getOrderedPolicies();
+      const bearerTokenAuthenticationPolicyFound = pipelinePolicies.some(
+        (pipelinePolicy) =>
+          pipelinePolicy.name ===
+          coreRestPipeline.bearerTokenAuthenticationPolicyName
+      );
+      if (!bearerTokenAuthenticationPolicyFound) {
+        this.pipeline.removePolicy({
+          name: coreRestPipeline.bearerTokenAuthenticationPolicyName
+        });
+        this.pipeline.addPolicy(
+          coreRestPipeline.bearerTokenAuthenticationPolicy({
+            scopes: `${optionsWithDefaults.baseUri}/.default`,
+            challengeCallbacks: {
+              authorizeRequestOnChallenge:
+                coreClient.authorizeRequestOnClaimChallenge
+            }
+          })
+        );
+      }
+    }
     // Parameter assignments
     this.subscriptionId = subscriptionId;
 
     // Assigning values to Constant parameters
     this.$host = options.$host || "https://management.azure.com";
-    this.apiVersion = options.apiVersion || "2022-01-01-preview";
+    this.apiVersion = options.apiVersion || "2022-05-01";
+    this.containerAppsAuthConfigs = new ContainerAppsAuthConfigsImpl(this);
     this.containerApps = new ContainerAppsImpl(this);
     this.containerAppsRevisions = new ContainerAppsRevisionsImpl(this);
     this.containerAppsRevisionReplicas = new ContainerAppsRevisionReplicasImpl(
       this
     );
-    this.managedEnvironments = new ManagedEnvironmentsImpl(this);
-    this.certificates = new CertificatesImpl(this);
-    this.operations = new OperationsImpl(this);
-    this.containerAppsSourceControls = new ContainerAppsSourceControlsImpl(
+    this.daprComponents = new DaprComponentsImpl(this);
+    this.containerAppsDiagnostics = new ContainerAppsDiagnosticsImpl(this);
+    this.managedEnvironmentDiagnostics = new ManagedEnvironmentDiagnosticsImpl(
       this
     );
-    this.daprComponents = new DaprComponentsImpl(this);
-    this.containerAppsAuthConfigs = new ContainerAppsAuthConfigsImpl(this);
+    this.managedEnvironmentsDiagnostics = new ManagedEnvironmentsDiagnosticsImpl(
+      this
+    );
+    this.operations = new OperationsImpl(this);
+    this.managedEnvironments = new ManagedEnvironmentsImpl(this);
+    this.certificates = new CertificatesImpl(this);
+    this.namespaces = new NamespacesImpl(this);
     this.managedEnvironmentsStorages = new ManagedEnvironmentsStoragesImpl(
       this
     );
+    this.containerAppsSourceControls = new ContainerAppsSourceControlsImpl(
+      this
+    );
+    this.addCustomApiVersionPolicy(options.apiVersion);
   }
 
+  /** A function that adds a policy that sets the api-version (or equivalent) to reflect the library version. */
+  private addCustomApiVersionPolicy(apiVersion?: string) {
+    if (!apiVersion) {
+      return;
+    }
+    const apiVersionPolicy = {
+      name: "CustomApiVersionPolicy",
+      async sendRequest(
+        request: PipelineRequest,
+        next: SendRequest
+      ): Promise<PipelineResponse> {
+        const param = request.url.split("?");
+        if (param.length > 1) {
+          const newParams = param[1].split("&").map((item) => {
+            if (item.indexOf("api-version") > -1) {
+              return item.replace(/(?<==).*$/, apiVersion);
+            } else {
+              return item;
+            }
+          });
+          request.url = param[0] + "?" + newParams.join("&");
+        }
+        return next(request);
+      }
+    };
+    this.pipeline.addPolicy(apiVersionPolicy);
+  }
+
+  containerAppsAuthConfigs: ContainerAppsAuthConfigs;
   containerApps: ContainerApps;
   containerAppsRevisions: ContainerAppsRevisions;
   containerAppsRevisionReplicas: ContainerAppsRevisionReplicas;
+  daprComponents: DaprComponents;
+  containerAppsDiagnostics: ContainerAppsDiagnostics;
+  managedEnvironmentDiagnostics: ManagedEnvironmentDiagnostics;
+  managedEnvironmentsDiagnostics: ManagedEnvironmentsDiagnostics;
+  operations: Operations;
   managedEnvironments: ManagedEnvironments;
   certificates: Certificates;
-  operations: Operations;
-  containerAppsSourceControls: ContainerAppsSourceControls;
-  daprComponents: DaprComponents;
-  containerAppsAuthConfigs: ContainerAppsAuthConfigs;
+  namespaces: Namespaces;
   managedEnvironmentsStorages: ManagedEnvironmentsStorages;
+  containerAppsSourceControls: ContainerAppsSourceControls;
 }
