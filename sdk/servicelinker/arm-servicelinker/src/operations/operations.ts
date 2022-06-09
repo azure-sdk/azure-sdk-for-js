64edb07c28ed2b483990a16a9ae21e37088ff377
SDKAuto <sdkautomation@microsoft.com>
2022-06-09 04:49:28 +0000

    CodeGen from PR 393 in test-repo-billy/azure-rest-api-specs-pr Merge f9647bb2b364dc8ee153c5efbc052b211ce481ed into b1851ec5e906a864358b2e0eb5610f17e885ecb7

--- sdk/servicelinker/arm-servicelinker/src/operations/operations.ts
+++ sdk/servicelinker/arm-servicelinker/src/operations/operations.ts
@@ -11,7 +11,7 @@ import { Operations } from "../operationsInterfaces";
 import * as coreClient from "@azure/core-client";
 import * as Mappers from "../models/mappers";
 import * as Parameters from "../models/parameters";
-import { ServiceLinkerManagementClient } from "../serviceLinkerManagementClient";
+import { MicrosoftServiceLinker } from "../microsoftServiceLinker";
 import {
   Operation,
   OperationsListNextOptionalParams,
@@ -23,13 +23,13 @@ import {
 /// <reference lib="esnext.asynciterable" />
 /** Class containing Operations operations. */
 export class OperationsImpl implements Operations {
-  private readonly client: ServiceLinkerManagementClient;
+  private readonly client: MicrosoftServiceLinker;
 
   /**
    * Initialize a new instance of the class Operations class.
    * @param client Reference to the service client
    */
-  constructor(client: ServiceLinkerManagementClient) {
+  constructor(client: MicrosoftServiceLinker) {
     this.client = client;
   }
 
