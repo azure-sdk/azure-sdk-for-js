d5c7c1108ad25ea7e887db1afb1223714b9b6950
SDKAuto <sdkautomation@microsoft.com>
2022-06-16 23:37:04 +0000

    CodeGen from PR 19448 in Azure/azure-rest-api-specs Merge a781dc15df6974ff3c91e15ca966560b712143dd into c1eac5f1be8ae4dd6a33604f783a834088effd44

--- sdk/dashboard/arm-dashboard/CHANGELOG.md
+++ sdk/dashboard/arm-dashboard/CHANGELOG.md
@@ -1,5 +1,63 @@
 # Release History
     
+## 1.0.0-beta.2 (2022-06-16)
+    
+**Features**
+
+  - Added operation group PrivateEndpointConnections
+  - Added operation group PrivateLinkResources
+  - Added Interface ManagedGrafanaPropertiesUpdateParameters
+  - Added Interface ManagedServiceIdentity
+  - Added Interface Operation
+  - Added Interface PrivateEndpoint
+  - Added Interface PrivateEndpointConnectionListResult
+  - Added Interface PrivateEndpointConnectionsApproveOptionalParams
+  - Added Interface PrivateEndpointConnectionsDeleteOptionalParams
+  - Added Interface PrivateEndpointConnectionsGetOptionalParams
+  - Added Interface PrivateEndpointConnectionsListNextOptionalParams
+  - Added Interface PrivateEndpointConnectionsListOptionalParams
+  - Added Interface PrivateLinkResourceListResult
+  - Added Interface PrivateLinkResourcesGetOptionalParams
+  - Added Interface PrivateLinkResourcesListNextOptionalParams
+  - Added Interface PrivateLinkResourcesListOptionalParams
+  - Added Interface PrivateLinkServiceConnectionState
+  - Added Interface Resource
+  - Added Type Alias ApiKey
+  - Added Type Alias DeterministicOutboundIP
+  - Added Type Alias ManagedServiceIdentityType
+  - Added Type Alias PrivateEndpointConnection
+  - Added Type Alias PrivateEndpointConnectionProvisioningState
+  - Added Type Alias PrivateEndpointConnectionsApproveResponse
+  - Added Type Alias PrivateEndpointConnectionsGetResponse
+  - Added Type Alias PrivateEndpointConnectionsListNextResponse
+  - Added Type Alias PrivateEndpointConnectionsListResponse
+  - Added Type Alias PrivateEndpointServiceConnectionStatus
+  - Added Type Alias PrivateLinkResource
+  - Added Type Alias PrivateLinkResourcesGetResponse
+  - Added Type Alias PrivateLinkResourcesListNextResponse
+  - Added Type Alias PrivateLinkResourcesListResponse
+  - Added Type Alias PublicNetworkAccess
+  - Interface ManagedGrafanaProperties has a new optional parameter apiKey
+  - Interface ManagedGrafanaProperties has a new optional parameter deterministicOutboundIP
+  - Interface ManagedGrafanaProperties has a new optional parameter outboundIPs
+  - Interface ManagedGrafanaProperties has a new optional parameter privateEndpointConnections
+  - Interface ManagedGrafanaProperties has a new optional parameter publicNetworkAccess
+  - Interface ManagedGrafanaUpdateParameters has a new optional parameter properties
+  - Class DashboardManagementClient has a new parameter privateEndpointConnections
+  - Class DashboardManagementClient has a new parameter privateLinkResources
+  - Added Enum KnownApiKey
+  - Added Enum KnownDeterministicOutboundIP
+  - Added Enum KnownManagedServiceIdentityType
+  - Added Enum KnownPrivateEndpointConnectionProvisioningState
+  - Added Enum KnownPrivateEndpointServiceConnectionStatus
+  - Added Enum KnownPublicNetworkAccess
+
+**Breaking Changes**
+
+  - Removed Enum KnownIdentityType
+  - Removed Enum KnownLastModifiedByType
+    
+    
 ## 1.0.0-beta.1 (2022-04-13)
 
 The package of @azure/arm-dashboard is using our next generation design principles. To learn more, please refer to our documentation [Quick Start](https://aka.ms/js-track2-quickstart).
--- sdk/dashboard/arm-dashboard/src/dashboardManagementClient.ts
+++ sdk/dashboard/arm-dashboard/src/dashboardManagementClient.ts
@@ -8,9 +8,24 @@
 
 import * as coreClient from "@azure/core-client";
 import * as coreRestPipeline from "@azure/core-rest-pipeline";
+import {
+  PipelineRequest,
+  PipelineResponse,
+  SendRequest
+} from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
-import { OperationsImpl, GrafanaImpl } from "./operations";
-import { Operations, Grafana } from "./operationsInterfaces";
+import {
+  OperationsImpl,
+  GrafanaImpl,
+  PrivateEndpointConnectionsImpl,
+  PrivateLinkResourcesImpl
+} from "./operations";
+import {
+  Operations,
+  Grafana,
+  PrivateEndpointConnections,
+  PrivateLinkResources
+} from "./operationsInterfaces";
 import { DashboardManagementClientOptionalParams } from "./models";
 
 export class DashboardManagementClient extends coreClient.ServiceClient {
@@ -21,8 +36,7 @@ export class DashboardManagementClient extends coreClient.ServiceClient {
   /**
    * Initializes a new instance of the DashboardManagementClient class.
    * @param credentials Subscription credentials which uniquely identify client subscription.
-   * @param subscriptionId Gets subscription credentials which uniquely identify Microsoft Azure
-   *                       subscription. The subscription ID forms part of the URI for every service call.
+   * @param subscriptionId The ID of the target subscription.
    * @param options The parameter options
    */
   constructor(
@@ -46,7 +60,7 @@ export class DashboardManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-dashboard/1.0.0-beta.1`;
+    const packageDetails = `azsdk-js-arm-dashboard/1.0.0-beta.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -93,11 +107,44 @@ export class DashboardManagementClient extends coreClient.ServiceClient {
 
     // Assigning values to Constant parameters
     this.$host = options.$host || "https://management.azure.com";
-    this.apiVersion = options.apiVersion || "2021-09-01-preview";
+    this.apiVersion = options.apiVersion || "2022-05-01-preview";
     this.operations = new OperationsImpl(this);
     this.grafana = new GrafanaImpl(this);
+    this.privateEndpointConnections = new PrivateEndpointConnectionsImpl(this);
+    this.privateLinkResources = new PrivateLinkResourcesImpl(this);
+    this.addCustomApiVersionPolicy(options.apiVersion);
+  }
+
+  /** A function that adds a policy that sets the api-version (or equivalent) to reflect the library version. */
+  private addCustomApiVersionPolicy(apiVersion?: string) {
+    if (!apiVersion) {
+      return;
+    }
+    const apiVersionPolicy = {
+      name: "CustomApiVersionPolicy",
+      async sendRequest(
+        request: PipelineRequest,
+        next: SendRequest
+      ): Promise<PipelineResponse> {
+        const param = request.url.split("?");
+        if (param.length > 1) {
+          const newParams = param[1].split("&").map((item) => {
+            if (item.indexOf("api-version") > -1) {
+              return item.replace(/(?<==).*$/, apiVersion);
+            } else {
+              return item;
+            }
+          });
+          request.url = param[0] + "?" + newParams.join("&");
+        }
+        return next(request);
+      }
+    };
+    this.pipeline.addPolicy(apiVersionPolicy);
   }
 
   operations: Operations;
   grafana: Grafana;
+  privateEndpointConnections: PrivateEndpointConnections;
+  privateLinkResources: PrivateLinkResources;
 }
