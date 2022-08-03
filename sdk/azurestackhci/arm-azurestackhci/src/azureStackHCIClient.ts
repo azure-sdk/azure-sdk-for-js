2ad27c89a5b23f060a30d24129796ea22f172e08
SDKAuto <sdkautomation@microsoft.com>
2022-08-03 15:10:11 +0000

    CodeGen from PR 20092 in Azure/azure-rest-api-specs Merge 2c9a6fcae63619f945d088aae271058858742ab7 into d7b7399fb1e1a328b49cd6a998714c6efb877bf2

--- sdk/azurestackhci/arm-azurestackhci/src/azureStackHCIClient.ts
+++ sdk/azurestackhci/arm-azurestackhci/src/azureStackHCIClient.ts
@@ -8,6 +8,11 @@
 
 import * as coreClient from "@azure/core-client";
 import * as coreRestPipeline from "@azure/core-rest-pipeline";
+import {
+  PipelineRequest,
+  PipelineResponse,
+  SendRequest
+} from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
 import {
   ArcSettingsImpl,
@@ -55,7 +60,7 @@ export class AzureStackHCIClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-azurestackhci/3.0.1`;
+    const packageDetails = `azsdk-js-arm-azurestackhci/3.1.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -107,6 +112,35 @@ export class AzureStackHCIClient extends coreClient.ServiceClient {
     this.clusters = new ClustersImpl(this);
     this.extensions = new ExtensionsImpl(this);
     this.operations = new OperationsImpl(this);
+    this.addCustomApiVersionPolicy(options.apiVersion);
+  }
+
+  /** A function that adds a policy that sets the api-version (or equivalent) to reflect the library version. */
+  private addCustomApiVersionPolicy(apiVersion?: string) {
+    if (!apiVersion) {
+      return;
+    }
+    const apiVersionPolicy = {
+      name: "CustomApiVersionPolicy",
+      async sendRequest(
+        request: PipelineRequest,
+        next: SendRequest
+      ): Promise<PipelineResponse> {
+        const param = request.url.split("?");
+        if (param.length > 1) {
+          const newParams = param[1].split("&").map((item) => {
+            if (item.indexOf("api-version") > -1) {
+              return "api-version=" + apiVersion;
+            } else {
+              return item;
+            }
+          });
+          request.url = param[0] + "?" + newParams.join("&");
+        }
+        return next(request);
+      }
+    };
+    this.pipeline.addPolicy(apiVersionPolicy);
   }
 
   arcSettings: ArcSettings;
