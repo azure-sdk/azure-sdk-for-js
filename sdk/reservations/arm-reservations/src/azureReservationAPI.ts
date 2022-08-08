f416d1754c0c38b9caa87835a264dc022ebba29c
SDKAuto <sdkautomation@microsoft.com>
2022-08-08 08:40:29 +0000

    CodeGen from PR 2591 in test-repo-billy/azure-rest-api-specs Merge fd3740520302f8432b55a6b6727082bdafc31607 into f840c84013f12b701aac7065ceeb13a59b26051b

--- sdk/reservations/arm-reservations/src/azureReservationAPI.ts
+++ sdk/reservations/arm-reservations/src/azureReservationAPI.ts
@@ -62,7 +62,7 @@ export class AzureReservationAPI extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-reservations/7.1.0`;
+    const packageDetails = `azsdk-js-arm-reservations/7.1.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
