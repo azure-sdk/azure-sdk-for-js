d21d61fd03879bbdcdf10a0c18a43acf507c4e37
SDKAuto <sdkautomation@microsoft.com>
2022-08-09 17:29:34 +0000

    CodeGen from PR 19410 in Azure/azure-rest-api-specs Merge 5951b8119d27b9afe5cd985cc4f35d028528a462 into 1f65c332a28a7d9769ec29333958f8f6acc5955f

--- sdk/app/arm-app/src/operations/index.ts
+++ sdk/app/arm-app/src/operations/index.ts
@@ -6,13 +6,17 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
+export * from "./containerAppsAuthConfigs";
 export * from "./containerApps";
 export * from "./containerAppsRevisions";
 export * from "./containerAppsRevisionReplicas";
+export * from "./daprComponents";
+export * from "./containerAppsDiagnostics";
+export * from "./managedEnvironmentDiagnostics";
+export * from "./managedEnvironmentsDiagnostics";
+export * from "./operations";
 export * from "./managedEnvironments";
 export * from "./certificates";
-export * from "./operations";
-export * from "./containerAppsSourceControls";
-export * from "./daprComponents";
-export * from "./containerAppsAuthConfigs";
+export * from "./namespaces";
 export * from "./managedEnvironmentsStorages";
+export * from "./containerAppsSourceControls";
