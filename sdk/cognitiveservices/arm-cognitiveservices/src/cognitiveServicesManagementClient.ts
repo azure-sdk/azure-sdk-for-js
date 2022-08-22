ea0a3cd3b0ca1bc1862892de2ed3cf299f7dc585
SDKAuto <sdkautomation@microsoft.com>
2022-08-22 05:19:10 +0000

    CodeGen from PR 2594 in test-repo-billy/azure-rest-api-specs Merge a031979d10f02c767cd844df3190a0a64b003f60 into a4bf1e830c545e3ac3e7175c70fbcf6ba6602cf1

--- sdk/cognitiveservices/arm-cognitiveservices/src/cognitiveServicesManagementClient.ts
+++ sdk/cognitiveservices/arm-cognitiveservices/src/cognitiveServicesManagementClient.ts
@@ -78,7 +78,7 @@ export class CognitiveServicesManagementClient extends coreClient.ServiceClient 
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-cognitiveservices/7.2.0`;
+    const packageDetails = `azsdk-js-arm-cognitiveservices/7.3.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -153,7 +153,7 @@ export class CognitiveServicesManagementClient extends coreClient.ServiceClient 
         if (param.length > 1) {
           const newParams = param[1].split("&").map((item) => {
             if (item.indexOf("api-version") > -1) {
-              return item.replace(/(?<==).*$/, apiVersion);
+              return "api-version=" + apiVersion;
             } else {
               return item;
             }
