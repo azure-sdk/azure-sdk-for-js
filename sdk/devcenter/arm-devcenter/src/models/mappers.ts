08ad86543d806abb5afecb8db99814908edb2497
SDKAuto <sdkautomation@microsoft.com>
2022-09-02 22:25:55 +0000

    CodeGen from PR 2618 in test-repo-billy/azure-rest-api-specs Merge 5eb9b7cda5c563a020f563da737aace8f88ac79d into 773b56abd149e061e36478d197891409e7bb184c

--- sdk/devcenter/arm-devcenter/src/models/mappers.ts
+++ sdk/devcenter/arm-devcenter/src/models/mappers.ts
@@ -632,6 +632,35 @@ export const EnvironmentTypeUpdate: coreClient.CompositeMapper = {
   }
 };
 
+export const AllowedEnvironmentTypeListResult: coreClient.CompositeMapper = {
+  type: {
+    name: "Composite",
+    className: "AllowedEnvironmentTypeListResult",
+    modelProperties: {
+      value: {
+        serializedName: "value",
+        readOnly: true,
+        type: {
+          name: "Sequence",
+          element: {
+            type: {
+              name: "Composite",
+              className: "AllowedEnvironmentType"
+            }
+          }
+        }
+      },
+      nextLink: {
+        serializedName: "nextLink",
+        readOnly: true,
+        type: {
+          name: "String"
+        }
+      }
+    }
+  }
+};
+
 export const ProjectEnvironmentTypeListResult: coreClient.CompositeMapper = {
   type: {
     name: "Composite",
@@ -1742,6 +1771,23 @@ export const EnvironmentType: coreClient.CompositeMapper = {
           value: { type: { name: "String" } }
         }
       },
+      provisioningState: {
+        serializedName: "properties.provisioningState",
+        readOnly: true,
+        type: {
+          name: "String"
+        }
+      }
+    }
+  }
+};
+
+export const AllowedEnvironmentType: coreClient.CompositeMapper = {
+  type: {
+    name: "Composite",
+    className: "AllowedEnvironmentType",
+    modelProperties: {
+      ...Resource.type.modelProperties,
       provisioningState: {
         serializedName: "properties.provisioningState",
         readOnly: true,
