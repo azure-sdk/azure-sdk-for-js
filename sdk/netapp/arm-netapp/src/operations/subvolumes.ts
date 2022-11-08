c227ebfc14626bf23eef6e35cc75055aab82787d
SDKAuto <sdkautomation@microsoft.com>
2022-11-08 03:30:25 +0000

    CodeGen from PR 21465 in Azure/azure-rest-api-specs Merge 5896811c37f2866f85ad35641cc65e3b65f2082f into 4903b1ed79e30f689d7c469cfa06734cfcd106d6

--- sdk/netapp/arm-netapp/src/operations/subvolumes.ts
+++ sdk/netapp/arm-netapp/src/operations/subvolumes.ts
@@ -379,8 +379,7 @@ export class SubvolumesImpl implements Subvolumes {
     );
     const poller = new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
-      intervalInMs: options?.updateIntervalInMs,
-      lroResourceLocationConfig: "azure-async-operation"
+      intervalInMs: options?.updateIntervalInMs
     });
     await poller.poll();
     return poller;
