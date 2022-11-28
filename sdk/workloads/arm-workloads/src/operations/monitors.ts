fadb00d290c0ae99c8bd700953c525a5d18b5c56
SDKAuto <sdkautomation@microsoft.com>
2022-11-28 06:45:28 +0000

    CodeGen from PR 21662 in Azure/azure-rest-api-specs Merge fa9cde1da3f589774cf5f1259c1237c593b3e794 into 58f076e3d73a63d60d71c5f57ba46acfdc40ecd0

--- sdk/workloads/arm-workloads/src/operations/monitors.ts
+++ sdk/workloads/arm-workloads/src/operations/monitors.ts
@@ -341,7 +341,8 @@ export class MonitorsImpl implements Monitors {
     );
     const poller = new LroEngine(lro, {
       resumeFrom: options?.resumeFrom,
-      intervalInMs: options?.updateIntervalInMs
+      intervalInMs: options?.updateIntervalInMs,
+      lroResourceLocationConfig: "azure-async-operation"
     });
     await poller.poll();
     return poller;
