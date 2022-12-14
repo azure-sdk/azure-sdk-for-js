3fea7c4902afbd0bde3f59c693f5c003d667680c
SDKAuto <sdkautomation@microsoft.com>
2022-12-14 10:52:42 +0000

    CodeGen from PR 601 in test-repo-billy/azure-rest-api-specs-pr Merge b32558dc8af13891c75d24807aee762c42552277 into 31903ac93d4443b7420b47e751e156203b01c444

--- sdk/servicelinker/arm-servicelinker/src/models/parameters.ts
+++ sdk/servicelinker/arm-servicelinker/src/models/parameters.ts
@@ -55,7 +55,7 @@ export const resourceUri: OperationURLParameter = {
 export const apiVersion: OperationQueryParameter = {
   parameterPath: "apiVersion",
   mapper: {
-    defaultValue: "2022-05-01",
+    defaultValue: "2008-11-18",
     isConstant: true,
     serializedName: "api-version",
     type: {
