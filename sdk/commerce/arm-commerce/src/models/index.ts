b9150e55c6c83a139fa43de7df388e8bcaf052be
SDKAuto <sdkautomation@microsoft.com>
2023-02-14 18:38:19 +0000

    CodeGen from PR 22611 in Azure/azure-rest-api-specs Merge 17f18de262c6258fdfed87557d62c0bad69f7343 into 3de2d5433510565336f0a2238bbfe874b777e242

--- sdk/commerce/arm-commerce/src/models/index.ts
+++ sdk/commerce/arm-commerce/src/models/index.ts
@@ -180,14 +180,7 @@ export type UsageAggregatesListResponse = UsageAggregationListResult;
 
 /** Optional parameters. */
 export interface UsageAggregatesListNextOptionalParams
-  extends coreClient.OperationOptions {
-  /** `True` returns usage data in instance-level detail, `false` causes server-side aggregation with fewer details. For example, if you have 3 website instances, by default you will get 3 line items for website consumption. If you specify showDetails = false, the data will be aggregated as a single line item for website consumption within the time period (for the given subscriptionId, meterId, usageStartTime and usageEndTime). */
-  showDetails?: boolean;
-  /** `Daily` (default) returns the data in daily granularity, `Hourly` returns the data in hourly granularity. */
-  aggregationGranularity?: AggregationGranularity;
-  /** Used when a continuation token string is provided in the response body of the previous call, enabling paging through a large result set. If not present, the data is retrieved from the beginning of the day/hour (based on the granularity) passed in. */
-  continuationToken?: string;
-}
+  extends coreClient.OperationOptions {}
 
 /** Contains response data for the listNext operation. */
 export type UsageAggregatesListNextResponse = UsageAggregationListResult;
