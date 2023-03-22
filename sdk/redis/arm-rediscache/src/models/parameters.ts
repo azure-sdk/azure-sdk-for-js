1203c67ee597d8c4a6285ee93fb401b1fa28e023
SDKAuto <sdkautomation@microsoft.com>
2023-03-22 17:23:27 +0000

    CodeGen from PR 23222 in Azure/azure-rest-api-specs Merge de4b61aaf4548af1f9414b2e10b42fe21992914e into 7824f04db829f016255f0042e86f7061401ca5bb

--- sdk/redis/arm-rediscache/src/models/parameters.ts
+++ sdk/redis/arm-rediscache/src/models/parameters.ts
@@ -52,7 +52,7 @@ export const $host: OperationURLParameter = {
 export const apiVersion: OperationQueryParameter = {
   parameterPath: "apiVersion",
   mapper: {
-    defaultValue: "2022-06-01",
+    defaultValue: "2022-06-01-privatepreview",
     isConstant: true,
     serializedName: "api-version",
     type: {
