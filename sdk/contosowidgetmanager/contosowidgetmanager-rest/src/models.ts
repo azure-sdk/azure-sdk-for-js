7dcd9411cfaca61c230ba4773832ee69f0417010
SDKAuto <sdkautomation@microsoft.com>
2023-04-04 19:45:37 +0000

    CodeGen from PR 23168 in Azure/azure-rest-api-specs Merge a0a4a47209e7c4813665a8f4cbd807a908f4cc0c into b32028a99442660664f5b2f341faa698190a3be8

---
+++ sdk/contosowidgetmanager/contosowidgetmanager-rest/src/models.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/** A widget. */
+export interface Widget {
+  /** The ID of the widget's manufacturer. */
+  manufacturerId: string;
+}
