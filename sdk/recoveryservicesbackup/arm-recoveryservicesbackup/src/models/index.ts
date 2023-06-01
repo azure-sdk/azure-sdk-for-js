d02c50b8677485d663ae838237505588b7553b60
SDKAuto <sdkautomation@microsoft.com>
2023-06-01 17:36:32 +0000

    CodeGen from PR 24258 in Azure/azure-rest-api-specs Merge 70f35604d6e171ba4c063707a03369f1a8893a31 into 74d2959659fda706a767579739f779bde0acc7ef

--- sdk/recoveryservicesbackup/arm-recoveryservicesbackup/src/models/index.ts
+++ sdk/recoveryservicesbackup/arm-recoveryservicesbackup/src/models/index.ts
@@ -168,7 +168,7 @@ export type AzureVmWorkloadProtectableItemUnion =
   | AzureVmWorkloadSAPHanaDatabaseProtectableItem
   | AzureVmWorkloadSAPHanaSystemProtectableItem
   | AzureVmWorkloadSAPHanaDBInstance
-  | AzureVmWorkloadSAPHanaHSR
+  | AzureVmWorkloadSAPHanaHSRProtectableItem
   | AzureVmWorkloadSQLAvailabilityGroupProtectableItem
   | AzureVmWorkloadSQLDatabaseProtectableItem
   | AzureVmWorkloadSQLInstanceProtectableItem;
@@ -490,6 +490,8 @@ export interface BackupResourceVaultConfig {
   enhancedSecurityState?: EnhancedSecurityState;
   /** Soft Delete feature state */
   softDeleteFeatureState?: SoftDeleteFeatureState;
+  /** Soft delete retention period in days */
+  softDeleteRetentionPeriod?: number;
   /** ResourceGuard Operation Requests */
   resourceGuardOperationRequests?: string[];
   /** Is soft delete feature state editable */
@@ -513,6 +515,8 @@ export interface PrivateEndpointConnection {
   provisioningState?: ProvisioningState;
   /** Gets or sets private endpoint associated with the private endpoint connection */
   privateEndpoint?: PrivateEndpoint;
+  /** Group Ids for the Private Endpoint */
+  groupIds?: VaultSubResourceType[];
   /** Gets or sets private link service connection state */
   privateLinkServiceConnectionState?: PrivateLinkServiceConnectionState;
 }
@@ -530,7 +534,7 @@ export interface PrivateLinkServiceConnectionState {
   /** Gets or sets description */
   description?: string;
   /** Gets or sets actions required */
-  actionRequired?: string;
+  actionsRequired?: string;
 }
 
 /** An error response from the Container Instance service. */
@@ -974,7 +978,7 @@ export interface WorkloadProtectableItem {
     | "SAPHanaDatabase"
     | "SAPHanaSystem"
     | "SAPHanaDBInstance"
-    | "SAPHanaHSR"
+    | "HanaHSRContainer"
     | "SQLAvailabilityGroupContainer"
     | "SQLDataBase"
     | "SQLInstance";
@@ -3280,7 +3284,7 @@ export interface AzureVmWorkloadProtectableItem
     | "SAPHanaDatabase"
     | "SAPHanaSystem"
     | "SAPHanaDBInstance"
-    | "SAPHanaHSR"
+    | "HanaHSRContainer"
     | "SQLAvailabilityGroupContainer"
     | "SQLDataBase"
     | "SQLInstance";
@@ -3633,11 +3637,11 @@ export interface AzureVmWorkloadSAPHanaDBInstance
   protectableItemType: "SAPHanaDBInstance";
 }
 
-/** Azure VM workload-specific protectable item representing SAP HANA Dbinstance. */
-export interface AzureVmWorkloadSAPHanaHSR
+/** Azure VM workload-specific protectable item representing HANA HSR. */
+export interface AzureVmWorkloadSAPHanaHSRProtectableItem
   extends AzureVmWorkloadProtectableItem {
   /** Polymorphic discriminator, which specifies the different types this object can be */
-  protectableItemType: "SAPHanaHSR";
+  protectableItemType: "HanaHSRContainer";
 }
 
 /** Azure VM workload-specific protectable item representing SQL Availability Group. */
@@ -4229,6 +4233,27 @@ export enum KnownProvisioningState {
  * **Pending**
  */
 export type ProvisioningState = string;
+
+/** Known values of {@link VaultSubResourceType} that the service accepts. */
+export enum KnownVaultSubResourceType {
+  /** AzureBackup */
+  AzureBackup = "AzureBackup",
+  /** AzureBackupSecondary */
+  AzureBackupSecondary = "AzureBackup_secondary",
+  /** AzureSiteRecovery */
+  AzureSiteRecovery = "AzureSiteRecovery"
+}
+
+/**
+ * Defines values for VaultSubResourceType. \
+ * {@link KnownVaultSubResourceType} can be used interchangeably with VaultSubResourceType,
+ *  this enum contains the known values that the service supports.
+ * ### Known values supported by the service
+ * **AzureBackup** \
+ * **AzureBackup_secondary** \
+ * **AzureSiteRecovery**
+ */
+export type VaultSubResourceType = string;
 
 /** Known values of {@link PrivateEndpointConnectionStatus} that the service accepts. */
 export enum KnownPrivateEndpointConnectionStatus {
--- sdk/recoveryservicesbackup/arm-recoveryservicesbackup/src/recoveryServicesBackupClient.ts
+++ sdk/recoveryservicesbackup/arm-recoveryservicesbackup/src/recoveryServicesBackupClient.ts
@@ -168,7 +168,7 @@ export class RecoveryServicesBackupClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-recoveryservicesbackup/10.1.1`;
+    const packageDetails = `azsdk-js-arm-recoveryservicesbackup/11.0.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
