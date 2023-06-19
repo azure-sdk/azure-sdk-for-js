dbb827ca441d1c932e0095936f5c6daf1c80832e
SDKAuto <sdkautomation@microsoft.com>
2023-06-19 03:36:28 +0000

    CodeGen from PR 24457 in Azure/azure-rest-api-specs Merge f09481cca0020662ac94a54fe9b5928e7ea59c98 into 4c231736c4edc01bbefd837ab6f561b223d56da8

--- sdk/security/arm-security/src/securityCenter.ts
+++ sdk/security/arm-security/src/securityCenter.ts
@@ -51,6 +51,7 @@ import {
   SecureScoreControlDefinitionsImpl,
   SecuritySolutionsImpl,
   ConnectorsImpl,
+  SensitivitySettingsImpl,
   AlertsImpl,
   SettingsImpl,
   IngestionSettingsImpl,
@@ -64,13 +65,13 @@ import {
   APICollectionImpl,
   APICollectionOnboardingImpl,
   APICollectionOffboardingImpl,
-  HealthReportsImpl,
-  HealthReportOperationsImpl,
   SqlVulnerabilityAssessmentScansImpl,
   SqlVulnerabilityAssessmentScanResultsImpl,
   SqlVulnerabilityAssessmentBaselineRulesImpl,
   SecurityConnectorsImpl,
-  SecurityOperatorsImpl
+  SecurityOperatorsImpl,
+  ServerVulnerabilityAssessmentsSettingsImpl,
+  HealthReportsImpl
 } from "./operations";
 import {
   MdeOnboardings,
@@ -114,6 +115,7 @@ import {
   SecureScoreControlDefinitions,
   SecuritySolutions,
   Connectors,
+  SensitivitySettings,
   Alerts,
   Settings,
   IngestionSettings,
@@ -127,19 +129,28 @@ import {
   APICollection,
   APICollectionOnboarding,
   APICollectionOffboarding,
-  HealthReports,
-  HealthReportOperations,
   SqlVulnerabilityAssessmentScans,
   SqlVulnerabilityAssessmentScanResults,
   SqlVulnerabilityAssessmentBaselineRules,
   SecurityConnectors,
-  SecurityOperators
+  SecurityOperators,
+  ServerVulnerabilityAssessmentsSettings,
+  HealthReports
 } from "./operationsInterfaces";
-import { SecurityCenterOptionalParams } from "./models";
+import * as Parameters from "./models/parameters";
+import * as Mappers from "./models/mappers";
+import {
+  SecurityCenterOptionalParams,
+  UpdateSensitivitySettingsRequest,
+  UpdateSensitivitySettingsOptionalParams,
+  UpdateSensitivitySettingsResponse,
+  GetSensitivitySettingsOptionalParams,
+  GetSensitivitySettingsOperationResponse
+} from "./models";
 
 export class SecurityCenter extends coreClient.ServiceClient {
   $host: string;
-  subscriptionId: string;
+  subscriptionId?: string;
 
   /**
    * Initializes a new instance of the SecurityCenter class.
@@ -151,12 +162,26 @@ export class SecurityCenter extends coreClient.ServiceClient {
     credentials: coreAuth.TokenCredential,
     subscriptionId: string,
     options?: SecurityCenterOptionalParams
+  );
+  constructor(
+    credentials: coreAuth.TokenCredential,
+    options?: SecurityCenterOptionalParams
+  );
+  constructor(
+    credentials: coreAuth.TokenCredential,
+    subscriptionIdOrOptions?: SecurityCenterOptionalParams | string,
+    options?: SecurityCenterOptionalParams
   ) {
     if (credentials === undefined) {
       throw new Error("'credentials' cannot be null");
     }
-    if (subscriptionId === undefined) {
-      throw new Error("'subscriptionId' cannot be null");
+
+    let subscriptionId: string | undefined;
+
+    if (typeof subscriptionIdOrOptions === "string") {
+      subscriptionId = subscriptionIdOrOptions;
+    } else if (typeof subscriptionIdOrOptions === "object") {
+      options = subscriptionIdOrOptions;
     }
 
     // Initializing default values for options
@@ -290,6 +315,7 @@ export class SecurityCenter extends coreClient.ServiceClient {
     );
     this.securitySolutions = new SecuritySolutionsImpl(this);
     this.connectors = new ConnectorsImpl(this);
+    this.sensitivitySettings = new SensitivitySettingsImpl(this);
     this.alerts = new AlertsImpl(this);
     this.settings = new SettingsImpl(this);
     this.ingestionSettings = new IngestionSettingsImpl(this);
@@ -307,8 +333,6 @@ export class SecurityCenter extends coreClient.ServiceClient {
     this.aPICollection = new APICollectionImpl(this);
     this.aPICollectionOnboarding = new APICollectionOnboardingImpl(this);
     this.aPICollectionOffboarding = new APICollectionOffboardingImpl(this);
-    this.healthReports = new HealthReportsImpl(this);
-    this.healthReportOperations = new HealthReportOperationsImpl(this);
     this.sqlVulnerabilityAssessmentScans = new SqlVulnerabilityAssessmentScansImpl(
       this
     );
@@ -320,6 +344,38 @@ export class SecurityCenter extends coreClient.ServiceClient {
     );
     this.securityConnectors = new SecurityConnectorsImpl(this);
     this.securityOperators = new SecurityOperatorsImpl(this);
+    this.serverVulnerabilityAssessmentsSettings = new ServerVulnerabilityAssessmentsSettingsImpl(
+      this
+    );
+    this.healthReports = new HealthReportsImpl(this);
+  }
+
+  /**
+   * Updates data sensitivity settings for sensitive data discovery
+   * @param sensitivitySettings The data sensitivity settings to update
+   * @param options The options parameters.
+   */
+  updateSensitivitySettings(
+    sensitivitySettings: UpdateSensitivitySettingsRequest,
+    options?: UpdateSensitivitySettingsOptionalParams
+  ): Promise<UpdateSensitivitySettingsResponse> {
+    return this.sendOperationRequest(
+      { sensitivitySettings, options },
+      updateSensitivitySettingsOperationSpec
+    );
+  }
+
+  /**
+   * Gets data sensitivity settings for sensitive data discovery
+   * @param options The options parameters.
+   */
+  getSensitivitySettings(
+    options?: GetSensitivitySettingsOptionalParams
+  ): Promise<GetSensitivitySettingsOperationResponse> {
+    return this.sendOperationRequest(
+      { options },
+      getSensitivitySettingsOperationSpec
+    );
   }
 
   mdeOnboardings: MdeOnboardings;
@@ -363,6 +419,7 @@ export class SecurityCenter extends coreClient.ServiceClient {
   secureScoreControlDefinitions: SecureScoreControlDefinitions;
   securitySolutions: SecuritySolutions;
   connectors: Connectors;
+  sensitivitySettings: SensitivitySettings;
   alerts: Alerts;
   settings: Settings;
   ingestionSettings: IngestionSettings;
@@ -376,11 +433,48 @@ export class SecurityCenter extends coreClient.ServiceClient {
   aPICollection: APICollection;
   aPICollectionOnboarding: APICollectionOnboarding;
   aPICollectionOffboarding: APICollectionOffboarding;
-  healthReports: HealthReports;
-  healthReportOperations: HealthReportOperations;
   sqlVulnerabilityAssessmentScans: SqlVulnerabilityAssessmentScans;
   sqlVulnerabilityAssessmentScanResults: SqlVulnerabilityAssessmentScanResults;
   sqlVulnerabilityAssessmentBaselineRules: SqlVulnerabilityAssessmentBaselineRules;
   securityConnectors: SecurityConnectors;
   securityOperators: SecurityOperators;
+  serverVulnerabilityAssessmentsSettings: ServerVulnerabilityAssessmentsSettings;
+  healthReports: HealthReports;
 }
+// Operation Specifications
+const serializer = coreClient.createSerializer(Mappers, /* isXml */ false);
+
+const updateSensitivitySettingsOperationSpec: coreClient.OperationSpec = {
+  path: "/providers/Microsoft.Security/sensitivitySettings/current",
+  httpMethod: "PUT",
+  responses: {
+    200: {
+      bodyMapper: Mappers.GetSensitivitySettingsResponse
+    },
+    default: {
+      bodyMapper: Mappers.CloudError
+    }
+  },
+  requestBody: Parameters.sensitivitySettings,
+  queryParameters: [Parameters.apiVersion12],
+  urlParameters: [Parameters.$host],
+  headerParameters: [Parameters.accept, Parameters.contentType],
+  mediaType: "json",
+  serializer
+};
+const getSensitivitySettingsOperationSpec: coreClient.OperationSpec = {
+  path: "/providers/Microsoft.Security/sensitivitySettings/current",
+  httpMethod: "GET",
+  responses: {
+    200: {
+      bodyMapper: Mappers.GetSensitivitySettingsResponse
+    },
+    default: {
+      bodyMapper: Mappers.CloudError
+    }
+  },
+  queryParameters: [Parameters.apiVersion12],
+  urlParameters: [Parameters.$host],
+  headerParameters: [Parameters.accept],
+  serializer
+};
