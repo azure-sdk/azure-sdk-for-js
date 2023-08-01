aee83f5ea61b90b1ff0606fb3aff3cf33333730b
SDKAuto <sdkautomation@microsoft.com>
2023-08-01 20:41:28 +0000

    CodeGen from PR 25088 in Azure/azure-rest-api-specs Merge 70092bbac05c025eb4b7d3c9f66b5bc99a0db101 into 0b18643d5c21cb1ddb57cbb1503f1d9269c337a7

--- sdk/managementgroups/arm-managementgroups/src/models/parameters.ts
+++ sdk/managementgroups/arm-managementgroups/src/models/parameters.ts
@@ -45,7 +45,7 @@ export const $host: OperationURLParameter = {
 export const apiVersion: OperationQueryParameter = {
   parameterPath: "apiVersion",
   mapper: {
-    defaultValue: "2021-04-01",
+    defaultValue: "2023-04-01",
     isConstant: true,
     serializedName: "api-version",
     type: {
