3d4546e23f835748c1a6f3c5dc94f08a76594789
SDKAuto <sdkautomation@microsoft.com>
2023-08-10 00:26:01 +0000

    CodeGen from PR 25252 in Azure/azure-rest-api-specs Merge 3ed9d9ecb28a185591c9f846d19ae7f502c1ab93 into c992976478b8ab8c3ea2b4abaeac9b0aacee55fe

--- sdk/resources-subscriptions/arm-resources-subscriptions/src/subscriptionClient.ts
+++ sdk/resources-subscriptions/arm-resources-subscriptions/src/subscriptionClient.ts
@@ -50,7 +50,7 @@ export class SubscriptionClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-resources-subscriptions/2.1.1`;
+    const packageDetails = `azsdk-js-arm-resources-subscriptions/1.0.0-beta.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
