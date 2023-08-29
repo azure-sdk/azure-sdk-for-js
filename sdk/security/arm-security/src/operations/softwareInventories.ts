c0474a43b8ad9f39682260a3517f2c6bd710b8e4
SDKAuto <sdkautomation@microsoft.com>
2023-08-29 09:14:40 +0000

    CodeGen from PR 25573 in Azure/azure-rest-api-specs Merge e5c0c0a7dcf22c0aba8984237911b9a629e85478 into 0762e82bcccef4a032e29dda5e4c07fd7cc822a6

--- sdk/security/arm-security/src/operations/softwareInventories.ts
+++ sdk/security/arm-security/src/operations/softwareInventories.ts
@@ -330,7 +330,7 @@ const listByExtendedResourceOperationSpec: coreClient.OperationSpec = {
       bodyMapper: Mappers.CloudError
     }
   },
-  queryParameters: [Parameters.apiVersion15],
+  queryParameters: [Parameters.apiVersion16],
   urlParameters: [
     Parameters.$host,
     Parameters.subscriptionId,
@@ -354,7 +354,7 @@ const listBySubscriptionOperationSpec: coreClient.OperationSpec = {
       bodyMapper: Mappers.CloudError
     }
   },
-  queryParameters: [Parameters.apiVersion15],
+  queryParameters: [Parameters.apiVersion16],
   urlParameters: [Parameters.$host, Parameters.subscriptionId],
   headerParameters: [Parameters.accept],
   serializer
@@ -371,7 +371,7 @@ const getOperationSpec: coreClient.OperationSpec = {
       bodyMapper: Mappers.CloudError
     }
   },
-  queryParameters: [Parameters.apiVersion15],
+  queryParameters: [Parameters.apiVersion16],
   urlParameters: [
     Parameters.$host,
     Parameters.subscriptionId,
