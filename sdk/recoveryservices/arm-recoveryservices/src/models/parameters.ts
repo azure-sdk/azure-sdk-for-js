78bfaae38fc264547e8227214f9977f24c8aafce
SDKAuto <sdkautomation@microsoft.com>
2023-09-01 07:46:19 +0000

    CodeGen from PR 25625 in Azure/azure-rest-api-specs Merge 7c6eaf837aeed0f6ae17c96857db8b0de2ddb2ff into 80c21c17b4a7aa57f637ee594f7cfd653255a7e0

--- sdk/recoveryservices/arm-recoveryservices/src/models/parameters.ts
+++ sdk/recoveryservices/arm-recoveryservices/src/models/parameters.ts
@@ -78,7 +78,7 @@ export const subscriptionId: OperationURLParameter = {
 export const apiVersion: OperationQueryParameter = {
   parameterPath: "apiVersion",
   mapper: {
-    defaultValue: "2023-02-01",
+    defaultValue: "2023-06-01",
     isConstant: true,
     serializedName: "api-version",
     type: {
