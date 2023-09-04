b91adae4fc88183cba4212fec81045110d0435f7
SDKAuto <sdkautomation@microsoft.com>
2023-09-04 10:00:22 +0000

    CodeGen from PR 24659 in Azure/azure-rest-api-specs Merge b893e09e0019f66ec5c420e87590d0cbb6abd83c into 2265cda5e0021ea1ab9cadd1fa3dc68bb4f5e6b4

--- sdk/apimanagement/arm-apimanagement/src/operations/policyFragment.ts
+++ sdk/apimanagement/arm-apimanagement/src/operations/policyFragment.ts
@@ -6,6 +6,8 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
+import { PagedAsyncIterableIterator, PageSettings } from "@azure/core-paging";
+import { setContinuationToken } from "../pagingHelper";
 import { PolicyFragment } from "../operationsInterfaces";
 import * as coreClient from "@azure/core-client";
 import * as Mappers from "../models/mappers";
@@ -18,20 +20,23 @@ import {
 } from "@azure/core-lro";
 import { createLroSpec } from "../lroImpl";
 import {
+  PolicyFragmentContract,
+  PolicyFragmentListByServiceNextOptionalParams,
   PolicyFragmentListByServiceOptionalParams,
   PolicyFragmentListByServiceResponse,
   PolicyFragmentGetEntityTagOptionalParams,
   PolicyFragmentGetEntityTagResponse,
   PolicyFragmentGetOptionalParams,
   PolicyFragmentGetResponse,
-  PolicyFragmentContract,
   PolicyFragmentCreateOrUpdateOptionalParams,
   PolicyFragmentCreateOrUpdateResponse,
   PolicyFragmentDeleteOptionalParams,
   PolicyFragmentListReferencesOptionalParams,
-  PolicyFragmentListReferencesResponse
+  PolicyFragmentListReferencesResponse,
+  PolicyFragmentListByServiceNextResponse
 } from "../models";
 
+/// <reference lib="esnext.asynciterable" />
 /** Class containing PolicyFragment operations. */
 export class PolicyFragmentImpl implements PolicyFragment {
   private readonly client: ApiManagementClient;
@@ -50,7 +55,91 @@ export class PolicyFragmentImpl implements PolicyFragment {
    * @param serviceName The name of the API Management service.
    * @param options The options parameters.
    */
-  listByService(
+  public listByService(
+    resourceGroupName: string,
+    serviceName: string,
+    options?: PolicyFragmentListByServiceOptionalParams
+  ): PagedAsyncIterableIterator<PolicyFragmentContract> {
+    const iter = this.listByServicePagingAll(
+      resourceGroupName,
+      serviceName,
+      options
+    );
+    return {
+      next() {
+        return iter.next();
+      },
+      [Symbol.asyncIterator]() {
+        return this;
+      },
+      byPage: (settings?: PageSettings) => {
+        if (settings?.maxPageSize) {
+          throw new Error("maxPageSize is not supported by this operation.");
+        }
+        return this.listByServicePagingPage(
+          resourceGroupName,
+          serviceName,
+          options,
+          settings
+        );
+      }
+    };
+  }
+
+  private async *listByServicePagingPage(
+    resourceGroupName: string,
+    serviceName: string,
+    options?: PolicyFragmentListByServiceOptionalParams,
+    settings?: PageSettings
+  ): AsyncIterableIterator<PolicyFragmentContract[]> {
+    let result: PolicyFragmentListByServiceResponse;
+    let continuationToken = settings?.continuationToken;
+    if (!continuationToken) {
+      result = await this._listByService(
+        resourceGroupName,
+        serviceName,
+        options
+      );
+      let page = result.value || [];
+      continuationToken = result.nextLink;
+      setContinuationToken(page, continuationToken);
+      yield page;
+    }
+    while (continuationToken) {
+      result = await this._listByServiceNext(
+        resourceGroupName,
+        serviceName,
+        continuationToken,
+        options
+      );
+      continuationToken = result.nextLink;
+      let page = result.value || [];
+      setContinuationToken(page, continuationToken);
+      yield page;
+    }
+  }
+
+  private async *listByServicePagingAll(
+    resourceGroupName: string,
+    serviceName: string,
+    options?: PolicyFragmentListByServiceOptionalParams
+  ): AsyncIterableIterator<PolicyFragmentContract> {
+    for await (const page of this.listByServicePagingPage(
+      resourceGroupName,
+      serviceName,
+      options
+    )) {
+      yield* page;
+    }
+  }
+
+  /**
+   * Gets all policy fragments.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
+   * @param serviceName The name of the API Management service.
+   * @param options The options parameters.
+   */
+  private _listByService(
     resourceGroupName: string,
     serviceName: string,
     options?: PolicyFragmentListByServiceOptionalParams
@@ -240,6 +329,25 @@ export class PolicyFragmentImpl implements PolicyFragment {
       listReferencesOperationSpec
     );
   }
+
+  /**
+   * ListByServiceNext
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
+   * @param serviceName The name of the API Management service.
+   * @param nextLink The nextLink from the previous successful call to the ListByService method.
+   * @param options The options parameters.
+   */
+  private _listByServiceNext(
+    resourceGroupName: string,
+    serviceName: string,
+    nextLink: string,
+    options?: PolicyFragmentListByServiceNextOptionalParams
+  ): Promise<PolicyFragmentListByServiceNextResponse> {
+    return this.client.sendOperationRequest(
+      { resourceGroupName, serviceName, nextLink, options },
+      listByServiceNextOperationSpec
+    );
+  }
 }
 // Operation Specifications
 const serializer = coreClient.createSerializer(Mappers, /* isXml */ false);
@@ -344,7 +452,7 @@ const createOrUpdateOperationSpec: coreClient.OperationSpec = {
       bodyMapper: Mappers.ErrorResponse
     }
   },
-  requestBody: Parameters.parameters57,
+  requestBody: Parameters.parameters62,
   queryParameters: [Parameters.apiVersion],
   urlParameters: [
     Parameters.$host,
@@ -402,6 +510,27 @@ const listReferencesOperationSpec: coreClient.OperationSpec = {
     Parameters.serviceName,
     Parameters.subscriptionId,
     Parameters.id
+  ],
+  headerParameters: [Parameters.accept],
+  serializer
+};
+const listByServiceNextOperationSpec: coreClient.OperationSpec = {
+  path: "{nextLink}",
+  httpMethod: "GET",
+  responses: {
+    200: {
+      bodyMapper: Mappers.PolicyFragmentCollection
+    },
+    default: {
+      bodyMapper: Mappers.ErrorResponse
+    }
+  },
+  urlParameters: [
+    Parameters.$host,
+    Parameters.resourceGroupName,
+    Parameters.serviceName,
+    Parameters.subscriptionId,
+    Parameters.nextLink
   ],
   headerParameters: [Parameters.accept],
   serializer
