1c58157b5524ac2f3e3343a7a3c438f97d6493db
SDKAuto <sdkautomation@microsoft.com>
2023-11-02 06:54:26 +0000

    CodeGen from PR 26412 in Azure/azure-rest-api-specs Merge 2000a90b0b55a448d324807fd7da5a846915e73f into b7b77b11ba1f6defc86d309d4ca0d51b2a2646a7

--- sdk/openai/openai/sources/generated/src/rest/parameters.ts
+++ sdk/openai/openai/sources/generated/src/rest/parameters.ts
@@ -3,18 +3,69 @@
 
 import { RequestParameters } from "@azure-rest/core-client";
 import {
-  EmbeddingsOptions,
+  AudioTranscriptionOptions,
+  AudioTranslationOptions,
   CompletionsOptions,
   ChatCompletionsOptions,
   ImageGenerationOptions,
+  EmbeddingsOptions,
 } from "./models.js";
 
-export interface GetEmbeddingsBodyParam {
-  body?: EmbeddingsOptions;
+export interface GetAudioTranscriptionAsPlainTextBodyParam {
+  body?: AudioTranscriptionOptions;
 }
 
-export type GetEmbeddingsParameters = GetEmbeddingsBodyParam &
-  RequestParameters;
+export interface GetAudioTranscriptionAsPlainTextMediaTypesParam {
+  /** The content type for the operation. Always multipart/form-data for this operation. */
+  contentType: "multipart/form-data";
+}
+
+export type GetAudioTranscriptionAsPlainTextParameters =
+  GetAudioTranscriptionAsPlainTextMediaTypesParam &
+    GetAudioTranscriptionAsPlainTextBodyParam &
+    RequestParameters;
+
+export interface GetAudioTranscriptionAsResponseObjectBodyParam {
+  body?: AudioTranscriptionOptions;
+}
+
+export interface GetAudioTranscriptionAsResponseObjectMediaTypesParam {
+  /** The content type for the operation. Always multipart/form-data for this operation. */
+  contentType: "multipart/form-data";
+}
+
+export type GetAudioTranscriptionAsResponseObjectParameters =
+  GetAudioTranscriptionAsResponseObjectMediaTypesParam &
+    GetAudioTranscriptionAsResponseObjectBodyParam &
+    RequestParameters;
+
+export interface GetAudioTranslationAsPlainTextBodyParam {
+  body?: AudioTranslationOptions;
+}
+
+export interface GetAudioTranslationAsPlainTextMediaTypesParam {
+  /** The content type for the operation. Always multipart/form-data for this operation. */
+  contentType: "multipart/form-data";
+}
+
+export type GetAudioTranslationAsPlainTextParameters =
+  GetAudioTranslationAsPlainTextMediaTypesParam &
+    GetAudioTranslationAsPlainTextBodyParam &
+    RequestParameters;
+
+export interface GetAudioTranslationAsResponseObjectBodyParam {
+  body?: AudioTranslationOptions;
+}
+
+export interface GetAudioTranslationAsResponseObjectMediaTypesParam {
+  /** The content type for the operation. Always multipart/form-data for this operation. */
+  contentType: "multipart/form-data";
+}
+
+export type GetAudioTranslationAsResponseObjectParameters =
+  GetAudioTranslationAsResponseObjectMediaTypesParam &
+    GetAudioTranslationAsResponseObjectBodyParam &
+    RequestParameters;
 
 export interface GetCompletionsBodyParam {
   body?: CompletionsOptions;
@@ -45,3 +96,10 @@ export interface BeginAzureBatchImageGenerationBodyParam {
 
 export type BeginAzureBatchImageGenerationParameters =
   BeginAzureBatchImageGenerationBodyParam & RequestParameters;
+
+export interface GetEmbeddingsBodyParam {
+  body?: EmbeddingsOptions;
+}
+
+export type GetEmbeddingsParameters = GetEmbeddingsBodyParam &
+  RequestParameters;
