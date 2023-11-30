ce2c6fb384f3e57ce0ec2b369e72e7995118b8d3
SDKAuto <sdkautomation@microsoft.com>
2023-11-30 16:48:54 +0000

    CodeGen from PR 26531 in Azure/azure-rest-api-specs Merge 66462905173b76ec80763dfaf538de2f12e5902f into 6556dd83e0f2fd5b690010b15270eb9982ca6ca9

--- sdk/containerservice/arm-containerservicefleet/src/models/index.ts
+++ sdk/containerservice/arm-containerservicefleet/src/models/index.ts
@@ -479,7 +479,7 @@ export interface TrackedResource extends Resource {
 /** The resource model definition for a Azure Resource Manager proxy resource. It will not have tags and a location */
 export interface ProxyResource extends Resource {}
 
-/** The Fleet resource. */
+/** The Fleet resource.. */
 export interface Fleet extends TrackedResource {
   /**
    * If eTag is provided in the response body, it may also be provided as a header per the normal etag convention.  Entity tags are used for comparing two or more entities from the same requested resource. HTTP/1.1 uses entity tags in the etag (section 14.19), If-Match (section 14.24), If-None-Match (section 14.26), and If-Range (section 14.27) header fields.
