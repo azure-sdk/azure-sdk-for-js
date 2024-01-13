54c8615808274af5b3d0c59ce338bac481785d69
SDKAuto <sdkautomation@microsoft.com>
2024-01-13 03:49:55 +0000

    CodeGen from PR 27371 in Azure/azure-rest-api-specs Merge f07ad75e9b792388a46c86dbbbebd2c7a049282c into e79c2debdebd659662c78885c117603d991d3b06

--- sdk/communication/communication-job-router-rest/src/clientDefinitions.ts
+++ sdk/communication/communication-job-router-rest/src/clientDefinitions.ts
@@ -115,7 +115,7 @@ import { Client, StreamableMethod } from "@azure-rest/core-client";
 export interface UpsertClassificationPolicy {
   /** Creates or updates a classification policy. */
   patch(
-    options: UpsertClassificationPolicyParameters,
+    options: UpsertClassificationPolicyParameters
   ): StreamableMethod<
     | UpsertClassificationPolicy200Response
     | UpsertClassificationPolicy201Response
@@ -123,29 +123,33 @@ export interface UpsertClassificationPolicy {
   >;
   /** Retrieves an existing classification policy by Id. */
   get(
-    options?: GetClassificationPolicyParameters,
-  ): StreamableMethod<GetClassificationPolicy200Response | GetClassificationPolicyDefaultResponse>;
+    options?: GetClassificationPolicyParameters
+  ): StreamableMethod<
+    GetClassificationPolicy200Response | GetClassificationPolicyDefaultResponse
+  >;
   /** Delete a classification policy by Id. */
   delete(
-    options?: DeleteClassificationPolicyParameters,
+    options?: DeleteClassificationPolicyParameters
   ): StreamableMethod<
-    DeleteClassificationPolicy204Response | DeleteClassificationPolicyDefaultResponse
+    | DeleteClassificationPolicy204Response
+    | DeleteClassificationPolicyDefaultResponse
   >;
 }
 
 export interface ListClassificationPolicies {
   /** Retrieves existing classification policies. */
   get(
-    options?: ListClassificationPoliciesParameters,
+    options?: ListClassificationPoliciesParameters
   ): StreamableMethod<
-    ListClassificationPolicies200Response | ListClassificationPoliciesDefaultResponse
+    | ListClassificationPolicies200Response
+    | ListClassificationPoliciesDefaultResponse
   >;
 }
 
 export interface UpsertDistributionPolicy {
   /** Creates or updates a distribution policy. */
   patch(
-    options: UpsertDistributionPolicyParameters,
+    options: UpsertDistributionPolicyParameters
   ): StreamableMethod<
     | UpsertDistributionPolicy200Response
     | UpsertDistributionPolicy201Response
@@ -153,29 +157,33 @@ export interface UpsertDistributionPolicy {
   >;
   /** Retrieves an existing distribution policy by Id. */
   get(
-    options?: GetDistributionPolicyParameters,
-  ): StreamableMethod<GetDistributionPolicy200Response | GetDistributionPolicyDefaultResponse>;
+    options?: GetDistributionPolicyParameters
+  ): StreamableMethod<
+    GetDistributionPolicy200Response | GetDistributionPolicyDefaultResponse
+  >;
   /** Delete a distribution policy by Id. */
   delete(
-    options?: DeleteDistributionPolicyParameters,
+    options?: DeleteDistributionPolicyParameters
   ): StreamableMethod<
-    DeleteDistributionPolicy204Response | DeleteDistributionPolicyDefaultResponse
+    | DeleteDistributionPolicy204Response
+    | DeleteDistributionPolicyDefaultResponse
   >;
 }
 
 export interface ListDistributionPolicies {
   /** Retrieves existing distribution policies. */
   get(
-    options?: ListDistributionPoliciesParameters,
+    options?: ListDistributionPoliciesParameters
   ): StreamableMethod<
-    ListDistributionPolicies200Response | ListDistributionPoliciesDefaultResponse
+    | ListDistributionPolicies200Response
+    | ListDistributionPoliciesDefaultResponse
   >;
 }
 
 export interface UpsertExceptionPolicy {
   /** Creates or updates a exception policy. */
   patch(
-    options: UpsertExceptionPolicyParameters,
+    options: UpsertExceptionPolicyParameters
   ): StreamableMethod<
     | UpsertExceptionPolicy200Response
     | UpsertExceptionPolicy201Response
@@ -183,139 +191,165 @@ export interface UpsertExceptionPolicy {
   >;
   /** Retrieves an existing exception policy by Id. */
   get(
-    options?: GetExceptionPolicyParameters,
-  ): StreamableMethod<GetExceptionPolicy200Response | GetExceptionPolicyDefaultResponse>;
+    options?: GetExceptionPolicyParameters
+  ): StreamableMethod<
+    GetExceptionPolicy200Response | GetExceptionPolicyDefaultResponse
+  >;
   /** Deletes a exception policy by Id. */
   delete(
-    options?: DeleteExceptionPolicyParameters,
-  ): StreamableMethod<DeleteExceptionPolicy204Response | DeleteExceptionPolicyDefaultResponse>;
+    options?: DeleteExceptionPolicyParameters
+  ): StreamableMethod<
+    DeleteExceptionPolicy204Response | DeleteExceptionPolicyDefaultResponse
+  >;
 }
 
 export interface ListExceptionPolicies {
   /** Retrieves existing exception policies. */
   get(
-    options?: ListExceptionPoliciesParameters,
-  ): StreamableMethod<ListExceptionPolicies200Response | ListExceptionPoliciesDefaultResponse>;
+    options?: ListExceptionPoliciesParameters
+  ): StreamableMethod<
+    ListExceptionPolicies200Response | ListExceptionPoliciesDefaultResponse
+  >;
 }
 
 export interface UpsertQueue {
   /** Creates or updates a queue. */
   patch(
-    options: UpsertQueueParameters,
-  ): StreamableMethod<UpsertQueue200Response | UpsertQueue201Response | UpsertQueueDefaultResponse>;
+    options: UpsertQueueParameters
+  ): StreamableMethod<
+    UpsertQueue200Response | UpsertQueue201Response | UpsertQueueDefaultResponse
+  >;
   /** Retrieves an existing queue by Id. */
   get(
-    options?: GetQueueParameters,
+    options?: GetQueueParameters
   ): StreamableMethod<GetQueue200Response | GetQueueDefaultResponse>;
   /** Deletes a queue by Id. */
   delete(
-    options?: DeleteQueueParameters,
+    options?: DeleteQueueParameters
   ): StreamableMethod<DeleteQueue204Response | DeleteQueueDefaultResponse>;
 }
 
 export interface ListQueues {
   /** Retrieves existing queues. */
   get(
-    options?: ListQueuesParameters,
+    options?: ListQueuesParameters
   ): StreamableMethod<ListQueues200Response | ListQueuesDefaultResponse>;
 }
 
 export interface UpsertJob {
   /** Creates or updates a router job. */
   patch(
-    options: UpsertJobParameters,
-  ): StreamableMethod<UpsertJob200Response | UpsertJob201Response | UpsertJobDefaultResponse>;
+    options: UpsertJobParameters
+  ): StreamableMethod<
+    UpsertJob200Response | UpsertJob201Response | UpsertJobDefaultResponse
+  >;
   /** Retrieves an existing job by Id. */
-  get(options?: GetJobParameters): StreamableMethod<GetJob200Response | GetJobDefaultResponse>;
+  get(
+    options?: GetJobParameters
+  ): StreamableMethod<GetJob200Response | GetJobDefaultResponse>;
   /** Deletes a job and all of its traces. */
   delete(
-    options?: DeleteJobParameters,
+    options?: DeleteJobParameters
   ): StreamableMethod<DeleteJob204Response | DeleteJobDefaultResponse>;
 }
 
 export interface Reclassify {
   /** Reclassify a job. */
   post(
-    options?: ReclassifyParameters,
+    options?: ReclassifyParameters
   ): StreamableMethod<Reclassify200Response | ReclassifyDefaultResponse>;
 }
 
 export interface Cancel {
   /** Submits request to cancel an existing job by Id while supplying free-form cancellation reason. */
-  post(options?: CancelParameters): StreamableMethod<Cancel200Response | CancelDefaultResponse>;
+  post(
+    options?: CancelParameters
+  ): StreamableMethod<Cancel200Response | CancelDefaultResponse>;
 }
 
 export interface Complete {
   /** Completes an assigned job. */
   post(
-    options?: CompleteParameters,
+    options?: CompleteParameters
   ): StreamableMethod<Complete200Response | CompleteDefaultResponse>;
 }
 
 export interface Close {
   /** Closes a completed job. */
-  post(options?: CloseParameters): StreamableMethod<Close200Response | CloseDefaultResponse>;
+  post(
+    options?: CloseParameters
+  ): StreamableMethod<Close200Response | CloseDefaultResponse>;
 }
 
 export interface ListJobs {
   /** Retrieves list of jobs based on filter parameters. */
   get(
-    options?: ListJobsParameters,
+    options?: ListJobsParameters
   ): StreamableMethod<ListJobs200Response | ListJobsDefaultResponse>;
 }
 
 export interface GetInQueuePosition {
   /** Gets a job's position details. */
   get(
-    options?: GetInQueuePositionParameters,
-  ): StreamableMethod<GetInQueuePosition200Response | GetInQueuePositionDefaultResponse>;
+    options?: GetInQueuePositionParameters
+  ): StreamableMethod<
+    GetInQueuePosition200Response | GetInQueuePositionDefaultResponse
+  >;
 }
 
 export interface Unassign {
   /** Unassign a job. */
   post(
-    options?: UnassignParameters,
+    options?: UnassignParameters
   ): StreamableMethod<Unassign200Response | UnassignDefaultResponse>;
 }
 
 export interface Accept {
   /** Accepts an offer to work on a job and returns a 409/Conflict if another agent accepted the job already. */
-  post(options?: AcceptParameters): StreamableMethod<Accept200Response | AcceptDefaultResponse>;
+  post(
+    options?: AcceptParameters
+  ): StreamableMethod<Accept200Response | AcceptDefaultResponse>;
 }
 
 export interface Decline {
   /** Declines an offer to work on a job. */
-  post(options?: DeclineParameters): StreamableMethod<Decline200Response | DeclineDefaultResponse>;
+  post(
+    options?: DeclineParameters
+  ): StreamableMethod<Decline200Response | DeclineDefaultResponse>;
 }
 
 export interface GetQueueStatistics {
   /** Retrieves a queue's statistics. */
   get(
-    options?: GetQueueStatisticsParameters,
-  ): StreamableMethod<GetQueueStatistics200Response | GetQueueStatisticsDefaultResponse>;
+    options?: GetQueueStatisticsParameters
+  ): StreamableMethod<
+    GetQueueStatistics200Response | GetQueueStatisticsDefaultResponse
+  >;
 }
 
 export interface UpsertWorker {
   /** Creates or updates a worker. */
   patch(
-    options: UpsertWorkerParameters,
+    options: UpsertWorkerParameters
   ): StreamableMethod<
-    UpsertWorker200Response | UpsertWorker201Response | UpsertWorkerDefaultResponse
+    | UpsertWorker200Response
+    | UpsertWorker201Response
+    | UpsertWorkerDefaultResponse
   >;
   /** Retrieves an existing worker by Id. */
   get(
-    options?: GetWorkerParameters,
+    options?: GetWorkerParameters
   ): StreamableMethod<GetWorker200Response | GetWorkerDefaultResponse>;
   /** Deletes a worker and all of its traces. */
   delete(
-    options?: DeleteWorkerParameters,
+    options?: DeleteWorkerParameters
   ): StreamableMethod<DeleteWorker204Response | DeleteWorkerDefaultResponse>;
 }
 
 export interface ListWorkers {
   /** Retrieves existing workers. */
   get(
-    options?: ListWorkersParameters,
+    options?: ListWorkersParameters
   ): StreamableMethod<ListWorkers200Response | ListWorkersDefaultResponse>;
 }
 
@@ -323,21 +357,21 @@ export interface Routes {
   /** Resource for '/routing/classificationPolicies/\{classificationPolicyId\}' has methods for the following verbs: patch, get, delete */
   (
     path: "/routing/classificationPolicies/{classificationPolicyId}",
-    classificationPolicyId: string,
+    classificationPolicyId: string
   ): UpsertClassificationPolicy;
   /** Resource for '/routing/classificationPolicies' has methods for the following verbs: get */
   (path: "/routing/classificationPolicies"): ListClassificationPolicies;
   /** Resource for '/routing/distributionPolicies/\{distributionPolicyId\}' has methods for the following verbs: patch, get, delete */
   (
     path: "/routing/distributionPolicies/{distributionPolicyId}",
-    distributionPolicyId: string,
+    distributionPolicyId: string
   ): UpsertDistributionPolicy;
   /** Resource for '/routing/distributionPolicies' has methods for the following verbs: get */
   (path: "/routing/distributionPolicies"): ListDistributionPolicies;
   /** Resource for '/routing/exceptionPolicies/\{exceptionPolicyId\}' has methods for the following verbs: patch, get, delete */
   (
     path: "/routing/exceptionPolicies/{exceptionPolicyId}",
-    exceptionPolicyId: string,
+    exceptionPolicyId: string
   ): UpsertExceptionPolicy;
   /** Resource for '/routing/exceptionPolicies' has methods for the following verbs: get */
   (path: "/routing/exceptionPolicies"): ListExceptionPolicies;
@@ -355,13 +389,13 @@ export interface Routes {
   (
     path: "/routing/jobs/{jobId}/assignments/{assignmentId}:complete",
     jobId: string,
-    assignmentId: string,
+    assignmentId: string
   ): Complete;
   /** Resource for '/routing/jobs/\{jobId\}/assignments/\{assignmentId\}:close' has methods for the following verbs: post */
   (
     path: "/routing/jobs/{jobId}/assignments/{assignmentId}:close",
     jobId: string,
-    assignmentId: string,
+    assignmentId: string
   ): Close;
   /** Resource for '/routing/jobs' has methods for the following verbs: get */
   (path: "/routing/jobs"): ListJobs;
@@ -371,22 +405,25 @@ export interface Routes {
   (
     path: "/routing/jobs/{jobId}/assignments/{assignmentId}:unassign",
     jobId: string,
-    assignmentId: string,
+    assignmentId: string
   ): Unassign;
   /** Resource for '/routing/workers/\{workerId\}/offers/\{offerId\}:accept' has methods for the following verbs: post */
   (
     path: "/routing/workers/{workerId}/offers/{offerId}:accept",
     workerId: string,
-    offerId: string,
+    offerId: string
   ): Accept;
   /** Resource for '/routing/workers/\{workerId\}/offers/\{offerId\}:decline' has methods for the following verbs: post */
   (
     path: "/routing/workers/{workerId}/offers/{offerId}:decline",
     workerId: string,
-    offerId: string,
+    offerId: string
   ): Decline;
   /** Resource for '/routing/queues/\{queueId\}/statistics' has methods for the following verbs: get */
-  (path: "/routing/queues/{queueId}/statistics", queueId: string): GetQueueStatistics;
+  (
+    path: "/routing/queues/{queueId}/statistics",
+    queueId: string
+  ): GetQueueStatistics;
   /** Resource for '/routing/workers/\{workerId\}' has methods for the following verbs: patch, get, delete */
   (path: "/routing/workers/{workerId}", workerId: string): UpsertWorker;
   /** Resource for '/routing/workers' has methods for the following verbs: get */
--- sdk/communication/communication-job-router-rest/src/isUnexpected.ts
+++ sdk/communication/communication-job-router-rest/src/isUnexpected.ts
@@ -77,7 +77,10 @@ import {
 } from "./responses";
 
 const responseMap: Record<string, string[]> = {
-  "PATCH /routing/classificationPolicies/{classificationPolicyId}": ["200", "201"],
+  "PATCH /routing/classificationPolicies/{classificationPolicyId}": [
+    "200",
+    "201",
+  ],
   "GET /routing/classificationPolicies/{classificationPolicyId}": ["200"],
   "DELETE /routing/classificationPolicies/{classificationPolicyId}": ["204"],
   "GET /routing/classificationPolicies": ["200"],
@@ -116,109 +119,134 @@ export function isUnexpected(
   response:
     | UpsertClassificationPolicy200Response
     | UpsertClassificationPolicy201Response
-    | UpsertClassificationPolicyDefaultResponse,
+    | UpsertClassificationPolicyDefaultResponse
 ): response is UpsertClassificationPolicyDefaultResponse;
 export function isUnexpected(
-  response: GetClassificationPolicy200Response | GetClassificationPolicyDefaultResponse,
+  response:
+    | GetClassificationPolicy200Response
+    | GetClassificationPolicyDefaultResponse
 ): response is GetClassificationPolicyDefaultResponse;
 export function isUnexpected(
-  response: DeleteClassificationPolicy204Response | DeleteClassificationPolicyDefaultResponse,
+  response:
+    | DeleteClassificationPolicy204Response
+    | DeleteClassificationPolicyDefaultResponse
 ): response is DeleteClassificationPolicyDefaultResponse;
 export function isUnexpected(
-  response: ListClassificationPolicies200Response | ListClassificationPoliciesDefaultResponse,
+  response:
+    | ListClassificationPolicies200Response
+    | ListClassificationPoliciesDefaultResponse
 ): response is ListClassificationPoliciesDefaultResponse;
 export function isUnexpected(
   response:
     | UpsertDistributionPolicy200Response
     | UpsertDistributionPolicy201Response
-    | UpsertDistributionPolicyDefaultResponse,
+    | UpsertDistributionPolicyDefaultResponse
 ): response is UpsertDistributionPolicyDefaultResponse;
 export function isUnexpected(
-  response: GetDistributionPolicy200Response | GetDistributionPolicyDefaultResponse,
+  response:
+    | GetDistributionPolicy200Response
+    | GetDistributionPolicyDefaultResponse
 ): response is GetDistributionPolicyDefaultResponse;
 export function isUnexpected(
-  response: DeleteDistributionPolicy204Response | DeleteDistributionPolicyDefaultResponse,
+  response:
+    | DeleteDistributionPolicy204Response
+    | DeleteDistributionPolicyDefaultResponse
 ): response is DeleteDistributionPolicyDefaultResponse;
 export function isUnexpected(
-  response: ListDistributionPolicies200Response | ListDistributionPoliciesDefaultResponse,
+  response:
+    | ListDistributionPolicies200Response
+    | ListDistributionPoliciesDefaultResponse
 ): response is ListDistributionPoliciesDefaultResponse;
 export function isUnexpected(
   response:
     | UpsertExceptionPolicy200Response
     | UpsertExceptionPolicy201Response
-    | UpsertExceptionPolicyDefaultResponse,
+    | UpsertExceptionPolicyDefaultResponse
 ): response is UpsertExceptionPolicyDefaultResponse;
 export function isUnexpected(
-  response: GetExceptionPolicy200Response | GetExceptionPolicyDefaultResponse,
+  response: GetExceptionPolicy200Response | GetExceptionPolicyDefaultResponse
 ): response is GetExceptionPolicyDefaultResponse;
 export function isUnexpected(
-  response: DeleteExceptionPolicy204Response | DeleteExceptionPolicyDefaultResponse,
+  response:
+    | DeleteExceptionPolicy204Response
+    | DeleteExceptionPolicyDefaultResponse
 ): response is DeleteExceptionPolicyDefaultResponse;
 export function isUnexpected(
-  response: ListExceptionPolicies200Response | ListExceptionPoliciesDefaultResponse,
+  response:
+    | ListExceptionPolicies200Response
+    | ListExceptionPoliciesDefaultResponse
 ): response is ListExceptionPoliciesDefaultResponse;
 export function isUnexpected(
-  response: UpsertQueue200Response | UpsertQueue201Response | UpsertQueueDefaultResponse,
+  response:
+    | UpsertQueue200Response
+    | UpsertQueue201Response
+    | UpsertQueueDefaultResponse
 ): response is UpsertQueueDefaultResponse;
 export function isUnexpected(
-  response: GetQueue200Response | GetQueueDefaultResponse,
+  response: GetQueue200Response | GetQueueDefaultResponse
 ): response is GetQueueDefaultResponse;
 export function isUnexpected(
-  response: DeleteQueue204Response | DeleteQueueDefaultResponse,
+  response: DeleteQueue204Response | DeleteQueueDefaultResponse
 ): response is DeleteQueueDefaultResponse;
 export function isUnexpected(
-  response: ListQueues200Response | ListQueuesDefaultResponse,
+  response: ListQueues200Response | ListQueuesDefaultResponse
 ): response is ListQueuesDefaultResponse;
 export function isUnexpected(
-  response: UpsertJob200Response | UpsertJob201Response | UpsertJobDefaultResponse,
+  response:
+    | UpsertJob200Response
+    | UpsertJob201Response
+    | UpsertJobDefaultResponse
 ): response is UpsertJobDefaultResponse;
 export function isUnexpected(
-  response: GetJob200Response | GetJobDefaultResponse,
+  response: GetJob200Response | GetJobDefaultResponse
 ): response is GetJobDefaultResponse;
 export function isUnexpected(
-  response: DeleteJob204Response | DeleteJobDefaultResponse,
+  response: DeleteJob204Response | DeleteJobDefaultResponse
 ): response is DeleteJobDefaultResponse;
 export function isUnexpected(
-  response: Reclassify200Response | ReclassifyDefaultResponse,
+  response: Reclassify200Response | ReclassifyDefaultResponse
 ): response is ReclassifyDefaultResponse;
 export function isUnexpected(
-  response: Cancel200Response | CancelDefaultResponse,
+  response: Cancel200Response | CancelDefaultResponse
 ): response is CancelDefaultResponse;
 export function isUnexpected(
-  response: Complete200Response | CompleteDefaultResponse,
+  response: Complete200Response | CompleteDefaultResponse
 ): response is CompleteDefaultResponse;
 export function isUnexpected(
-  response: Close200Response | CloseDefaultResponse,
+  response: Close200Response | CloseDefaultResponse
 ): response is CloseDefaultResponse;
 export function isUnexpected(
-  response: ListJobs200Response | ListJobsDefaultResponse,
+  response: ListJobs200Response | ListJobsDefaultResponse
 ): response is ListJobsDefaultResponse;
 export function isUnexpected(
-  response: GetInQueuePosition200Response | GetInQueuePositionDefaultResponse,
+  response: GetInQueuePosition200Response | GetInQueuePositionDefaultResponse
 ): response is GetInQueuePositionDefaultResponse;
 export function isUnexpected(
-  response: Unassign200Response | UnassignDefaultResponse,
+  response: Unassign200Response | UnassignDefaultResponse
 ): response is UnassignDefaultResponse;
 export function isUnexpected(
-  response: Accept200Response | AcceptDefaultResponse,
+  response: Accept200Response | AcceptDefaultResponse
 ): response is AcceptDefaultResponse;
 export function isUnexpected(
-  response: Decline200Response | DeclineDefaultResponse,
+  response: Decline200Response | DeclineDefaultResponse
 ): response is DeclineDefaultResponse;
 export function isUnexpected(
-  response: GetQueueStatistics200Response | GetQueueStatisticsDefaultResponse,
+  response: GetQueueStatistics200Response | GetQueueStatisticsDefaultResponse
 ): response is GetQueueStatisticsDefaultResponse;
 export function isUnexpected(
-  response: UpsertWorker200Response | UpsertWorker201Response | UpsertWorkerDefaultResponse,
+  response:
+    | UpsertWorker200Response
+    | UpsertWorker201Response
+    | UpsertWorkerDefaultResponse
 ): response is UpsertWorkerDefaultResponse;
 export function isUnexpected(
-  response: GetWorker200Response | GetWorkerDefaultResponse,
+  response: GetWorker200Response | GetWorkerDefaultResponse
 ): response is GetWorkerDefaultResponse;
 export function isUnexpected(
-  response: DeleteWorker204Response | DeleteWorkerDefaultResponse,
+  response: DeleteWorker204Response | DeleteWorkerDefaultResponse
 ): response is DeleteWorkerDefaultResponse;
 export function isUnexpected(
-  response: ListWorkers200Response | ListWorkersDefaultResponse,
+  response: ListWorkers200Response | ListWorkersDefaultResponse
 ): response is ListWorkersDefaultResponse;
 export function isUnexpected(
   response:
@@ -293,7 +321,7 @@ export function isUnexpected(
     | DeleteWorker204Response
     | DeleteWorkerDefaultResponse
     | ListWorkers200Response
-    | ListWorkersDefaultResponse,
+    | ListWorkersDefaultResponse
 ): response is
   | UpsertClassificationPolicyDefaultResponse
   | GetClassificationPolicyDefaultResponse
@@ -360,17 +388,24 @@ function getParametrizedPathSuccess(method: string, path: string): string[] {
 
     // track if we have found a match to return the values found.
     let found = true;
-    for (let i = candidateParts.length - 1, j = pathParts.length - 1; i >= 1 && j >= 1; i--, j--) {
-      if (candidateParts[i]?.startsWith("{") && candidateParts[i]?.indexOf("}") !== -1) {
+    for (
+      let i = candidateParts.length - 1, j = pathParts.length - 1;
+      i >= 1 && j >= 1;
+      i--, j--
+    ) {
+      if (
+        candidateParts[i]?.startsWith("{") &&
+        candidateParts[i]?.indexOf("}") !== -1
+      ) {
         const start = candidateParts[i]!.indexOf("}") + 1,
           end = candidateParts[i]?.length;
         // If the current part of the candidate is a "template" part
         // Try to use the suffix of pattern to match the path
         // {guid} ==> $
         // {guid}:export ==> :export$
-        const isMatched = new RegExp(`${candidateParts[i]?.slice(start, end)}`).test(
-          pathParts[j] || "",
-        );
+        const isMatched = new RegExp(
+          `${candidateParts[i]?.slice(start, end)}`
+        ).test(pathParts[j] || "");
 
         if (!isMatched) {
           found = false;
