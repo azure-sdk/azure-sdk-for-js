2b07576f7bb54a40287a4e3b2d5c75e9eb849c36
SDKAuto <sdkautomation@microsoft.com>
2024-01-29 19:36:58 +0000

    CodeGen from PR 27452 in Azure/azure-rest-api-specs Merge b68ecbe886fddf02d1a6f623cb7ad02b1f8f7716 into faafd9ca89e45cdc7f590942a90ae6138b4f3de1

---
+++ sdk/deid/azure-healthdataaiservices-deid-rest/src/outputModels.ts
@@ -0,0 +1,172 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { Paged } from "@azure/core-paging";
+
+/** A job containing a batch of documents to deidentify. */
+export interface JobOutput {
+  /** The name of a job. */
+  readonly name: string;
+  /**
+   * Current status of a job.
+   *
+   * Possible values: "NotStarted", "Running", "Succeeded", "Failed", "Canceled", "PartialFailed"
+   */
+  readonly status?: string;
+  /** Summary of a job. Exists only when the job is completed. */
+  readonly summary?: JobSummaryOutput;
+  /** Date and time when the job was created. */
+  readonly createdAt?: string;
+  /** Date and time when the job was started. */
+  readonly startedAt?: string;
+  /**
+   * Date and time when the job was completed.
+   *
+   * If the job is canceled, this is the time when the job was canceled.
+   *
+   * If the job failed, this is the time when the job failed.
+   */
+  readonly completedAt?: string;
+  /** Storage location to perform the operation on. */
+  sourceLocation: SourceStorageLocationOutput;
+  /** Target location to store output of operation. */
+  targetLocation: TargetStorageLocationOutput;
+  /**
+   * Data type of the input documents.
+   *
+   * Possible values: "Unknown", "PlainText"
+   */
+  dataType: string;
+  /**
+   * Operation to perform on the input documents.
+   *
+   * Possible values: "Redact", "Surrogate", "Tag"
+   */
+  operation: string;
+  /** Format of the redacted output. Only valid when Operation is Redact. */
+  redactionFormat?: string;
+}
+
+/** Summary metrics the documents pertaining to a job. */
+export interface JobSummaryOutput {
+  /** Number of blobs that have completed. */
+  successful: number;
+  /** Number of blobs that have failed. */
+  failed: number;
+  /** Number of blobs that have been canceled. */
+  canceled: number;
+  /** Number of blobs total. */
+  total: number;
+  /** Number of bytes processed. */
+  bytesProcessed: number;
+}
+
+/** Storage location. */
+export interface SourceStorageLocationOutput {
+  /** URL to storage location. Must be a valid Azure Storage SAS URI. */
+  location: string;
+  /** Prefix to filter blobs by. */
+  prefix: string;
+  /** List of extensions to filter blobs by. */
+  extensions?: string[];
+}
+
+/** Storage location. */
+export interface TargetStorageLocationOutput {
+  /** URL to storage location. Must be a valid Azure Storage SAS URI. */
+  location: string;
+  /** Prefix to filter blobs by. */
+  prefix: string;
+}
+
+/** Provides the 'x-ms-client-request-id' header to enable request correlation in requests and responses. */
+export interface ClientRequestIdHeaderOutput {}
+
+/** File report once job has completed. */
+export interface FileOutput {
+  /** Relative path to file from location prefix. */
+  readonly path: string;
+  /**
+   * Status of the file.
+   *
+   * Possible values: "NotStarted", "Running", "Succeeded", "Failed", "Canceled"
+   */
+  status: string;
+  /** Explanation of current status. If needed. */
+  explanation?: string;
+  /** The entity tag for this resource. */
+  readonly etag: string;
+}
+
+/** Request for synchronous DeID operation. */
+export interface DeidRequestOutput {
+  /** Input text to deidentify. */
+  inputText: string;
+  /**
+   * Data type of the input.
+   *
+   * Possible values: "Unknown", "PlainText"
+   */
+  dataType: string;
+  /**
+   * Operation to perform on the input.
+   *
+   * Possible values: "Redact", "Surrogate", "Tag"
+   */
+  operation: string;
+  /**
+   * Requested Encoding of the tag response indices.
+   *
+   * Possible values: "TextElement_v8", "UnicodeCodePoint", "Utf16CodeUnit"
+   */
+  stringIndexType?: string;
+  /** Format of the redacted output. Only valid when OperationType is Redact. */
+  redactionFormat?: string;
+}
+
+/** Response for synchronous DeID operation. */
+export interface DeidResponseOutput {
+  /** Output text after deidentification. */
+  outputText: string;
+  /** Result of the tag operation. */
+  taggerResult?: PhiTaggerResultOutput;
+}
+
+/** Result of the tag operation. */
+export interface PhiTaggerResultOutput {
+  /** List of entities detected in the input. */
+  entities: Array<PhiEntityOutput>;
+  /**
+   * Requested Encoding of the tag response indices.
+   *
+   * Possible values: "TextElement_v8", "UnicodeCodePoint", "Utf16CodeUnit"
+   */
+  stringIndexType: string;
+  /** Path to the file in the storage container. */
+  path?: string;
+  /** Checksum of the file. */
+  checksum?: string;
+}
+
+/** Phi Entity tag in the input. */
+export interface PhiEntityOutput {
+  /**
+   * Phi Category of the entity.
+   *
+   * Possible values: "Unknown", "Account", "Age", "BioID", "City", "CountryOrRegion", "Date", "Device", "Doctor", "Email", "Fax", "HealthPlan", "Hospital", "IDNum", "IPAddress", "License", "LocationOther", "MedicalRecord", "Organization", "Patient", "Phone", "Profession", "SocialSecurity", "State", "Street", "Url", "Username", "Vehicle", "Zip"
+   */
+  category: string;
+  /** Starting index of the location from within the input text. */
+  offset: number;
+  /** Length of the input text. */
+  length: number;
+  /** Text of the entity. */
+  text?: string;
+  /** Confidence score of the text/type pairing. */
+  confidenceScore?: number;
+}
+
+/** Paged collection of Job items */
+export type PagedJobOutput = Paged<JobOutput>;
+/** Paged collection of File items */
+export type PagedFileOutput = Paged<FileOutput>;
