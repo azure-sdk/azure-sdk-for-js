4c82b03bcb694749b7631f76aa8e90da5be0a988
SDKAuto <sdkautomation@microsoft.com>
2024-02-27 07:26:40 +0000

    CodeGen from PR 27936 in Azure/azure-rest-api-specs Merge c4ce221bc1f4d3c1b3ee03b19f397e240d82bf4e into a875e5d38a46b6c0b17350b9df807508ed03c411

---
+++ sdk/contosowidgetmanager/contosowidgetmanager-rest/src/models.ts
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export interface WidgetSuite {
+  /** The ID of the widget's manufacturer. */
+  manufacturerId: string;
+  /** The faked shared model. */
+  sharedModel?: FakedSharedModel;
+}
+
+/** Faked shared model */
+export interface FakedSharedModel {
+  /** The tag. */
+  tag: string;
+  /** The created date. */
+  createdAt: Date | string;
+}
