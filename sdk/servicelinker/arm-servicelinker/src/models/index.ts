29eed7e85bc1e55a648353f309d0f8043c1b2e70
SDKAuto <sdkautomation@microsoft.com>
2024-03-08 09:34:18 +0000

    CodeGen from PR 3224 in test-repo-billy/azure-rest-api-specs Merge 53e250267557fb3fe9931cacb31a2ff816abc2e1 into f82646fc1477ba3bfd90478e044e01bef3b8831a

--- sdk/servicelinker/arm-servicelinker/src/models/index.ts
+++ sdk/servicelinker/arm-servicelinker/src/models/index.ts
@@ -29,14 +29,6 @@ export type SecretInfoBaseUnion =
   | KeyVaultSecretReferenceSecretInfo
   | KeyVaultSecretUriSecretInfo;
 
-/** The list of Linker. */
-export interface LinkerList {
-  /** The link used to get the next page of Linker list. */
-  nextLink?: string;
-  /** The list of Linkers. */
-  value?: LinkerResource[];
-}
-
 /** The target service properties */
 export interface TargetServiceBase {
   /** Polymorphic discriminator, which specifies the different types this object can be */
@@ -302,6 +294,14 @@ export interface SecretInfoBase {
   secretType: "rawValue" | "keyVaultSecretReference" | "keyVaultSecretUri";
 }
 
+/** The list of Linker. */
+export interface LinkerList {
+  /** The link used to get the next page of Linker list. */
+  nextLink?: string;
+  /** The list of Linkers. */
+  value?: LinkerResource[];
+}
+
 /** The azure resource info when target service type is AzureResource */
 export interface AzureResource extends TargetServiceBase {
   /** Polymorphic discriminator, which specifies the different types this object can be */
@@ -448,7 +448,7 @@ export enum KnownTargetServiceType {
   /** ConfluentBootstrapServer */
   ConfluentBootstrapServer = "ConfluentBootstrapServer",
   /** ConfluentSchemaRegistry */
-  ConfluentSchemaRegistry = "ConfluentSchemaRegistry"
+  ConfluentSchemaRegistry = "ConfluentSchemaRegistry",
 }
 
 /**
@@ -473,7 +473,7 @@ export enum KnownAuthType {
   /** ServicePrincipalCertificate */
   ServicePrincipalCertificate = "servicePrincipalCertificate",
   /** Secret */
-  Secret = "secret"
+  Secret = "secret",
 }
 
 /**
@@ -510,7 +510,9 @@ export enum KnownClientType {
   /** Nodejs */
   Nodejs = "nodejs",
   /** SpringBoot */
-  SpringBoot = "springBoot"
+  SpringBoot = "springBoot",
+  /** KafkaSpringBoot */
+  KafkaSpringBoot = "kafka-springBoot",
 }
 
 /**
@@ -527,7 +529,8 @@ export enum KnownClientType {
  * **ruby** \
  * **django** \
  * **nodejs** \
- * **springBoot**
+ * **springBoot** \
+ * **kafka-springBoot**
  */
 export type ClientType = string;
 
@@ -536,7 +539,7 @@ export enum KnownVNetSolutionType {
   /** ServiceEndpoint */
   ServiceEndpoint = "serviceEndpoint",
   /** PrivateLink */
-  PrivateLink = "privateLink"
+  PrivateLink = "privateLink",
 }
 
 /**
@@ -558,7 +561,7 @@ export enum KnownCreatedByType {
   /** ManagedIdentity */
   ManagedIdentity = "ManagedIdentity",
   /** Key */
-  Key = "Key"
+  Key = "Key",
 }
 
 /**
@@ -580,7 +583,7 @@ export enum KnownValidationResultStatus {
   /** Failure */
   Failure = "failure",
   /** Warning */
-  Warning = "warning"
+  Warning = "warning",
 }
 
 /**
@@ -601,7 +604,7 @@ export enum KnownOrigin {
   /** System */
   System = "system",
   /** UserSystem */
-  UserSystem = "user,system"
+  UserSystem = "user,system",
 }
 
 /**
@@ -618,7 +621,7 @@ export type Origin = string;
 /** Known values of {@link ActionType} that the service accepts. */
 export enum KnownActionType {
   /** Internal */
-  Internal = "Internal"
+  Internal = "Internal",
 }
 
 /**
@@ -633,7 +636,7 @@ export type ActionType = string;
 /** Known values of {@link AzureResourceType} that the service accepts. */
 export enum KnownAzureResourceType {
   /** KeyVault */
-  KeyVault = "KeyVault"
+  KeyVault = "KeyVault",
 }
 
 /**
@@ -652,7 +655,7 @@ export enum KnownSecretType {
   /** KeyVaultSecretUri */
   KeyVaultSecretUri = "keyVaultSecretUri",
   /** KeyVaultSecretReference */
-  KeyVaultSecretReference = "keyVaultSecretReference"
+  KeyVaultSecretReference = "keyVaultSecretReference",
 }
 
 /**
@@ -665,12 +668,6 @@ export enum KnownSecretType {
  * **keyVaultSecretReference**
  */
 export type SecretType = string;
-
-/** Optional parameters. */
-export interface LinkerListOptionalParams extends coreClient.OperationOptions {}
-
-/** Contains response data for the list operation. */
-export type LinkerListResponse = LinkerList;
 
 /** Optional parameters. */
 export interface LinkerGetOptionalParams extends coreClient.OperationOptions {}
@@ -729,13 +726,6 @@ export interface LinkerListConfigurationsOptionalParams
 
 /** Contains response data for the listConfigurations operation. */
 export type LinkerListConfigurationsResponse = SourceConfigurationResult;
-
-/** Optional parameters. */
-export interface LinkerListNextOptionalParams
-  extends coreClient.OperationOptions {}
-
-/** Contains response data for the listNext operation. */
-export type LinkerListNextResponse = LinkerList;
 
 /** Optional parameters. */
 export interface OperationsListOptionalParams
