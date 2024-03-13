022ad4a36d45f410b3a548daa0dcad5b7a0e0750
SDKAuto <sdkautomation@microsoft.com>
2024-03-13 04:52:11 +0000

    CodeGen from PR 28225 in Azure/azure-rest-api-specs Merge 37b98fa04d7de2d87d600ffab2507443a373a1b1 into 95676e11697075eb0fe1e0dcba6e35804810dc5a

--- sdk/containerservice/arm-containerservice/src/models/parameters.ts
+++ sdk/containerservice/arm-containerservice/src/models/parameters.ts
@@ -54,7 +54,7 @@ export const $host: OperationURLParameter = {
 export const apiVersion: OperationQueryParameter = {
   parameterPath: "apiVersion",
   mapper: {
-    defaultValue: "2023-11-02-preview",
+    defaultValue: "2024-02-02-preview",
     isConstant: true,
     serializedName: "api-version",
     type: {
