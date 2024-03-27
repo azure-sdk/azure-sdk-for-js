fc1ff8c55436f2f18983393cf6bcd2c673cc0b27
SDKAuto <sdkautomation@microsoft.com>
2024-03-27 18:46:27 +0000

    CodeGen from PR 28349 in Azure/azure-rest-api-specs Merge 8464c4a9ecca6f474bb931f6dced731af9463a3a into e64ac2b3e4e255ac5f8f49359eac153f1cdae2da

---
+++ sdk/deid/azure-healthdataaiservices-deid-rest/src/index.ts
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import DeidentificationClient from "./deidentificationClient";
+
+export * from "./deidentificationClient";
+export * from "./parameters";
+export * from "./responses";
+export * from "./clientDefinitions";
+export * from "./isUnexpected";
+export * from "./models";
+export * from "./outputModels";
+export * from "./paginateHelper";
+export * from "./pollingHelper";
+
+export default DeidentificationClient;
