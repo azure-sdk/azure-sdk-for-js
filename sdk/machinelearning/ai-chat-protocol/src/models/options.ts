a4b9f42f854a895c82488fe8b437bc1c1f3eed4c
SDKAuto <sdkautomation@microsoft.com>
2024-05-14 05:46:01 +0000

    CodeGen from PR 3349 in test-repo-billy/azure-rest-api-specs Merge 935e8d5c4e3382f90879a6b914dabfdeb9b5a448 into 9789b00f225d650cdf2c6623967095ea21614520

---
+++ sdk/machinelearning/ai-chat-protocol/src/models/options.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { OperationOptions } from "@azure-rest/core-client";
+
+export interface ChatCreateStreamingOptionalParams extends OperationOptions {}
+
+export interface ChatCreateOptionalParams extends OperationOptions {}
