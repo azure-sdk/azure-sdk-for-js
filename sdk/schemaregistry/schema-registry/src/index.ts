254b630c98f4d4149d954761b9a8b5a7c42e2f21
SDKAuto <sdkautomation@microsoft.com>
2024-06-14 09:41:07 +0000

    CodeGen from PR 29440 in Azure/azure-rest-api-specs Merge 495ee87e131eed7c14f3033a79d38449b223307a into 7bf13ef1c776f6d973b9645906281d0ec4660fc8

--- sdk/schemaregistry/schema-registry/src/index.ts
+++ sdk/schemaregistry/schema-registry/src/index.ts
@@ -1,5 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export * from "./models";
-export { SchemaRegistryClient } from "./schemaRegistryClient";
+import SchemaRegistryClient from "./schemaRegistryClient.js";
+
+export * from "./schemaRegistryClient.js";
+export * from "./parameters.js";
+export * from "./responses.js";
+export * from "./clientDefinitions.js";
+export * from "./isUnexpected.js";
+export * from "./models.js";
+export * from "./outputModels.js";
+export * from "./paginateHelper.js";
+
+export default SchemaRegistryClient;
