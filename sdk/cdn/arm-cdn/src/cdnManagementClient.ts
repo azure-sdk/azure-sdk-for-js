1b4d9bbb5222beda812e4be761c948dc601906ae
SDKAuto <sdkautomation@microsoft.com>
2024-06-28 06:42:18 +0000

    CodeGen from PR 29629 in Azure/azure-rest-api-specs Merge dafbb66d086ca156bd0b5f23e7a946dd2ce41431 into 00a6d4f6cdfe2e3ffd069d0771ee0a52143a13c0

--- sdk/cdn/arm-cdn/src/cdnManagementClient.ts
+++ sdk/cdn/arm-cdn/src/cdnManagementClient.ts
@@ -123,7 +123,7 @@ export class CdnManagementClient extends coreClient.ServiceClient {
       credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-cdn/9.1.1`;
+    const packageDetails = `azsdk-js-arm-cdn/10.0.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
