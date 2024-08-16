ea30167616c406be39a34e34156ed2d759184fbf
SDKAuto <sdkautomation@microsoft.com>
2024-08-16 15:48:23 +0000

    CodeGen from PR 29283 in Azure/azure-rest-api-specs Merge 42a342ab7af35772956c5d38831a7dac34a75bce into 0fb6ae0a038ffb740144971c119d4fb5cd426450

---
+++ sdk/ai/ai-inference-rest/src/api/index.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export {
+  createChatCompletions,
+  ModelContext,
+  ChatCompletionsClientOptionalParams,
+} from "./chatCompletionsContext.js";
+export { complete, getModelInfo } from "./operations.js";
