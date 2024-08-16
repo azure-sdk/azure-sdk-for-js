b025f904e82df12e508971fc9461490c7b3eebc2
SDKAuto <sdkautomation@microsoft.com>
2024-08-16 12:52:10 +0000

    CodeGen from PR 3461 in test-repo-billy/azure-rest-api-specs Merge ae371fa064dfef97d1ae6ed7d1d0ca24e23d11c1 into 2ad400fcd7d7055b38957f542ffbe1a72290023e

--- sdk/loadtesting/arm-loadtesting/src/index.ts
+++ sdk/loadtesting/arm-loadtesting/src/index.ts
@@ -9,5 +9,5 @@
 /// <reference lib="esnext.asynciterable" />
 export { getContinuationToken } from "./pagingHelper";
 export * from "./models";
-export { LoadTestClient } from "./loadTestClient";
+export { LoadTestClientForTesting } from "./loadTestClientForTesting";
 export * from "./operationsInterfaces";
