57cd02d7edc184308d4635b95db71e132af9e705
SDKAuto <sdkautomation@microsoft.com>
2024-08-22 09:05:37 +0000

    CodeGen from PR 3490 in test-repo-billy/azure-rest-api-specs Merge 10c3fc536deb5a700538e6afb4d77a76f4dbcc4e into c221294c903b4bcbe7b1ee4bce901322a7056943

--- sdk/migrationdiscovery/arm-migrationdiscoverysap/src/index.ts
+++ sdk/migrationdiscovery/arm-migrationdiscoverysap/src/index.ts
@@ -9,5 +9,5 @@
 /// <reference lib="esnext.asynciterable" />
 export { getContinuationToken } from "./pagingHelper";
 export * from "./models";
-export { WorkloadsClient } from "./workloadsClient";
+export { WorkloadsClientForTesting } from "./workloadsClientForTesting";
 export * from "./operationsInterfaces";
