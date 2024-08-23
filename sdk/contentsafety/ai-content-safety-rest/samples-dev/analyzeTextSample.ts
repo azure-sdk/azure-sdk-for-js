f8464f55dac050e61b3045abec84081993c2bcd3
SDKAuto <sdkautomation@microsoft.com>
2024-08-23 08:07:24 +0000

    CodeGen from PR 3485 in test-repo-billy/azure-rest-api-specs Merge 8a902f8809c8533025263b1641caf63032db8e5b into c221294c903b4bcbe7b1ee4bce901322a7056943

---
+++ sdk/contentsafety/ai-content-safety-rest/samples-dev/analyzeTextSample.ts
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import createContentSafetyClient from "@azure-rest/ai-content-safety";
+import { AzureKeyCredential } from "@azure/core-auth";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+/**
+ * This sample demonstrates how to call operation AnalyzeText
+ *
+ * @summary call operation AnalyzeText
+ */
+async function analyzeTextSample() {
+  const endpointParam = "{Your endpointParam}";
+  const credential = new AzureKeyCredential("{Your API key}");
+  const client = createContentSafetyClient(endpointParam, credential);
+  const result = await client
+    .path("/text:analyze")
+    .post({
+      body: {
+        text: "{Your text}",
+        categories: ["Hate"],
+        blocklistNames: ["{Your blocklistNames}"],
+        haltOnBlocklistHit: true,
+        outputType: "FourSeverityLevels",
+      },
+    });
+  console.log(result);
+}
+
+async function main() {
+  analyzeTextSample();
+}
+
+main().catch(console.error);
