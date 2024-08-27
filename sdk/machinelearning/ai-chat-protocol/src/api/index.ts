ba2912c83d756802f2a5fc3c72f4a2337b8bc2cb
SDKAuto <sdkautomation@microsoft.com>
2024-08-27 04:50:04 +0000

    CodeGen from PR 3461 in test-repo-billy/azure-rest-api-specs Merge c800dad7162baede2debc40d848ad348a8c7df56 into 2b91022c223ed3618c268c750f831b76fe0f4fb5

---
+++ sdk/machinelearning/ai-chat-protocol/src/api/index.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export {
+  createChatProtocol,
+  ChatProtocolContext,
+  ChatProtocolClientOptionalParams,
+} from "./chatProtocolContext.js";
