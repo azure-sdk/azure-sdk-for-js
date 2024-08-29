5d861eea0f6905673598250d237a1d07faff3e44
SDKAuto <sdkautomation@microsoft.com>
2024-08-29 02:14:48 +0000

    CodeGen from PR 3460 in test-repo-billy/azure-rest-api-specs Merge e9a1b8932dd08a9c4b181c32edddab799c19ea8e into 2b91022c223ed3618c268c750f831b76fe0f4fb5

--- sdk/containerservice/arm-containerservicefleet/src/operations/fleetMembers.ts
+++ sdk/containerservice/arm-containerservicefleet/src/operations/fleetMembers.ts
@@ -12,7 +12,7 @@ import { FleetMembers } from "../operationsInterfaces";
 import * as coreClient from "@azure/core-client";
 import * as Mappers from "../models/mappers";
 import * as Parameters from "../models/parameters";
-import { ContainerServiceFleetClient } from "../containerServiceFleetClient";
+import { ContainerServiceFleetClientTest } from "../containerServiceFleetClientTest";
 import {
   SimplePollerLike,
   OperationState,
@@ -38,13 +38,13 @@ import {
 /// <reference lib="esnext.asynciterable" />
 /** Class containing FleetMembers operations. */
 export class FleetMembersImpl implements FleetMembers {
-  private readonly client: ContainerServiceFleetClient;
+  private readonly client: ContainerServiceFleetClientTest;
 
   /**
    * Initialize a new instance of the class FleetMembers class.
    * @param client Reference to the service client
    */
-  constructor(client: ContainerServiceFleetClient) {
+  constructor(client: ContainerServiceFleetClientTest) {
     this.client = client;
   }
 
