df479ff8e76130cc9a305402e67187060dfd6c16
SDKAuto <sdkautomation@microsoft.com>
2024-08-30 21:33:42 +0000

    CodeGen from PR 30189 in Azure/azure-rest-api-specs Merge d96ac18f46f73fe975aff34a0f8cdc9fd75e46f1 into e79d9ef3e065f2dcb6bd1db51e29c62a99dff5cb

--- sdk/appconfiguration/arm-appconfiguration/src/appConfigurationManagementClient.ts
+++ sdk/appconfiguration/arm-appconfiguration/src/appConfigurationManagementClient.ts
@@ -11,7 +11,7 @@ import * as coreRestPipeline from "@azure/core-rest-pipeline";
 import {
   PipelineRequest,
   PipelineResponse,
-  SendRequest
+  SendRequest,
 } from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
 import {
@@ -20,7 +20,9 @@ import {
   PrivateEndpointConnectionsImpl,
   PrivateLinkResourcesImpl,
   KeyValuesImpl,
-  ReplicasImpl
+  ReplicasImpl,
+  SnapshotsImpl,
+  NetworkSecurityPerimeterConfigurationsImpl,
 } from "./operations";
 import {
   ConfigurationStores,
@@ -28,7 +30,9 @@ import {
   PrivateEndpointConnections,
   PrivateLinkResources,
   KeyValues,
-  Replicas
+  Replicas,
+  Snapshots,
+  NetworkSecurityPerimeterConfigurations,
 } from "./operationsInterfaces";
 import { AppConfigurationManagementClientOptionalParams } from "./models";
 
@@ -40,13 +44,13 @@ export class AppConfigurationManagementClient extends coreClient.ServiceClient {
   /**
    * Initializes a new instance of the AppConfigurationManagementClient class.
    * @param credentials Subscription credentials which uniquely identify client subscription.
-   * @param subscriptionId The Microsoft Azure subscription ID.
+   * @param subscriptionId The ID of the target subscription. The value must be an UUID.
    * @param options The parameter options
    */
   constructor(
     credentials: coreAuth.TokenCredential,
     subscriptionId: string,
-    options?: AppConfigurationManagementClientOptionalParams
+    options?: AppConfigurationManagementClientOptionalParams,
   ) {
     if (credentials === undefined) {
       throw new Error("'credentials' cannot be null");
@@ -61,10 +65,10 @@ export class AppConfigurationManagementClient extends coreClient.ServiceClient {
     }
     const defaults: AppConfigurationManagementClientOptionalParams = {
       requestContentType: "application/json; charset=utf-8",
-      credential: credentials
+      credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-appconfiguration/4.0.1`;
+    const packageDetails = `azsdk-js-arm-appconfiguration/5.0.0-beta.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -74,20 +78,21 @@ export class AppConfigurationManagementClient extends coreClient.ServiceClient {
       ...defaults,
       ...options,
       userAgentOptions: {
-        userAgentPrefix
+        userAgentPrefix,
       },
       endpoint:
-        options.endpoint ?? options.baseUri ?? "https://management.azure.com"
+        options.endpoint ?? options.baseUri ?? "https://management.azure.com",
     };
     super(optionsWithDefaults);
 
     let bearerTokenAuthenticationPolicyFound: boolean = false;
     if (options?.pipeline && options.pipeline.getOrderedPolicies().length > 0) {
-      const pipelinePolicies: coreRestPipeline.PipelinePolicy[] = options.pipeline.getOrderedPolicies();
+      const pipelinePolicies: coreRestPipeline.PipelinePolicy[] =
+        options.pipeline.getOrderedPolicies();
       bearerTokenAuthenticationPolicyFound = pipelinePolicies.some(
         (pipelinePolicy) =>
           pipelinePolicy.name ===
-          coreRestPipeline.bearerTokenAuthenticationPolicyName
+          coreRestPipeline.bearerTokenAuthenticationPolicyName,
       );
     }
     if (
@@ -97,7 +102,7 @@ export class AppConfigurationManagementClient extends coreClient.ServiceClient {
       !bearerTokenAuthenticationPolicyFound
     ) {
       this.pipeline.removePolicy({
-        name: coreRestPipeline.bearerTokenAuthenticationPolicyName
+        name: coreRestPipeline.bearerTokenAuthenticationPolicyName,
       });
       this.pipeline.addPolicy(
         coreRestPipeline.bearerTokenAuthenticationPolicy({
@@ -107,9 +112,9 @@ export class AppConfigurationManagementClient extends coreClient.ServiceClient {
             `${optionsWithDefaults.endpoint}/.default`,
           challengeCallbacks: {
             authorizeRequestOnChallenge:
-              coreClient.authorizeRequestOnClaimChallenge
-          }
-        })
+              coreClient.authorizeRequestOnClaimChallenge,
+          },
+        }),
       );
     }
     // Parameter assignments
@@ -117,13 +122,16 @@ export class AppConfigurationManagementClient extends coreClient.ServiceClient {
 
     // Assigning values to Constant parameters
     this.$host = options.$host || "https://management.azure.com";
-    this.apiVersion = options.apiVersion || "2023-03-01";
+    this.apiVersion = options.apiVersion || "2024-07-01-preview";
     this.configurationStores = new ConfigurationStoresImpl(this);
     this.operations = new OperationsImpl(this);
     this.privateEndpointConnections = new PrivateEndpointConnectionsImpl(this);
     this.privateLinkResources = new PrivateLinkResourcesImpl(this);
     this.keyValues = new KeyValuesImpl(this);
     this.replicas = new ReplicasImpl(this);
+    this.snapshots = new SnapshotsImpl(this);
+    this.networkSecurityPerimeterConfigurations =
+      new NetworkSecurityPerimeterConfigurationsImpl(this);
     this.addCustomApiVersionPolicy(options.apiVersion);
   }
 
@@ -136,7 +144,7 @@ export class AppConfigurationManagementClient extends coreClient.ServiceClient {
       name: "CustomApiVersionPolicy",
       async sendRequest(
         request: PipelineRequest,
-        next: SendRequest
+        next: SendRequest,
       ): Promise<PipelineResponse> {
         const param = request.url.split("?");
         if (param.length > 1) {
@@ -150,7 +158,7 @@ export class AppConfigurationManagementClient extends coreClient.ServiceClient {
           request.url = param[0] + "?" + newParams.join("&");
         }
         return next(request);
-      }
+      },
     };
     this.pipeline.addPolicy(apiVersionPolicy);
   }
@@ -161,4 +169,6 @@ export class AppConfigurationManagementClient extends coreClient.ServiceClient {
   privateLinkResources: PrivateLinkResources;
   keyValues: KeyValues;
   replicas: Replicas;
+  snapshots: Snapshots;
+  networkSecurityPerimeterConfigurations: NetworkSecurityPerimeterConfigurations;
 }
