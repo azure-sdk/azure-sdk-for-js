446256adb0922385ade314b0f16acb77b93e483a
SDKAuto <sdkautomation@microsoft.com>
2024-09-02 02:15:02 +0000

    CodeGen from PR 3485 in test-repo-billy/azure-rest-api-specs Merge 132571e58c16b3f4fa5d7833351bfd983eede61d into 2b91022c223ed3618c268c750f831b76fe0f4fb5

---
+++ sdk/contentsafety/ai-content-safety-rest/samples-dev/deleteTextBlocklistSample.ts
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import createContentSafetyClient from "@azure-rest/ai-content-safety";
+import { AzureKeyCredential } from "@azure/core-auth";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+/**
+ * This sample demonstrates how to call operation DeleteTextBlocklist
+ *
+ * @summary call operation DeleteTextBlocklist
+ */
+async function deleteTextBlocklistSample() {
+  const endpointParam = "{Your endpointParam}";
+  const credential = new AzureKeyCredential("{Your API key}");
+  const client = createContentSafetyClient(endpointParam, credential);
+  const blocklistName = "{Your blocklistName}";
+  const result = await client
+    .path("/text/blocklists/{blocklistName}", blocklistName)
+    .delete();
+  console.log(result);
+}
+
+async function main() {
+  deleteTextBlocklistSample();
+}
+
+main().catch(console.error);
