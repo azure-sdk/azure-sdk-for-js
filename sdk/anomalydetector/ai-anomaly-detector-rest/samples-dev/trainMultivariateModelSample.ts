f77a4ff7309e740df99b6529d4e864a93eb4a117
SDKAuto <sdkautomation@microsoft.com>
2024-09-02 02:13:06 +0000

    CodeGen from PR 3485 in test-repo-billy/azure-rest-api-specs Merge 132571e58c16b3f4fa5d7833351bfd983eede61d into 2b91022c223ed3618c268c750f831b76fe0f4fb5

---
+++ sdk/anomalydetector/ai-anomaly-detector-rest/samples-dev/trainMultivariateModelSample.ts
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import createAnomalyDetectorClient from "@azure-rest/ai-anomaly-detector";
+import { AzureKeyCredential } from "@azure/core-auth";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+/**
+ * This sample demonstrates how to call operation TrainMultivariateModel
+ *
+ * @summary call operation TrainMultivariateModel
+ */
+async function trainMultivariateModelSample() {
+  const endpointParam = "{Your endpointParam}";
+  const credential = new AzureKeyCredential("{Your API key}");
+  const client = createAnomalyDetectorClient(endpointParam, credential);
+  const result = await client
+    .path("/multivariate/models")
+    .post({
+      body: {
+        dataSource: "{Your dataSource}",
+        dataSchema: "OneTable",
+        startTime: new Date(),
+        endTime: new Date(),
+        displayName: "{Your displayName}",
+        slidingWindow: 123,
+        alignPolicy: {
+          alignMode: "Inner",
+          fillNAMethod: "Previous",
+          paddingValue: 123,
+        },
+      },
+    });
+  console.log(result);
+}
+
+async function main() {
+  trainMultivariateModelSample();
+}
+
+main().catch(console.error);
