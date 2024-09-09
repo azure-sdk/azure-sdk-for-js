a0336ffea34258b50d9355f75dd484461bfb4d20
SDKAuto <sdkautomation@microsoft.com>
2024-09-09 04:10:51 +0000

    CodeGen from PR 3485 in test-repo-billy/azure-rest-api-specs Merge 132571e58c16b3f4fa5d7833351bfd983eede61d into 02e500856ce974e6873e205ffdb6523ffdc60bb7

---
+++ sdk/contentsafety/ai-content-safety-rest/samples-dev/removeBlocklistItemsSample.ts
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import createContentSafetyClient from "@azure-rest/ai-content-safety";
+import { AzureKeyCredential } from "@azure/core-auth";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+/**
+ * This sample demonstrates how to call operation RemoveBlocklistItems
+ *
+ * @summary call operation RemoveBlocklistItems
+ */
+async function removeBlocklistItemsSample() {
+  const endpointParam = "{Your endpointParam}";
+  const credential = new AzureKeyCredential("{Your API key}");
+  const client = createContentSafetyClient(endpointParam, credential);
+  const blocklistName = "{Your blocklistName}";
+  const result = await client
+    .path(
+      "/text/blocklists/{blocklistName}:removeBlocklistItems",
+      blocklistName,
+    )
+    .post({ body: { blocklistItemIds: ["{Your blocklistItemIds}"] } });
+  console.log(result);
+}
+
+async function main() {
+  removeBlocklistItemsSample();
+}
+
+main().catch(console.error);
