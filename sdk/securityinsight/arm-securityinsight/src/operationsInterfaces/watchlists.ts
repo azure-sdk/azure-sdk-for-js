0821406bcd450cdbf92d5cd9c55adf41a6fd41c3
SDKAuto <sdkautomation@microsoft.com>
2024-10-18 14:46:57 +0000

    CodeGen from PR 31040 in Azure/azure-rest-api-specs Merge 924dbd17eaa003371a7c6af78f427b0e91fd4e40 into ba7c89c5c27bb73d2afd6c1db05fd289256a7c9e

--- sdk/securityinsight/arm-securityinsight/src/operationsInterfaces/watchlists.ts
+++ sdk/securityinsight/arm-securityinsight/src/operationsInterfaces/watchlists.ts
@@ -7,6 +7,7 @@
  */
 
 import { PagedAsyncIterableIterator } from "@azure/core-paging";
+import { SimplePollerLike, OperationState } from "@azure/core-lro";
 import {
   Watchlist,
   WatchlistsListOptionalParams,
@@ -15,14 +16,14 @@ import {
   WatchlistsDeleteOptionalParams,
   WatchlistsDeleteResponse,
   WatchlistsCreateOrUpdateOptionalParams,
-  WatchlistsCreateOrUpdateResponse
+  WatchlistsCreateOrUpdateResponse,
 } from "../models";
 
 /// <reference lib="esnext.asynciterable" />
 /** Interface representing a Watchlists. */
 export interface Watchlists {
   /**
-   * Gets all watchlists, without watchlist items.
+   * Get all watchlists, without watchlist items.
    * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param workspaceName The name of the workspace.
    * @param options The options parameters.
@@ -30,52 +31,89 @@ export interface Watchlists {
   list(
     resourceGroupName: string,
     workspaceName: string,
-    options?: WatchlistsListOptionalParams
+    options?: WatchlistsListOptionalParams,
   ): PagedAsyncIterableIterator<Watchlist>;
   /**
-   * Gets a watchlist, without its watchlist items.
+   * Get a watchlist, without its watchlist items.
    * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param workspaceName The name of the workspace.
-   * @param watchlistAlias Watchlist Alias
+   * @param watchlistAlias The watchlist alias
    * @param options The options parameters.
    */
   get(
     resourceGroupName: string,
     workspaceName: string,
     watchlistAlias: string,
-    options?: WatchlistsGetOptionalParams
+    options?: WatchlistsGetOptionalParams,
   ): Promise<WatchlistsGetResponse>;
   /**
    * Delete a watchlist.
    * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param workspaceName The name of the workspace.
-   * @param watchlistAlias Watchlist Alias
+   * @param watchlistAlias The watchlist alias
    * @param options The options parameters.
    */
-  delete(
+  beginDelete(
     resourceGroupName: string,
     workspaceName: string,
     watchlistAlias: string,
-    options?: WatchlistsDeleteOptionalParams
+    options?: WatchlistsDeleteOptionalParams,
+  ): Promise<
+    SimplePollerLike<
+      OperationState<WatchlistsDeleteResponse>,
+      WatchlistsDeleteResponse
+    >
+  >;
+  /**
+   * Delete a watchlist.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
+   * @param workspaceName The name of the workspace.
+   * @param watchlistAlias The watchlist alias
+   * @param options The options parameters.
+   */
+  beginDeleteAndWait(
+    resourceGroupName: string,
+    workspaceName: string,
+    watchlistAlias: string,
+    options?: WatchlistsDeleteOptionalParams,
   ): Promise<WatchlistsDeleteResponse>;
   /**
    * Create or update a Watchlist and its Watchlist Items (bulk creation, e.g. through text/csv content
-   * type). To create a Watchlist and its Items, we should call this endpoint with either rawContent or a
-   * valid SAR URI and contentType properties. The rawContent is mainly used for small watchlist (content
-   * size below 3.8 MB). The SAS URI enables the creation of large watchlist, where the content size can
-   * go up to 500 MB. The status of processing such large file can be polled through the URL returned in
-   * Azure-AsyncOperation header.
+   * type). To create a Watchlist and its Items, we should call this endpoint with rawContent and
+   * contentType properties.
    * @param resourceGroupName The name of the resource group. The name is case insensitive.
    * @param workspaceName The name of the workspace.
-   * @param watchlistAlias Watchlist Alias
+   * @param watchlistAlias The watchlist alias
    * @param watchlist The watchlist
    * @param options The options parameters.
    */
-  createOrUpdate(
+  beginCreateOrUpdate(
     resourceGroupName: string,
     workspaceName: string,
     watchlistAlias: string,
     watchlist: Watchlist,
-    options?: WatchlistsCreateOrUpdateOptionalParams
+    options?: WatchlistsCreateOrUpdateOptionalParams,
+  ): Promise<
+    SimplePollerLike<
+      OperationState<WatchlistsCreateOrUpdateResponse>,
+      WatchlistsCreateOrUpdateResponse
+    >
+  >;
+  /**
+   * Create or update a Watchlist and its Watchlist Items (bulk creation, e.g. through text/csv content
+   * type). To create a Watchlist and its Items, we should call this endpoint with rawContent and
+   * contentType properties.
+   * @param resourceGroupName The name of the resource group. The name is case insensitive.
+   * @param workspaceName The name of the workspace.
+   * @param watchlistAlias The watchlist alias
+   * @param watchlist The watchlist
+   * @param options The options parameters.
+   */
+  beginCreateOrUpdateAndWait(
+    resourceGroupName: string,
+    workspaceName: string,
+    watchlistAlias: string,
+    watchlist: Watchlist,
+    options?: WatchlistsCreateOrUpdateOptionalParams,
   ): Promise<WatchlistsCreateOrUpdateResponse>;
 }
