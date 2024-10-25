30396d610dc72f96c55afdb3d600b92314d59e24
SDKAuto <sdkautomation@microsoft.com>
2024-10-25 22:11:59 +0000

    CodeGen from PR 31245 in Azure/azure-rest-api-specs Merge 4da786d6ceed7d6c7f53ebd207e50a6549991229 into e6094e6653921fca7b0b61fbc3e6e7c351e58093

---
+++ sdk/contoso/arm-contoso/src/models/index.ts
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  Resource,
+  SystemData,
+  KnownCreatedByType,
+  CreatedByType,
+  TrackedResource,
+  Employee,
+  EmployeeProperties,
+  KnownResourceProvisioningState,
+  ResourceProvisioningState,
+  ErrorResponse,
+  ErrorDetail,
+  ErrorAdditionalInfo,
+  EmployeeUpdate,
+  EmployeeUpdateProperties,
+  Operation,
+  OperationDisplay,
+  KnownOrigin,
+  Origin,
+  KnownActionType,
+  ActionType,
+  Versions,
+  ProvisioningState,
+} from "./models.js";
+export {
+  OperationsListOptionalParams,
+  EmployeesGetOptionalParams,
+  EmployeesCreateOrUpdateOptionalParams,
+  EmployeesUpdateOptionalParams,
+  EmployeesDeleteOptionalParams,
+  EmployeesListByResourceGroupOptionalParams,
+  EmployeesListBySubscriptionOptionalParams,
+} from "./options.js";
