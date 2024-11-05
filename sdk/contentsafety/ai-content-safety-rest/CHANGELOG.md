34a858f166a06e3d93691d1373df93bf6f4b5643
SDKAuto <sdkautomation@microsoft.com>
2024-11-05 09:18:36 +0000

    CodeGen from PR 3383 in test-repo-billy/azure-rest-api-specs Merge 12d12592e186a02f280c2f6a28d663d6d96e5aa2 into 7b5d4fa50d1365f0e981b995c60da3ac591c6bba

--- sdk/contentsafety/ai-content-safety-rest/CHANGELOG.md
+++ sdk/contentsafety/ai-content-safety-rest/CHANGELOG.md
@@ -1,56 +1,43 @@
 # Release History
-
-## 1.0.1 (Unreleased)
-
+    
+## 2.0.0 (2024-11-05)
+    
 ### Features Added
+
+  - Added operation group DetectTextProtectedMaterial
+  - Added operation group ShieldPrompt
+  - Added operation in Routes for path: "/text:shieldPrompt"
+  - Added operation in Routes for path: "/text:detectProtectedMaterial"
+  - Added Interface ContentSafetyClientOptions
+  - Added Interface DetectTextProtectedMaterial200Response
+  - Added Interface DetectTextProtectedMaterialResultOutput
+  - Added Interface DocumentInjectionAnalysisResultOutput
+  - Added Interface ShieldPrompt200Response
+  - Added Interface ShieldPromptBodyParam
+  - Added Interface ShieldPromptOptions
+  - Added Interface ShieldPromptResultOutput
+  - Added Interface TextProtectedMaterialAnalysisResultOutput
+  - Added Interface UserPromptInjectionAnalysisResultOutput
+  - Added Type Alias AnalyzeImageOutputType
+  - Added Type Alias AnalyzeTextOutputType
+  - Added Type Alias ImageCategory
+  - Added Type Alias ImageCategoryOutput
+  - Added Type Alias ShieldPromptParameters
+  - Added Type Alias TextCategory
+  - Added Type Alias TextCategoryOutput
+  - Interface TextBlocklistItem has a new optional parameter isRegex
+  - Interface TextBlocklistItemOutput has a new optional parameter isRegex
+  - Added function overload "export function isUnexpected(response: ShieldPrompt200Response | ShieldPromptDefaultResponse): response is ShieldPromptDefaultResponse;"
+  - Added function overload "export function isUnexpected(response: DetectTextProtectedMaterial200Response | DetectTextProtectedMaterialDefaultResponse): response is DetectTextProtectedMaterialDefaultResponse;"
 
 ### Breaking Changes
 
-### Bugs Fixed
-
-### Other Changes
-
-## 1.0.0 (2023-12-13)
-
-### Features Added
-
-- Support AAD Authentication
-- Support 8 severity levels output for Text Analyze
-
-### Breaking Changes
-
-Contract change for AnalyzeText, AnalyzeImage, Blocklist management related parameters
-
-#### AnalyzeText
-
-- AnalyzeTextOptions
-  - Renamed `breakByBlocklists` to `haltOnBlocklistHit`
-  - Add `outputType`
-- AnalyzeTextResultOutput
-  - Renamed `blocklistsMatchResults` to `blocklistsMatch`
-  - Replaced `TextAnalyzeSeverityResultOutput` by `TextCategoriesAnalysisOutput`
-
-#### AnalyzeImage
-
-- AnalyzeImageOptions
-  - Add `outputType`
-- AnalyzeImageResultOutput
-  - Replaced `ImageAnalyzeSeverityResultOutput` by `ImageCategoriesAnalysisOutput`
-
-#### Blocklist management
-
-- Renamed `"/text/blocklists/{blocklistName}:addBlockItems"` to `"/text/blocklists/{blocklistName}:addOrUpdateBlocklistItems"`
-- Renamed `"/text/blocklists/{blocklistName}:removeBlockItems"` to `"/text/blocklists/{blocklistName}:removeBlocklistItems"`
-- Renamed `"/text/blocklists/{blocklistName}/blockItems"` to `"/text/blocklists/{blocklistName}/blocklistItems"`
-- Renamed `"/text/blocklists/{blocklistName}/blockItems/{blockItemId}"` to `"/text/blocklists/{blocklistName}/blocklistItems/{blocklistItemId}"`
-- Renamed `AddBlockItemsOptions` to `AddOrUpdateTextBlocklistItemsOptions`
-  - Renamed `blockItems` to `blocklistItems`
-- Renamed `AddBlockItemsResultOutput` to `AddOrUpdateTextBlocklistItemsResultOutput`
-    - Renamed `value` to `blocklistItems`
-- Renamed `RemoveBlockItemsOptions` to `RemoveTextBlocklistItemsOptions`
-  - Renamed `blockItemIds` to `blocklistItemIds`
-- Renamed `TextBlockItemInfo` to `TextBlocklistItem`
-
-## 1.0.0-beta.1 (2023-09-28)
-
-- This is the initial beta release for Azure AI Content Safety, see README.md for details.
+  - Interface AnalyzeTextOptions no longer has parameter text
+  - Interface AnalyzeTextOptions has a new required parameter text2
+  - Type of parameter categories of interface AnalyzeImageOptions is changed from string[] to ImageCategory[]
+  - Type of parameter outputType of interface AnalyzeImageOptions is changed from string to AnalyzeImageOutputType
+  - Type of parameter categories of interface AnalyzeTextOptions is changed from string[] to TextCategory[]
+  - Type of parameter outputType of interface AnalyzeTextOptions is changed from string to AnalyzeTextOutputType
+  - Type of parameter category of interface ImageCategoriesAnalysisOutput is changed from string to ImageCategoryOutput
+  - Type of parameter category of interface TextCategoriesAnalysisOutput is changed from string to TextCategoryOutput
+    
--- sdk/contentsafety/ai-content-safety-rest/src/isUnexpected.ts
+++ sdk/contentsafety/ai-content-safety-rest/src/isUnexpected.ts
@@ -1,11 +1,15 @@
 // Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
+// Licensed under the MIT License.
 
 import {
-  AnalyzeText200Response,
-  AnalyzeTextDefaultResponse,
   AnalyzeImage200Response,
   AnalyzeImageDefaultResponse,
+  AnalyzeText200Response,
+  AnalyzeTextDefaultResponse,
+  ShieldPrompt200Response,
+  ShieldPromptDefaultResponse,
+  DetectTextProtectedMaterial200Response,
+  DetectTextProtectedMaterialDefaultResponse,
   GetTextBlocklist200Response,
   GetTextBlocklistDefaultResponse,
   CreateOrUpdateTextBlocklist200Response,
@@ -23,27 +27,39 @@ import {
   GetTextBlocklistItemDefaultResponse,
   ListTextBlocklistItems200Response,
   ListTextBlocklistItemsDefaultResponse,
-} from "./responses";
+} from "./responses.js";
 
 const responseMap: Record<string, string[]> = {
-  "POST /text:analyze": ["200"],
   "POST /image:analyze": ["200"],
+  "POST /text:analyze": ["200"],
+  "POST /text:shieldPrompt": ["200"],
+  "POST /text:detectProtectedMaterial": ["200"],
   "GET /text/blocklists/{blocklistName}": ["200"],
   "PATCH /text/blocklists/{blocklistName}": ["200", "201"],
   "DELETE /text/blocklists/{blocklistName}": ["204"],
   "GET /text/blocklists": ["200"],
   "POST /text/blocklists/{blocklistName}:addOrUpdateBlocklistItems": ["200"],
   "POST /text/blocklists/{blocklistName}:removeBlocklistItems": ["204"],
-  "GET /text/blocklists/{blocklistName}/blocklistItems/{blocklistItemId}": ["200"],
+  "GET /text/blocklists/{blocklistName}/blocklistItems/{blocklistItemId}": [
+    "200",
+  ],
   "GET /text/blocklists/{blocklistName}/blocklistItems": ["200"],
 };
 
 export function isUnexpected(
+  response: AnalyzeImage200Response | AnalyzeImageDefaultResponse,
+): response is AnalyzeImageDefaultResponse;
+export function isUnexpected(
   response: AnalyzeText200Response | AnalyzeTextDefaultResponse,
 ): response is AnalyzeTextDefaultResponse;
 export function isUnexpected(
-  response: AnalyzeImage200Response | AnalyzeImageDefaultResponse,
-): response is AnalyzeImageDefaultResponse;
+  response: ShieldPrompt200Response | ShieldPromptDefaultResponse,
+): response is ShieldPromptDefaultResponse;
+export function isUnexpected(
+  response:
+    | DetectTextProtectedMaterial200Response
+    | DetectTextProtectedMaterialDefaultResponse,
+): response is DetectTextProtectedMaterialDefaultResponse;
 export function isUnexpected(
   response: GetTextBlocklist200Response | GetTextBlocklistDefaultResponse,
 ): response is GetTextBlocklistDefaultResponse;
@@ -60,23 +76,35 @@ export function isUnexpected(
   response: ListTextBlocklists200Response | ListTextBlocklistsDefaultResponse,
 ): response is ListTextBlocklistsDefaultResponse;
 export function isUnexpected(
-  response: AddOrUpdateBlocklistItems200Response | AddOrUpdateBlocklistItemsDefaultResponse,
+  response:
+    | AddOrUpdateBlocklistItems200Response
+    | AddOrUpdateBlocklistItemsDefaultResponse,
 ): response is AddOrUpdateBlocklistItemsDefaultResponse;
 export function isUnexpected(
-  response: RemoveBlocklistItems204Response | RemoveBlocklistItemsDefaultResponse,
+  response:
+    | RemoveBlocklistItems204Response
+    | RemoveBlocklistItemsDefaultResponse,
 ): response is RemoveBlocklistItemsDefaultResponse;
 export function isUnexpected(
-  response: GetTextBlocklistItem200Response | GetTextBlocklistItemDefaultResponse,
+  response:
+    | GetTextBlocklistItem200Response
+    | GetTextBlocklistItemDefaultResponse,
 ): response is GetTextBlocklistItemDefaultResponse;
 export function isUnexpected(
-  response: ListTextBlocklistItems200Response | ListTextBlocklistItemsDefaultResponse,
+  response:
+    | ListTextBlocklistItems200Response
+    | ListTextBlocklistItemsDefaultResponse,
 ): response is ListTextBlocklistItemsDefaultResponse;
 export function isUnexpected(
   response:
-    | AnalyzeText200Response
-    | AnalyzeTextDefaultResponse
     | AnalyzeImage200Response
     | AnalyzeImageDefaultResponse
+    | AnalyzeText200Response
+    | AnalyzeTextDefaultResponse
+    | ShieldPrompt200Response
+    | ShieldPromptDefaultResponse
+    | DetectTextProtectedMaterial200Response
+    | DetectTextProtectedMaterialDefaultResponse
     | GetTextBlocklist200Response
     | GetTextBlocklistDefaultResponse
     | CreateOrUpdateTextBlocklist200Response
@@ -95,8 +123,10 @@ export function isUnexpected(
     | ListTextBlocklistItems200Response
     | ListTextBlocklistItemsDefaultResponse,
 ): response is
-  | AnalyzeTextDefaultResponse
   | AnalyzeImageDefaultResponse
+  | AnalyzeTextDefaultResponse
+  | ShieldPromptDefaultResponse
+  | DetectTextProtectedMaterialDefaultResponse
   | GetTextBlocklistDefaultResponse
   | CreateOrUpdateTextBlocklistDefaultResponse
   | DeleteTextBlocklistDefaultResponse
@@ -137,17 +167,24 @@ function getParametrizedPathSuccess(method: string, path: string): string[] {
 
     // track if we have found a match to return the values found.
     let found = true;
-    for (let i = candidateParts.length - 1, j = pathParts.length - 1; i >= 1 && j >= 1; i--, j--) {
-      if (candidateParts[i]?.startsWith("{") && candidateParts[i]?.indexOf("}") !== -1) {
+    for (
+      let i = candidateParts.length - 1, j = pathParts.length - 1;
+      i >= 1 && j >= 1;
+      i--, j--
+    ) {
+      if (
+        candidateParts[i]?.startsWith("{") &&
+        candidateParts[i]?.indexOf("}") !== -1
+      ) {
         const start = candidateParts[i]!.indexOf("}") + 1,
           end = candidateParts[i]?.length;
         // If the current part of the candidate is a "template" part
         // Try to use the suffix of pattern to match the path
         // {guid} ==> $
         // {guid}:export ==> :export$
-        const isMatched = new RegExp(`${candidateParts[i]?.slice(start, end)}`).test(
-          pathParts[j] || "",
-        );
+        const isMatched = new RegExp(
+          `${candidateParts[i]?.slice(start, end)}`,
+        ).test(pathParts[j] || "");
 
         if (!isMatched) {
           found = false;
