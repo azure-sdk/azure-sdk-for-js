b82dcddcc6b88441a8723ad3f27adf219b2db042
SDKAuto <sdkautomation@microsoft.com>
2024-11-07 03:41:55 +0000

    CodeGen from PR 31453 in Azure/azure-rest-api-specs Merge 786b60c16c76ff2bd2c18a186a543225be5fda07 into 735c26627331047d1c74526d171933af00d8c5d1

---
+++ sdk/healthinsights/azurehealthinsightsradiologyinsights-rest/src/index.ts
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import AzureHealthInsightsClient from "./azureHealthInsightsClient.js";
+
+export * from "./azureHealthInsightsClient.js";
+export * from "./parameters.js";
+export * from "./responses.js";
+export * from "./clientDefinitions.js";
+export * from "./isUnexpected.js";
+export * from "./models.js";
+export * from "./outputModels.js";
+export * from "./pollingHelper.js";
+export * from "./serializeHelper.js";
+
+export default AzureHealthInsightsClient;
