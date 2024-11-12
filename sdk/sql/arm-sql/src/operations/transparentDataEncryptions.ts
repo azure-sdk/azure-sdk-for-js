9fe9ae6a87a9bdd8fd7af35f0adad4151635bc7b
SDKAuto <sdkautomation@microsoft.com>
2024-11-12 17:45:31 +0000

    CodeGen from PR 31458 in Azure/azure-rest-api-specs Merge 70f9ca9c292a3a54542743fde93457899ffc30e1 into 033b90238477043a06bd7444a05c0e9aad7b8438

--- sdk/sql/arm-sql/src/operations/transparentDataEncryptions.ts
+++ sdk/sql/arm-sql/src/operations/transparentDataEncryptions.ts
@@ -385,7 +385,7 @@ const createOrUpdateOperationSpec: coreClient.OperationSpec = {
     },
     default: {},
   },
-  requestBody: Parameters.parameters89,
+  requestBody: Parameters.parameters91,
   queryParameters: [Parameters.apiVersion2],
   urlParameters: [
     Parameters.$host,
