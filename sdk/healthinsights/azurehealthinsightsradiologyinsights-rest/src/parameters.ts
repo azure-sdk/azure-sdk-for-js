3b211b903646e1741a96f87df1886d35a1443c81
SDKAuto <sdkautomation@microsoft.com>
2024-11-12 07:57:04 +0000

    CodeGen from PR 29181 in Azure/azure-rest-api-specs Merge 5e95fafc0f97636babb89fab5a9a140d5708c248 into fcee3edac78a8afb8a402e4920c395998caf12fb

---
+++ sdk/healthinsights/azurehealthinsightsradiologyinsights-rest/src/parameters.ts
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { RequestParameters } from "@azure-rest/core-client";
+import { RadiologyInsightsJob } from "./models.js";
+
+export interface GetJobQueryParamProperties {
+  /** Expand the indicated resources into the response. This parameter needs to be formatted as multi collection, we provide buildMultiCollection from serializeHelper.ts to help, you will probably need to set skipUrlEncoding as true when sending the request */
+  expand?: string;
+}
+
+export interface GetJobQueryParam {
+  queryParameters?: GetJobQueryParamProperties;
+}
+
+export type GetJobParameters = GetJobQueryParam & RequestParameters;
+
+export interface CreateJobBodyParam {
+  /** The resource instance. */
+  body: RadiologyInsightsJob;
+}
+
+export interface CreateJobQueryParamProperties {
+  /** Expand the indicated resources into the response. This parameter needs to be formatted as multi collection, we provide buildMultiCollection from serializeHelper.ts to help, you will probably need to set skipUrlEncoding as true when sending the request */
+  expand?: string;
+}
+
+export interface CreateJobQueryParam {
+  queryParameters?: CreateJobQueryParamProperties;
+}
+
+export type CreateJobParameters = CreateJobQueryParam &
+  CreateJobBodyParam &
+  RequestParameters;
