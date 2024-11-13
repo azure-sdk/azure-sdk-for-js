bc32c6d5a686c64457159f9f1dbb64086ca5fbe3
SDKAuto <sdkautomation@microsoft.com>
2024-11-13 05:36:27 +0000

    CodeGen from PR 31167 in Azure/azure-rest-api-specs Merge 4a04d8730b543e6355ba7e1539799d2b054aae97 into 533d972b32b91774f13e5b56190ab6573760ee85

---
+++ sdk/workloads/arm-workloadssapvirtualinstance/src/classic/index.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export { SAPApplicationServerInstancesOperations } from "./sAPApplicationServerInstances/index.js";
+export { SAPCentralServerInstancesOperations } from "./sAPCentralServerInstances/index.js";
+export { SAPDatabaseInstancesOperations } from "./sAPDatabaseInstances/index.js";
+export { SAPVirtualInstancesOperations } from "./sAPVirtualInstances/index.js";
