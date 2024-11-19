ff803477afea95d742a6c7cf5fc4fe79b41e536b
SDKAuto <sdkautomation@microsoft.com>
2024-11-19 21:51:31 +0000

    CodeGen from PR 31523 in Azure/azure-rest-api-specs Merge d2cc02b6b085f4e581636e79cac47f39039e3631 into 9f7cb94bf8871fd992a25c81d40236d3d2d34300

--- sdk/kubernetesconfiguration/arm-kubernetesconfiguration/src/sourceControlConfigurationClient.ts
+++ sdk/kubernetesconfiguration/arm-kubernetesconfiguration/src/sourceControlConfigurationClient.ts
@@ -11,25 +11,11 @@ import * as coreRestPipeline from "@azure/core-rest-pipeline";
 import {
   PipelineRequest,
   PipelineResponse,
-  SendRequest
+  SendRequest,
 } from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
-import {
-  ExtensionsImpl,
-  OperationStatusImpl,
-  FluxConfigurationsImpl,
-  FluxConfigOperationStatusImpl,
-  SourceControlConfigurationsImpl,
-  OperationsImpl
-} from "./operations";
-import {
-  Extensions,
-  OperationStatus,
-  FluxConfigurations,
-  FluxConfigOperationStatus,
-  SourceControlConfigurations,
-  Operations
-} from "./operationsInterfaces";
+import { SourceControlConfigurationsImpl } from "./operations";
+import { SourceControlConfigurations } from "./operationsInterfaces";
 import { SourceControlConfigurationClientOptionalParams } from "./models";
 
 export class SourceControlConfigurationClient extends coreClient.ServiceClient {
@@ -46,7 +32,7 @@ export class SourceControlConfigurationClient extends coreClient.ServiceClient {
   constructor(
     credentials: coreAuth.TokenCredential,
     subscriptionId: string,
-    options?: SourceControlConfigurationClientOptionalParams
+    options?: SourceControlConfigurationClientOptionalParams,
   ) {
     if (credentials === undefined) {
       throw new Error("'credentials' cannot be null");
@@ -61,10 +47,10 @@ export class SourceControlConfigurationClient extends coreClient.ServiceClient {
     }
     const defaults: SourceControlConfigurationClientOptionalParams = {
       requestContentType: "application/json; charset=utf-8",
-      credential: credentials
+      credential: credentials,
     };
 
-    const packageDetails = `azsdk-js-arm-kubernetesconfiguration/6.1.1`;
+    const packageDetails = `azsdk-js-sourcecontrolconfiguration/1.0.0-beta.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -74,20 +60,21 @@ export class SourceControlConfigurationClient extends coreClient.ServiceClient {
       ...defaults,
       ...options,
       userAgentOptions: {
-        userAgentPrefix
+        userAgentPrefix,
       },
       endpoint:
-        options.endpoint ?? options.baseUri ?? "https://management.azure.com"
+        options.endpoint ?? options.baseUri ?? "https://management.azure.com",
     };
     super(optionsWithDefaults);
 
     let bearerTokenAuthenticationPolicyFound: boolean = false;
     if (options?.pipeline && options.pipeline.getOrderedPolicies().length > 0) {
-      const pipelinePolicies: coreRestPipeline.PipelinePolicy[] = options.pipeline.getOrderedPolicies();
+      const pipelinePolicies: coreRestPipeline.PipelinePolicy[] =
+        options.pipeline.getOrderedPolicies();
       bearerTokenAuthenticationPolicyFound = pipelinePolicies.some(
         (pipelinePolicy) =>
           pipelinePolicy.name ===
-          coreRestPipeline.bearerTokenAuthenticationPolicyName
+          coreRestPipeline.bearerTokenAuthenticationPolicyName,
       );
     }
     if (
@@ -97,7 +84,7 @@ export class SourceControlConfigurationClient extends coreClient.ServiceClient {
       !bearerTokenAuthenticationPolicyFound
     ) {
       this.pipeline.removePolicy({
-        name: coreRestPipeline.bearerTokenAuthenticationPolicyName
+        name: coreRestPipeline.bearerTokenAuthenticationPolicyName,
       });
       this.pipeline.addPolicy(
         coreRestPipeline.bearerTokenAuthenticationPolicy({
@@ -107,9 +94,9 @@ export class SourceControlConfigurationClient extends coreClient.ServiceClient {
             `${optionsWithDefaults.endpoint}/.default`,
           challengeCallbacks: {
             authorizeRequestOnChallenge:
-              coreClient.authorizeRequestOnClaimChallenge
-          }
-        })
+              coreClient.authorizeRequestOnClaimChallenge,
+          },
+        }),
       );
     }
     // Parameter assignments
@@ -117,15 +104,10 @@ export class SourceControlConfigurationClient extends coreClient.ServiceClient {
 
     // Assigning values to Constant parameters
     this.$host = options.$host || "https://management.azure.com";
-    this.apiVersion = options.apiVersion || "2023-05-01";
-    this.extensions = new ExtensionsImpl(this);
-    this.operationStatus = new OperationStatusImpl(this);
-    this.fluxConfigurations = new FluxConfigurationsImpl(this);
-    this.fluxConfigOperationStatus = new FluxConfigOperationStatusImpl(this);
+    this.apiVersion = options.apiVersion || "2024-11-01";
     this.sourceControlConfigurations = new SourceControlConfigurationsImpl(
-      this
+      this,
     );
-    this.operations = new OperationsImpl(this);
     this.addCustomApiVersionPolicy(options.apiVersion);
   }
 
@@ -138,7 +120,7 @@ export class SourceControlConfigurationClient extends coreClient.ServiceClient {
       name: "CustomApiVersionPolicy",
       async sendRequest(
         request: PipelineRequest,
-        next: SendRequest
+        next: SendRequest,
       ): Promise<PipelineResponse> {
         const param = request.url.split("?");
         if (param.length > 1) {
@@ -152,15 +134,10 @@ export class SourceControlConfigurationClient extends coreClient.ServiceClient {
           request.url = param[0] + "?" + newParams.join("&");
         }
         return next(request);
-      }
+      },
     };
     this.pipeline.addPolicy(apiVersionPolicy);
   }
 
-  extensions: Extensions;
-  operationStatus: OperationStatus;
-  fluxConfigurations: FluxConfigurations;
-  fluxConfigOperationStatus: FluxConfigOperationStatus;
   sourceControlConfigurations: SourceControlConfigurations;
-  operations: Operations;
 }
