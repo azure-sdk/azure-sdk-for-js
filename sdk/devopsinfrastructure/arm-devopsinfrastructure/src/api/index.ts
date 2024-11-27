b18851ceeedd6028de7cc58a9cba38b39c5a3574
SDKAuto <sdkautomation@microsoft.com>
2024-11-27 06:07:24 +0000

    CodeGen from PR 31422 in Azure/azure-rest-api-specs Merge 184f146575835b375d8ce9905c37d3853b075d53 into fdcf40595e792db3a048098504a568edc113b881

---
+++ sdk/devopsinfrastructure/arm-devopsinfrastructure/src/api/index.ts
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  createDevOpsInfrastructure,
+  DevOpsInfrastructureContext,
+  DevOpsInfrastructureClientOptionalParams,
+} from "./devOpsInfrastructureContext.js";
+export {
+  OperationsListOptionalParams,
+  PoolsGetOptionalParams,
+  PoolsCreateOrUpdateOptionalParams,
+  PoolsUpdateOptionalParams,
+  PoolsDeleteOptionalParams,
+  PoolsListByResourceGroupOptionalParams,
+  PoolsListBySubscriptionOptionalParams,
+  PoolsCheckNameAvailabilityOptionalParams,
+  ResourceDetailsListByPoolOptionalParams,
+  SkuListByLocationOptionalParams,
+  SubscriptionUsagesUsagesOptionalParams,
+  ImageVersionsListByImageOptionalParams,
+} from "./options.js";
+export { imageVersionsListByImage } from "./imageVersions/index.js";
+export { operationsList } from "./operations/index.js";
+export {
+  poolsGet,
+  poolsCreateOrUpdate,
+  poolsUpdate,
+  poolsDelete,
+  poolsListByResourceGroup,
+  poolsListBySubscription,
+  poolsCheckNameAvailability,
+} from "./pools/index.js";
+export { resourceDetailsListByPool } from "./resourceDetails/index.js";
+export { skuListByLocation } from "./sku/index.js";
+export { subscriptionUsagesUsages } from "./subscriptionUsages/index.js";
