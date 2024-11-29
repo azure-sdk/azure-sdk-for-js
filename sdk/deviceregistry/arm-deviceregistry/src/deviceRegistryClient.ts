58962d7d4a2c807f1e1a9c8cb1ae6029e711d6a4
SDKAuto <sdkautomation@microsoft.com>
2024-11-29 06:27:48 +0000

    CodeGen from PR 31543 in Azure/azure-rest-api-specs Merge b9933f6f77779bdd9c747533813f3b4766b4230f into 43ce04961ccb6496e421789c54798ba1e0880f64

---
+++ sdk/deviceregistry/arm-deviceregistry/src/deviceRegistryClient.ts
@@ -0,0 +1,80 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import {
+  getOperationsOperations,
+  OperationsOperations,
+} from "./classic/operations/index.js";
+import {
+  getOperationStatusOperations,
+  OperationStatusOperations,
+} from "./classic/operationStatus/index.js";
+import {
+  getAssetsOperations,
+  AssetsOperations,
+} from "./classic/assets/index.js";
+import {
+  getAssetEndpointProfilesOperations,
+  AssetEndpointProfilesOperations,
+} from "./classic/assetEndpointProfiles/index.js";
+import {
+  getBillingContainersOperations,
+  BillingContainersOperations,
+} from "./classic/billingContainers/index.js";
+import {
+  createDeviceRegistry,
+  DeviceRegistryContext,
+  DeviceRegistryClientOptionalParams,
+} from "./api/index.js";
+import { Pipeline } from "@azure/core-rest-pipeline";
+import { TokenCredential } from "@azure/core-auth";
+
+export { DeviceRegistryClientOptionalParams } from "./api/deviceRegistryContext.js";
+
+export class DeviceRegistryClient {
+  private _client: DeviceRegistryContext;
+  /** The pipeline used by this client to make requests */
+  public readonly pipeline: Pipeline;
+
+  /** Microsoft.DeviceRegistry Resource Provider management API. */
+  constructor(
+    credential: TokenCredential,
+    subscriptionId: string,
+    options: DeviceRegistryClientOptionalParams = {},
+  ) {
+    const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
+    const userAgentPrefix = prefixFromOptions
+      ? `${prefixFromOptions} azsdk-js-client`
+      : `azsdk-js-client`;
+    this._client = createDeviceRegistry(credential, {
+      ...options,
+      userAgentOptions: { userAgentPrefix },
+    });
+    this.pipeline = this._client.pipeline;
+    this.operations = getOperationsOperations(this._client);
+    this.operationStatus = getOperationStatusOperations(
+      this._client,
+      subscriptionId,
+    );
+    this.assets = getAssetsOperations(this._client, subscriptionId);
+    this.assetEndpointProfiles = getAssetEndpointProfilesOperations(
+      this._client,
+      subscriptionId,
+    );
+    this.billingContainers = getBillingContainersOperations(
+      this._client,
+      subscriptionId,
+    );
+  }
+
+  /** The operation groups for Operations */
+  public readonly operations: OperationsOperations;
+  /** The operation groups for OperationStatus */
+  public readonly operationStatus: OperationStatusOperations;
+  /** The operation groups for Assets */
+  public readonly assets: AssetsOperations;
+  /** The operation groups for AssetEndpointProfiles */
+  public readonly assetEndpointProfiles: AssetEndpointProfilesOperations;
+  /** The operation groups for BillingContainers */
+  public readonly billingContainers: BillingContainersOperations;
+}
