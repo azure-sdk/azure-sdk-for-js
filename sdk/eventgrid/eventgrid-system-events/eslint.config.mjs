2c4f4d9980e0f8319792c94b8b36441263f7606d
SDKAuto <sdkautomation@microsoft.com>
2024-12-03 19:25:34 +0000

    CodeGen from PR 30936 in Azure/azure-rest-api-specs Merge 7ee58423ba6f43466717be39645255a602427ce5 into d0c3a661b6478798a1b028d4ddbb4633f634e808

--- sdk/eventgrid/eventgrid-system-events/eslint.config.mjs
+++ sdk/eventgrid/eventgrid-system-events/eslint.config.mjs
@@ -4,7 +4,14 @@ export default [
   ...azsdkEslint.configs.recommended,
   {
     rules: {
-      "@typescript-eslint/naming-convention": "warn",
-    },
-  },
+      "@azure/azure-sdk/ts-modules-only-named": "warn",
+      "@azure/azure-sdk/ts-apiextractor-json-types": "warn",
+      "@azure/azure-sdk/ts-package-json-types": "warn",
+      "@azure/azure-sdk/ts-package-json-engine-is-present": "warn",
+      "@azure/azure-sdk/ts-package-json-module": "off",
+      "@azure/azure-sdk/ts-package-json-files-required": "off",
+      "@azure/azure-sdk/ts-package-json-main-is-cjs": "off",
+      "tsdoc/syntax": "warn"
+    }
+  }
 ];
