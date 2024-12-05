a6234bd2b9eb29b92947d50ac99a29d56f49d1a0
SDKAuto <sdkautomation@microsoft.com>
2024-12-05 09:26:33 +0000

    CodeGen from PR 31643 in Azure/azure-rest-api-specs Merge e0f69cd801691afd27b2cb6bab515874bbf8e441 into c59b222396f95f45efa7a57c1b5a5ae1ac73a9cb

--- sdk/openai/openai/src/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-/**
- * @packageDocumentation https://learn.microsoft.com/azure/cognitive-services/openai/overview
- */
