0878cbcf714452218eb72b2675fcd377c6667f74
SDKAuto <sdkautomation@microsoft.com>
2024-12-10 16:06:44 +0000

    CodeGen from PR 31006 in Azure/azure-rest-api-specs Merge 8e259f58b77a7a30e0110642710394141ed40767 into bd37988c515821dcd48192ec1db5cb39c39e3c97

--- sdk/databoundaries/arm-databoundaries/src/index.ts
+++ sdk/databoundaries/arm-databoundaries/src/index.ts
@@ -6,6 +6,8 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
+/// <reference lib="esnext.asynciterable" />
+export { getContinuationToken } from "./pagingHelper";
 export * from "./models";
 export { DataboundariesManegementClient } from "./databoundariesManegementClient";
 export * from "./operationsInterfaces";
