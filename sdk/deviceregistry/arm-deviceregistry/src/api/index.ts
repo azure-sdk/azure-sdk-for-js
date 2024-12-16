d19093070d8849302286b7bb1eb1f7d2804de1ea
SDKAuto <sdkautomation@microsoft.com>
2024-12-16 02:31:17 +0000

    CodeGen from PR 31874 in Azure/azure-rest-api-specs Merge 198bea37f7a2bbc17c8d98df0b217bbb450613e3 into 58740206b853320974ef5e4864f7be8120b15a27

---
+++ sdk/deviceregistry/arm-deviceregistry/src/api/index.ts
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  createDeviceRegistryManagement,
+  DeviceRegistryManagementContext,
+  DeviceRegistryManagementClientOptionalParams,
+} from "./deviceRegistryManagementContext.js";
+export {
+  OperationsListOptionalParams,
+  OperationStatusGetOptionalParams,
+  AssetsGetOptionalParams,
+  AssetsCreateOrReplaceOptionalParams,
+  AssetsUpdateOptionalParams,
+  AssetsDeleteOptionalParams,
+  AssetsListByResourceGroupOptionalParams,
+  AssetsListBySubscriptionOptionalParams,
+  AssetEndpointProfilesGetOptionalParams,
+  AssetEndpointProfilesCreateOrReplaceOptionalParams,
+  AssetEndpointProfilesUpdateOptionalParams,
+  AssetEndpointProfilesDeleteOptionalParams,
+  AssetEndpointProfilesListByResourceGroupOptionalParams,
+  AssetEndpointProfilesListBySubscriptionOptionalParams,
+  BillingContainersGetOptionalParams,
+  BillingContainersListBySubscriptionOptionalParams,
+} from "./options.js";
+export {
+  assetEndpointProfilesGet,
+  assetEndpointProfilesCreateOrReplace,
+  assetEndpointProfilesUpdate,
+  assetEndpointProfilesDelete,
+  assetEndpointProfilesListByResourceGroup,
+  assetEndpointProfilesListBySubscription,
+} from "./assetEndpointProfiles/index.js";
+export {
+  assetsGet,
+  assetsCreateOrReplace,
+  assetsUpdate,
+  assetsDelete,
+  assetsListByResourceGroup,
+  assetsListBySubscription,
+} from "./assets/index.js";
+export {
+  billingContainersGet,
+  billingContainersListBySubscription,
+} from "./billingContainers/index.js";
+export { operationsList } from "./operations/index.js";
+export { operationStatusGet } from "./operationStatus/index.js";
