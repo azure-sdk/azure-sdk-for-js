a74dcd1708eea097585e7b51f6da93b3ff8e4dd1
SDKAuto <sdkautomation@microsoft.com>
2024-12-18 15:54:17 +0000

    CodeGen from PR 31796 in Azure/azure-rest-api-specs Merge 880959f11dd0eb61cad2125d02b0971ad5a936fb into 416353cabfea1fc7a1c7c46ba3340408a2618de1

--- sdk/keyvault/arm-keyvault/src/operationsInterfaces/mhsmRegions.ts
+++ sdk/keyvault/arm-keyvault/src/operationsInterfaces/mhsmRegions.ts
@@ -9,7 +9,7 @@
 import { PagedAsyncIterableIterator } from "@azure/core-paging";
 import {
   MhsmGeoReplicatedRegion,
-  MhsmRegionsListByResourceOptionalParams
+  MhsmRegionsListByResourceOptionalParams,
 } from "../models";
 
 /// <reference lib="esnext.asynciterable" />
@@ -24,6 +24,6 @@ export interface MhsmRegions {
   listByResource(
     resourceGroupName: string,
     name: string,
-    options?: MhsmRegionsListByResourceOptionalParams
+    options?: MhsmRegionsListByResourceOptionalParams,
   ): PagedAsyncIterableIterator<MhsmGeoReplicatedRegion>;
 }
