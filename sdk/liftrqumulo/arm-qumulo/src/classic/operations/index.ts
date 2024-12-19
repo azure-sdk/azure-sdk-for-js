49b3b21decb3067ebe21b07be38ef6eae265ad91
SDKAuto <sdkautomation@microsoft.com>
2024-12-19 09:24:35 +0000

    CodeGen from PR 31921 in Azure/azure-rest-api-specs Merge 2f34def878a6014bd38ae290d95f137f3c4a3ce9 into f8ece47e1ce7bb262b470c15fcee32059e658532

---
+++ sdk/liftrqumulo/arm-qumulo/src/classic/operations/index.ts
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { StorageContext } from "../../api/storageContext.js";
+import { operationsList } from "../../api/operations/index.js";
+import { Operation } from "../../models/models.js";
+import { PagedAsyncIterableIterator } from "../../static-helpers/pagingHelpers.js";
+import { OperationsListOptionalParams } from "../../api/options.js";
+
+/** Interface representing a Operations operations. */
+export interface OperationsOperations {
+  /** List the operations for the provider */
+  list: (
+    options?: OperationsListOptionalParams,
+  ) => PagedAsyncIterableIterator<Operation>;
+}
+
+export function getOperations(context: StorageContext) {
+  return {
+    list: (options?: OperationsListOptionalParams) =>
+      operationsList(context, options),
+  };
+}
+
+export function getOperationsOperations(
+  context: StorageContext,
+): OperationsOperations {
+  return {
+    ...getOperations(context),
+  };
+}
