d378ba2e2934fd6fcca47bfcf9ec16710a2a5a14
SDKAuto <sdkautomation@microsoft.com>
2024-12-19 19:10:11 +0000

    CodeGen from PR 29249 in Azure/azure-rest-api-specs Merge b1caaf5a33601e3333d27069df3e4aceb3b31cef into d7d8b18fbf5ca2335decea897117c7b837467c9b

---
+++ sdk/keyvault/keyvault-secrets/src/models/index.ts
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  SecretSetParameters,
+  SecretAttributes,
+  KnownDeletionRecoveryLevel,
+  DeletionRecoveryLevel,
+  SecretBundle,
+  DeletedSecretBundle,
+  SecretUpdateParameters,
+  SecretItem,
+  DeletedSecretItem,
+  BackupSecretResult,
+  SecretRestoreParameters,
+  KnownVersions,
+} from "./models.js";
