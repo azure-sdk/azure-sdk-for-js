7f2afd8065c39b1beb9d40de56ef6957b9118620
SDKAuto <sdkautomation@microsoft.com>
2025-01-07 11:07:38 +0000

    CodeGen from PR 31855 in Azure/azure-rest-api-specs Merge dad65926bcf41c836545c8f5ac3e133291732850 into 49456eb149e3bb5d0528cbca714cfa1806415484

---
+++ sdk/playwrighttesting/microsoft-playwright-testing-rest/src/models.ts
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/** Model of an access-token linked to an account. */
+export interface AccessToken {
+  /** The access-token name. */
+  name: string;
+  /** The access-token expiryAt utcDateTime. */
+  expiryAt: Date | string;
+}
+
+/** Alias for AccessTokenState */
+export type AccessTokenState = string;
+/** Alias for Os */
+export type Os = string;
