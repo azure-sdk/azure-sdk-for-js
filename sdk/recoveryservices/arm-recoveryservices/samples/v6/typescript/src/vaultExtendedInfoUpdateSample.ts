5e0c6024401cec83d62850dc518e38c03f3215bd
SDKAuto <sdkautomation@microsoft.com>
2025-01-09 03:19:03 +0000

    CodeGen from PR 32041 in Azure/azure-rest-api-specs Merge eea90072c2cbebe6eabd0420e06e7b6480fa6939 into c29b9330313c91d43a75d9d08144c98071c904d0

--- sdk/recoveryservices/arm-recoveryservices/samples/v6/typescript/src/vaultExtendedInfoUpdateSample.ts
+++ sdk/recoveryservices/arm-recoveryservices/samples/v6/typescript/src/vaultExtendedInfoUpdateSample.ts
@@ -10,9 +10,7 @@
 // Licensed under the MIT License.
 import { RecoveryServicesClient } from "@azure/arm-recoveryservices";
 import { DefaultAzureCredential } from "@azure/identity";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
 
 /**
  * This sample demonstrates how to Update vault extended info.
