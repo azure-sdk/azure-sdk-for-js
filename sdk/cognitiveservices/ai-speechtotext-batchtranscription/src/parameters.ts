7d4766113719b1803e7c814e8275f8b1a7181000
SDKAuto <sdkautomation@microsoft.com>
2025-01-15 13:50:39 +0000

    CodeGen from PR 31097 in Azure/azure-rest-api-specs Merge a34ea3a03e497d75e0e77a220b9adf41030e1f42 into aa4ba9eb0ab21268118c11052b081ff5d2e01e4d

---
+++ sdk/cognitiveservices/ai-speechtotext-batchtranscription/src/parameters.ts
@@ -0,0 +1,54 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { RequestParameters } from "@azure-rest/core-client";
+import { Transcription, TranscriptionUpdate } from "./models.js";
+
+export interface SubmitTranscriptionBodyParam {
+  /** Transcription */
+  body: Transcription;
+}
+
+export type SubmitTranscriptionParameters = SubmitTranscriptionBodyParam & RequestParameters;
+
+export interface GetTranscriptionsQueryParamProperties {
+  /** Number of datasets that will be skipped. */
+  skip?: number;
+  /** Number of datasets that will be included after skipping. */
+  top?: number;
+  /**
+   * A filtering expression for selecting a subset of the available transcriptions.
+   *             - Supported properties: displayName, description, createdDateTime, lastActionDateTime, status, locale.
+   *             - Operators:
+   *               - eq, ne are supported for all properties.
+   *               - gt, ge, lt, le are supported for createdDateTime and lastActionDateTime.
+   *               - and, or, not are supported.
+   *             - Example:
+   *               filter=createdDateTime gt 2022-02-01T11:00:00Z
+   */
+  filter?: string;
+}
+
+export interface GetTranscriptionsQueryParam {
+  queryParameters?: GetTranscriptionsQueryParamProperties;
+}
+
+export type GetTranscriptionsParameters = GetTranscriptionsQueryParam & RequestParameters;
+export type GetTranscriptionParameters = RequestParameters;
+/** Transcription Update */
+export type TranscriptionUpdateResourceMergeAndPatch = Partial<TranscriptionUpdate>;
+
+export interface PatchTranscriptionBodyParam {
+  /** Transcription Update */
+  body: TranscriptionUpdate;
+}
+
+export interface PatchTranscriptionMediaTypesParam {
+  /** request content type */
+  contentType: "application/json" | "application/merge-patch+json";
+}
+
+export type PatchTranscriptionParameters = PatchTranscriptionMediaTypesParam &
+  PatchTranscriptionBodyParam &
+  RequestParameters;
+export type DeleteTranscriptionParameters = RequestParameters;
