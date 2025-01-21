be739c081205bf760d8dd18b069897fd92f904d1
SDKAuto <sdkautomation@microsoft.com>
2025-01-21 08:11:36 +0000

    CodeGen from PR 32243 in Azure/azure-rest-api-specs Merge 2a61ad4370ca8c35ca8cb3c8729c05b3df95a276 into c4ac1e0ba0936cf75113791f3038cd8b50ea9263

---
+++ sdk/hybridconnectivity/arm-hybridconnectivity/samples-dev/generateAwsTemplateGenerateAwsTemplatePostSample.ts
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { HybridConnectivityManagementAPIClient } from "@azure/arm-hybridconnectivity";
+import { DefaultAzureCredential } from "@azure/identity";
+
+/**
+ * This sample demonstrates how to retrieve AWS Cloud Formation template
+ *
+ * @summary retrieve AWS Cloud Formation template
+ * x-ms-original-file: 2024-12-01/GenerateAwsTemplate_Post.json
+ */
+async function generateAwsTemplatePost(): Promise<void> {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "5ACC4579-DB34-4C2F-8F8C-25061168F342";
+  const client = new HybridConnectivityManagementAPIClient(credential, subscriptionId);
+  const result = await client.generateAwsTemplate.GenerateAwsTemplate_post({
+    connectorId: "pnxcfjidglabnwxit",
+    solutionTypes: [{ solutionType: "hjyownzpfxwiufmd", solutionSettings: {} }],
+  });
+  console.log(result);
+}
+
+async function main(): Promise<void> {
+  generateAwsTemplatePost();
+}
+
+main().catch(console.error);
