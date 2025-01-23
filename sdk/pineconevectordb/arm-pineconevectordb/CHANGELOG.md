d93f1746899d60fd0e15a0da2b746a77c1a327e0
SDKAuto <sdkautomation@microsoft.com>
2025-01-23 03:28:34 +0000

    CodeGen from PR 32273 in Azure/azure-rest-api-specs Merge a7e03b26f514d11a96f482a737dc8ff808e1d306 into 508a12f060dc46c5dcfabd0cd98a913b44b24e47

---
+++ sdk/pineconevectordb/arm-pineconevectordb/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Release History
+    
+## 1.0.0-beta.1 (2025-01-23)
+
+### Features Added
+
+The package of @azure/arm-pineconevectordb is using our next generation design principles. To learn more, please refer to our documentation [Quick Start](https://aka.ms/azsdk/js/mgmt/quickstart).
---
+++ sdk/pineconevectordb/arm-pineconevectordb/src/api/organizations/index.ts
@@ -0,0 +1,355 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import {
+  VectorDbContext as Client,
+  OrganizationsCreateOrUpdateOptionalParams,
+  OrganizationsDeleteOptionalParams,
+  OrganizationsGetOptionalParams,
+  OrganizationsListByResourceGroupOptionalParams,
+  OrganizationsListBySubscriptionOptionalParams,
+  OrganizationsUpdateOptionalParams,
+} from "../index.js";
+import {
+  OrganizationResource,
+  organizationResourceSerializer,
+  organizationResourceDeserializer,
+  errorResponseDeserializer,
+  OrganizationResourceUpdate,
+  organizationResourceUpdateSerializer,
+  _OrganizationResourceListResult,
+  _organizationResourceListResultDeserializer,
+} from "../../models/models.js";
+import {
+  PagedAsyncIterableIterator,
+  buildPagedAsyncIterator,
+} from "../../static-helpers/pagingHelpers.js";
+import { getLongRunningPoller } from "../../static-helpers/pollingHelpers.js";
+import {
+  StreamableMethod,
+  PathUncheckedResponse,
+  createRestError,
+  operationOptionsToRequestParameters,
+} from "@azure-rest/core-client";
+import { PollerLike, OperationState } from "@azure/core-lro";
+
+export function _organizationsListBySubscriptionSend(
+  context: Client,
+  options: OrganizationsListBySubscriptionOptionalParams = {
+    requestOptions: {},
+  },
+): StreamableMethod {
+  return context
+    .path(
+      "/subscriptions/{subscriptionId}/providers/Pinecone.VectorDb/organizations",
+      context.subscriptionId,
+    )
+    .get({
+      ...operationOptionsToRequestParameters(options),
+      headers: {
+        accept: "application/json",
+        ...options.requestOptions?.headers,
+      },
+      queryParameters: { "api-version": context.apiVersion },
+    });
+}
+
+export async function _organizationsListBySubscriptionDeserialize(
+  result: PathUncheckedResponse,
+): Promise<_OrganizationResourceListResult> {
+  const expectedStatuses = ["200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return _organizationResourceListResultDeserializer(result.body);
+}
+
+/** List OrganizationResource resources by subscription ID */
+export function organizationsListBySubscription(
+  context: Client,
+  options: OrganizationsListBySubscriptionOptionalParams = {
+    requestOptions: {},
+  },
+): PagedAsyncIterableIterator<OrganizationResource> {
+  return buildPagedAsyncIterator(
+    context,
+    () => _organizationsListBySubscriptionSend(context, options),
+    _organizationsListBySubscriptionDeserialize,
+    ["200"],
+    { itemName: "value", nextLinkName: "nextLink" },
+  );
+}
+
+export function _organizationsListByResourceGroupSend(
+  context: Client,
+  resourceGroupName: string,
+  options: OrganizationsListByResourceGroupOptionalParams = {
+    requestOptions: {},
+  },
+): StreamableMethod {
+  return context
+    .path(
+      "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Pinecone.VectorDb/organizations",
+      context.subscriptionId,
+      resourceGroupName,
+    )
+    .get({
+      ...operationOptionsToRequestParameters(options),
+      headers: {
+        accept: "application/json",
+        ...options.requestOptions?.headers,
+      },
+      queryParameters: { "api-version": context.apiVersion },
+    });
+}
+
+export async function _organizationsListByResourceGroupDeserialize(
+  result: PathUncheckedResponse,
+): Promise<_OrganizationResourceListResult> {
+  const expectedStatuses = ["200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return _organizationResourceListResultDeserializer(result.body);
+}
+
+/** List OrganizationResource resources by resource group */
+export function organizationsListByResourceGroup(
+  context: Client,
+  resourceGroupName: string,
+  options: OrganizationsListByResourceGroupOptionalParams = {
+    requestOptions: {},
+  },
+): PagedAsyncIterableIterator<OrganizationResource> {
+  return buildPagedAsyncIterator(
+    context,
+    () => _organizationsListByResourceGroupSend(context, resourceGroupName, options),
+    _organizationsListByResourceGroupDeserialize,
+    ["200"],
+    { itemName: "value", nextLinkName: "nextLink" },
+  );
+}
+
+export function _organizationsDeleteSend(
+  context: Client,
+  resourceGroupName: string,
+  organizationname: string,
+  options: OrganizationsDeleteOptionalParams = { requestOptions: {} },
+): StreamableMethod {
+  return context
+    .path(
+      "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Pinecone.VectorDb/organizations/{organizationname}",
+      context.subscriptionId,
+      resourceGroupName,
+      organizationname,
+    )
+    .delete({
+      ...operationOptionsToRequestParameters(options),
+      headers: {
+        accept: "application/json",
+        ...options.requestOptions?.headers,
+      },
+      queryParameters: { "api-version": context.apiVersion },
+    });
+}
+
+export async function _organizationsDeleteDeserialize(
+  result: PathUncheckedResponse,
+): Promise<void> {
+  const expectedStatuses = ["202", "204", "200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return;
+}
+
+/** Delete a OrganizationResource */
+export function organizationsDelete(
+  context: Client,
+  resourceGroupName: string,
+  organizationname: string,
+  options: OrganizationsDeleteOptionalParams = { requestOptions: {} },
+): PollerLike<OperationState<void>, void> {
+  return getLongRunningPoller(context, _organizationsDeleteDeserialize, ["202", "204", "200"], {
+    updateIntervalInMs: options?.updateIntervalInMs,
+    abortSignal: options?.abortSignal,
+    getInitialResponse: () =>
+      _organizationsDeleteSend(context, resourceGroupName, organizationname, options),
+    resourceLocationConfig: "location",
+  }) as PollerLike<OperationState<void>, void>;
+}
+
+export function _organizationsUpdateSend(
+  context: Client,
+  resourceGroupName: string,
+  organizationname: string,
+  properties: OrganizationResourceUpdate,
+  options: OrganizationsUpdateOptionalParams = { requestOptions: {} },
+): StreamableMethod {
+  return context
+    .path(
+      "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Pinecone.VectorDb/organizations/{organizationname}",
+      context.subscriptionId,
+      resourceGroupName,
+      organizationname,
+    )
+    .patch({
+      ...operationOptionsToRequestParameters(options),
+      contentType: "application/json",
+      headers: {
+        accept: "application/json",
+        ...options.requestOptions?.headers,
+      },
+      queryParameters: { "api-version": context.apiVersion },
+      body: organizationResourceUpdateSerializer(properties),
+    });
+}
+
+export async function _organizationsUpdateDeserialize(
+  result: PathUncheckedResponse,
+): Promise<OrganizationResource> {
+  const expectedStatuses = ["200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return organizationResourceDeserializer(result.body);
+}
+
+/** Update a OrganizationResource */
+export async function organizationsUpdate(
+  context: Client,
+  resourceGroupName: string,
+  organizationname: string,
+  properties: OrganizationResourceUpdate,
+  options: OrganizationsUpdateOptionalParams = { requestOptions: {} },
+): Promise<OrganizationResource> {
+  const result = await _organizationsUpdateSend(
+    context,
+    resourceGroupName,
+    organizationname,
+    properties,
+    options,
+  );
+  return _organizationsUpdateDeserialize(result);
+}
+
+export function _organizationsCreateOrUpdateSend(
+  context: Client,
+  resourceGroupName: string,
+  organizationname: string,
+  resource: OrganizationResource,
+  options: OrganizationsCreateOrUpdateOptionalParams = { requestOptions: {} },
+): StreamableMethod {
+  return context
+    .path(
+      "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Pinecone.VectorDb/organizations/{organizationname}",
+      context.subscriptionId,
+      resourceGroupName,
+      organizationname,
+    )
+    .put({
+      ...operationOptionsToRequestParameters(options),
+      contentType: "application/json",
+      headers: {
+        accept: "application/json",
+        ...options.requestOptions?.headers,
+      },
+      queryParameters: { "api-version": context.apiVersion },
+      body: organizationResourceSerializer(resource),
+    });
+}
+
+export async function _organizationsCreateOrUpdateDeserialize(
+  result: PathUncheckedResponse,
+): Promise<OrganizationResource> {
+  const expectedStatuses = ["200", "201"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return organizationResourceDeserializer(result.body);
+}
+
+/** Create a OrganizationResource */
+export function organizationsCreateOrUpdate(
+  context: Client,
+  resourceGroupName: string,
+  organizationname: string,
+  resource: OrganizationResource,
+  options: OrganizationsCreateOrUpdateOptionalParams = { requestOptions: {} },
+): PollerLike<OperationState<OrganizationResource>, OrganizationResource> {
+  return getLongRunningPoller(context, _organizationsCreateOrUpdateDeserialize, ["200", "201"], {
+    updateIntervalInMs: options?.updateIntervalInMs,
+    abortSignal: options?.abortSignal,
+    getInitialResponse: () =>
+      _organizationsCreateOrUpdateSend(
+        context,
+        resourceGroupName,
+        organizationname,
+        resource,
+        options,
+      ),
+    resourceLocationConfig: "azure-async-operation",
+  }) as PollerLike<OperationState<OrganizationResource>, OrganizationResource>;
+}
+
+export function _organizationsGetSend(
+  context: Client,
+  resourceGroupName: string,
+  organizationname: string,
+  options: OrganizationsGetOptionalParams = { requestOptions: {} },
+): StreamableMethod {
+  return context
+    .path(
+      "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Pinecone.VectorDb/organizations/{organizationname}",
+      context.subscriptionId,
+      resourceGroupName,
+      organizationname,
+    )
+    .get({
+      ...operationOptionsToRequestParameters(options),
+      headers: {
+        accept: "application/json",
+        ...options.requestOptions?.headers,
+      },
+      queryParameters: { "api-version": context.apiVersion },
+    });
+}
+
+export async function _organizationsGetDeserialize(
+  result: PathUncheckedResponse,
+): Promise<OrganizationResource> {
+  const expectedStatuses = ["200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return organizationResourceDeserializer(result.body);
+}
+
+/** Get a OrganizationResource */
+export async function organizationsGet(
+  context: Client,
+  resourceGroupName: string,
+  organizationname: string,
+  options: OrganizationsGetOptionalParams = { requestOptions: {} },
+): Promise<OrganizationResource> {
+  const result = await _organizationsGetSend(context, resourceGroupName, organizationname, options);
+  return _organizationsGetDeserialize(result);
+}
