37644452c08207d403139a3bf766e603a35259c3
SDKAuto <sdkautomation@microsoft.com>
2025-01-27 20:47:09 +0000

    CodeGen from PR 32255 in Azure/azure-rest-api-specs Merge 982127ab0981960a7c77272788ed7d6c82e3fb36 into f8e906707f0bed0609606370ad33de50546f8c63

--- sdk/resources-subscriptions/arm-resources-subscriptions/src/operationsInterfaces/tenants.ts
+++ sdk/resources-subscriptions/arm-resources-subscriptions/src/operationsInterfaces/tenants.ts
@@ -7,7 +7,7 @@
  */
 
 import { PagedAsyncIterableIterator } from "@azure/core-paging";
-import { TenantIdDescription, TenantsListOptionalParams } from "../models";
+import { TenantIdDescription, TenantsListOptionalParams } from "../models/index.js";
 
 /// <reference lib="esnext.asynciterable" />
 /** Interface representing a Tenants. */
@@ -17,6 +17,6 @@ export interface Tenants {
    * @param options The options parameters.
    */
   list(
-    options?: TenantsListOptionalParams
+    options?: TenantsListOptionalParams,
   ): PagedAsyncIterableIterator<TenantIdDescription>;
 }
