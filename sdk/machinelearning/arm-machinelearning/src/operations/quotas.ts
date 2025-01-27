9212ba7c40633d2e992ba4c57386613e4d550b0e
SDKAuto <sdkautomation@microsoft.com>
2025-01-27 15:33:51 +0000

    CodeGen from PR 32280 in Azure/azure-rest-api-specs Merge 24239b55c8d57266be7f4d26a6584098dbea81b9 into 7e8bb49d87cf0b33cf3c1a17aff368112883a019

--- sdk/machinelearning/arm-machinelearning/src/operations/quotas.ts
+++ sdk/machinelearning/arm-machinelearning/src/operations/quotas.ts
@@ -7,12 +7,12 @@
  */
 
 import { PagedAsyncIterableIterator, PageSettings } from "@azure/core-paging";
-import { setContinuationToken } from "../pagingHelper";
-import { Quotas } from "../operationsInterfaces";
+import { setContinuationToken } from "../pagingHelper.js";
+import { Quotas } from "../operationsInterfaces/index.js";
 import * as coreClient from "@azure/core-client";
-import * as Mappers from "../models/mappers";
-import * as Parameters from "../models/parameters";
-import { AzureMachineLearningServicesManagementClient } from "../azureMachineLearningServicesManagementClient";
+import * as Mappers from "../models/mappers.js";
+import * as Parameters from "../models/parameters.js";
+import { AzureMachineLearningServicesManagementClient } from "../azureMachineLearningServicesManagementClient.js";
 import {
   ResourceQuota,
   QuotasListNextOptionalParams,
@@ -22,7 +22,7 @@ import {
   QuotasUpdateOptionalParams,
   QuotasUpdateResponse,
   QuotasListNextResponse,
-} from "../models";
+} from "../models/index.js";
 
 /// <reference lib="esnext.asynciterable" />
 /** Class containing Quotas operations. */
@@ -158,7 +158,7 @@ const updateOperationSpec: coreClient.OperationSpec = {
       bodyMapper: Mappers.ErrorResponse,
     },
   },
-  requestBody: Parameters.parameters3,
+  requestBody: Parameters.parameters,
   queryParameters: [Parameters.apiVersion],
   urlParameters: [
     Parameters.$host,
@@ -203,8 +203,8 @@ const listNextOperationSpec: coreClient.OperationSpec = {
   urlParameters: [
     Parameters.$host,
     Parameters.subscriptionId,
-    Parameters.nextLink,
     Parameters.location,
+    Parameters.nextLink,
   ],
   headerParameters: [Parameters.accept],
   serializer,
