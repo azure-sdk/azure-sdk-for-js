05c2cc6a57a96b6e91d0004d167f95d31c6d6fa9
SDKAuto <sdkautomation@microsoft.com>
2025-02-15 01:38:35 +0000

    CodeGen from PR 32490 in Azure/azure-rest-api-specs Merge 1838a22fc92ff27f72274f22fd7f9e2767eefc45 into e26b89bcbec9eed5026c01416e481408b2a1ca1a

--- sdk/servicenetworking/arm-servicenetworking/samples-dev/securityPoliciesInterfaceSecurityPoliciesInterfaceCreateOrUpdateSample.ts
+++ sdk/servicenetworking/arm-servicenetworking/samples-dev/securityPoliciesInterfaceSecurityPoliciesInterfaceCreateOrUpdateSample.ts
@@ -8,25 +8,53 @@ import { DefaultAzureCredential } from "@azure/identity";
  * This sample demonstrates how to create a SecurityPolicy
  *
  * @summary create a SecurityPolicy
- * x-ms-original-file: 2025-01-01/SecurityPolicyPut.json
+ * x-ms-original-file: 2025-03-01-preview/IpAccessRulesSecurityPolicyPut.json
  */
-async function putSecurityPolicy(): Promise<void> {
+async function putIpAccessRulesSecurityPolicy(): Promise<void> {
   const credential = new DefaultAzureCredential();
   const subscriptionId = "subid";
   const client = new ServiceNetworkingManagementClient(credential, subscriptionId);
-  const result = await client.securityPoliciesInterface.createOrUpdate("rg1", "tc1", "sp1", {
-    location: "NorthCentralUS",
-    properties: {
-      wafPolicy: {
-        id: "/subscriptions/subid/resourcegroups/rg1/providers/Microsoft.Networking/applicationGatewayWebApplicationFirewallPolicies/wp-0",
+  const result = await client.securityPoliciesInterface.SecurityPoliciesInterface_createOrUpdate(
+    "rg1",
+    "tc1",
+    "sp1",
+    {
+      location: "NorthCentralUS",
+      properties: { ipAccessRulesPolicy: { rules: [] } },
+    },
+  );
+  console.log(result);
+}
+
+/**
+ * This sample demonstrates how to create a SecurityPolicy
+ *
+ * @summary create a SecurityPolicy
+ * x-ms-original-file: 2025-03-01-preview/WafSecurityPolicyPut.json
+ */
+async function putWafSecurityPolicy(): Promise<void> {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "subid";
+  const client = new ServiceNetworkingManagementClient(credential, subscriptionId);
+  const result = await client.securityPoliciesInterface.SecurityPoliciesInterface_createOrUpdate(
+    "rg1",
+    "tc1",
+    "sp1",
+    {
+      location: "NorthCentralUS",
+      properties: {
+        wafPolicy: {
+          id: "/subscriptions/subid/resourcegroups/rg1/providers/Microsoft.Networking/applicationGatewayWebApplicationFirewallPolicies/wp-0",
+        },
       },
     },
-  });
+  );
   console.log(result);
 }
 
 async function main(): Promise<void> {
-  await putSecurityPolicy();
+  await putIpAccessRulesSecurityPolicy();
+  putWafSecurityPolicy();
 }
 
 main().catch(console.error);
