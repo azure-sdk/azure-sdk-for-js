566d34022665c587d1eb821e0197e96822712843
SDKAuto <sdkautomation@microsoft.com>
2025-02-22 04:31:41 +0000

    CodeGen from PR 32741 in Azure/azure-rest-api-specs Merge 2834538e8e1e0d506e5a049326c175bb5dc4aa8e into 7438e68d87366629ddbfb3e4f3953672f0efb476

--- sdk/servicenetworking/arm-servicenetworking/samples-dev/associationsInterfaceAssociationsInterfaceUpdateSample.ts
+++ sdk/servicenetworking/arm-servicenetworking/samples-dev/associationsInterfaceAssociationsInterfaceUpdateSample.ts
@@ -14,14 +14,19 @@ async function updateAssociation(): Promise<void> {
   const credential = new DefaultAzureCredential();
   const subscriptionId = "subid";
   const client = new ServiceNetworkingManagementClient(credential, subscriptionId);
-  const result = await client.associationsInterface.update("rg1", "tc1", "as1", {
-    properties: {
-      associationType: "subnets",
-      subnet: {
-        id: "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/virtualNetworks/vnet-tc/subnets/tc-subnet",
+  const result = await client.associationsInterface.AssociationsInterface_update(
+    "rg1",
+    "tc1",
+    "as1",
+    {
+      properties: {
+        associationType: "subnets",
+        subnet: {
+          id: "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/virtualNetworks/vnet-tc/subnets/tc-subnet",
+        },
       },
     },
-  });
+  );
   console.log(result);
 }
 
