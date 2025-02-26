982348dd24220fceb449c2cd226a33519dbe15ce
SDKAuto <sdkautomation@microsoft.com>
2025-02-26 00:19:43 +0000

    CodeGen from PR 32808 in Azure/azure-rest-api-specs Merge dc819d2d30965175ba8d7e4d3ef83c3644fd4e51 into 5eebf11c2c98feded3199df1a68ccf6d2c626d63

---
+++ sdk/commonedgesitemanageroperations/arm-commonedgesitemanageroperations/src/api/index.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export { createEdge, EdgeContext, EdgeClientOptionalParams } from "./edgeContext.js";
+export { OperationsListOptionalParams } from "./options.js";
+export { operationsList } from "./operations/index.js";
