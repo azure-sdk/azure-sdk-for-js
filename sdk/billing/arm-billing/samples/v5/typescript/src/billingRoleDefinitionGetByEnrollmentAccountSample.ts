d9e3f41d44aa401cc8f44467282428d2ebeb137b
SDKAuto <sdkautomation@microsoft.com>
2025-02-26 23:22:20 +0000

    CodeGen from PR 32870 in Azure/azure-rest-api-specs Merge a2da65fc5341b00150c59cd2aeb90301eaee8f0a into 2d73bd7fb1c68a8b6829fac1760bd0fc84ca5554

--- sdk/billing/arm-billing/samples/v5/typescript/src/billingRoleDefinitionGetByEnrollmentAccountSample.ts
+++ sdk/billing/arm-billing/samples/v5/typescript/src/billingRoleDefinitionGetByEnrollmentAccountSample.ts
@@ -15,7 +15,7 @@ import "dotenv/config";
  * @summary Gets the definition for a role on an enrollment account. The operation is supported for billing accounts with agreement type Enterprise Agreement.
  * x-ms-original-file: specification/billing/resource-manager/Microsoft.Billing/stable/2024-04-01/examples/billingRoleDefinitionGetByEnrollmentAccount.json
  */
-async function billingRoleDefinitionGetByEnrollmentAccount() {
+async function billingRoleDefinitionGetByEnrollmentAccount(): Promise<void> {
   const billingAccountName = "123456";
   const enrollmentAccountName = "4568789";
   const roleDefinitionName = "50000000-aaaa-bbbb-cccc-100000000000";
@@ -30,7 +30,7 @@ async function billingRoleDefinitionGetByEnrollmentAccount() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   billingRoleDefinitionGetByEnrollmentAccount();
 }
 
