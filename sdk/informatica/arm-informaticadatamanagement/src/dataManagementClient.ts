4fb60fa4777f9f0537e222f92882b097d6433104
SDKAuto <sdkautomation@microsoft.com>
2025-02-27 06:27:54 +0000

    CodeGen from PR 32871 in Azure/azure-rest-api-specs Merge 65ad9a2bd12495b6340db1b7ff600aa70b69c534 into c2afa6e1f7ced1b3c1fdd3518a9d49b44871d7a7

---
+++ sdk/informatica/arm-informaticadatamanagement/src/dataManagementClient.ts
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import {
+  _getServerlessRuntimesOperations,
+  ServerlessRuntimesOperations,
+} from "./classic/serverlessRuntimes/index.js";
+import {
+  _getOrganizationsOperations,
+  OrganizationsOperations,
+} from "./classic/organizations/index.js";
+import { _getOperationsOperations, OperationsOperations } from "./classic/operations/index.js";
+import {
+  createDataManagement,
+  DataManagementContext,
+  DataManagementClientOptionalParams,
+} from "./api/index.js";
+import { Pipeline } from "@azure/core-rest-pipeline";
+import { TokenCredential } from "@azure/core-auth";
+
+export { DataManagementClientOptionalParams } from "./api/dataManagementContext.js";
+
+export class DataManagementClient {
+  private _client: DataManagementContext;
+  /** The pipeline used by this client to make requests */
+  public readonly pipeline: Pipeline;
+
+  constructor(
+    credential: TokenCredential,
+    subscriptionId: string,
+    options: DataManagementClientOptionalParams = {},
+  ) {
+    const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
+    const userAgentPrefix = prefixFromOptions
+      ? `${prefixFromOptions} azsdk-js-client`
+      : `azsdk-js-client`;
+    this._client = createDataManagement(credential, subscriptionId, {
+      ...options,
+      userAgentOptions: { userAgentPrefix },
+    });
+    this.pipeline = this._client.pipeline;
+    this.serverlessRuntimes = _getServerlessRuntimesOperations(this._client);
+    this.organizations = _getOrganizationsOperations(this._client);
+    this.operations = _getOperationsOperations(this._client);
+  }
+
+  /** The operation groups for serverlessRuntimes */
+  public readonly serverlessRuntimes: ServerlessRuntimesOperations;
+  /** The operation groups for organizations */
+  public readonly organizations: OrganizationsOperations;
+  /** The operation groups for operations */
+  public readonly operations: OperationsOperations;
+}
