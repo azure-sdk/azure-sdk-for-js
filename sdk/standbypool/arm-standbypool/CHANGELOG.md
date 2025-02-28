307a64bf043f20c6b642b08bb6251217ede68a09
SDKAuto <sdkautomation@microsoft.com>
2025-02-28 02:07:41 +0000

    CodeGen from PR 32288 in Azure/azure-rest-api-specs Merge 53babc34cb2820cda1fe62aabd807f99939851c4 into 57323ac6c745ba3af373dd1f62fa924e79fb828c

--- sdk/standbypool/arm-standbypool/CHANGELOG.md
+++ sdk/standbypool/arm-standbypool/CHANGELOG.md
@@ -1,15 +1,48 @@
 # Release History
-
-## 1.0.2 (Unreleased)
-
+    
+## 2.0.0 (2025-02-28)
+    
 ### Features Added
+
+  - Added Interface ErrorAdditionalInfo
+  - Added Interface ErrorDetail
+  - Added Interface ErrorResponse
+  - Added Interface PoolContainerGroupStateCount
+  - Added Interface PoolStatus
+  - Added Interface PoolVirtualMachineStateCount
+  - Added Interface StandbyContainerGroupPoolForecastValues
+  - Added Interface StandbyContainerGroupPoolPrediction
+  - Added Interface StandbyVirtualMachinePoolForecastValues
+  - Added Interface StandbyVirtualMachinePoolPrediction
+  - Added Type Alias HealthStateCode
+  - Added Type Alias PoolContainerGroupState
+  - Added Type Alias PoolVirtualMachineState
+  - Interface ContainerGroupInstanceCountSummary has a new optional parameter zone
+  - Interface StandbyContainerGroupPoolResourceProperties has a new optional parameter zones
+  - Interface StandbyContainerGroupPoolResourceUpdateProperties has a new optional parameter zones
+  - Interface StandbyContainerGroupPoolRuntimeViewResourceProperties has a new optional parameter prediction
+  - Interface StandbyContainerGroupPoolRuntimeViewResourceProperties has a new optional parameter status
+  - Interface StandbyVirtualMachinePoolRuntimeViewResourceProperties has a new optional parameter prediction
+  - Interface StandbyVirtualMachinePoolRuntimeViewResourceProperties has a new optional parameter status
+  - Added Enum KnownHealthStateCode
+  - Added Enum KnownPoolContainerGroupState
+  - Added Enum KnownPoolVirtualMachineState
+  - Added Enum KnownVersions
+  - Enum KnownOrigin has a new value "user,system"
+  - Enum KnownOrigin has a new value system
+  - Enum KnownOrigin has a new value user
+  - Enum KnownRefillPolicy has a new value always
+  - Enum KnownVirtualMachineState has a new value Hibernated
 
 ### Breaking Changes
 
-### Bugs Fixed
-
-### Other Changes
-
+  - Type of parameter instanceCountsByState of interface ContainerGroupInstanceCountSummary is changed from PoolResourceStateCount[] to PoolContainerGroupStateCount[]
+  - Type of parameter instanceCountsByState of interface VirtualMachineInstanceCountSummary is changed from PoolResourceStateCount[] to PoolVirtualMachineStateCount[]
+  - Enum KnownOrigin no longer has value System
+  - Enum KnownOrigin no longer has value User
+  - Enum KnownOrigin no longer has value UserSystem
+  - Enum KnownRefillPolicy no longer has value Always
+    
 ## 1.0.1 (2024-11-04)
 
 ### Bugs Fixed
--- sdk/standbypool/arm-standbypool/src/api/standbyVirtualMachinePoolRuntimeViews/index.ts
+++ sdk/standbypool/arm-standbypool/src/api/standbyVirtualMachinePoolRuntimeViews/index.ts
@@ -2,115 +2,30 @@
 // Licensed under the MIT License.
 
 import {
-  StandbyVirtualMachinePoolRuntimeViewResource,
-  _StandbyVirtualMachinePoolRuntimeViewResourceListResult,
-} from "../../models/models.js";
-import { StandbyPoolContext as Client } from "../index.js";
+  StandbyPoolManagementContext as Client,
+  StandbyVirtualMachinePoolRuntimeViewsGetOptionalParams,
+  StandbyVirtualMachinePoolRuntimeViewsListByStandbyPoolOptionalParams,
+} from "../index.js";
 import {
-  StreamableMethod,
-  operationOptionsToRequestParameters,
-  PathUncheckedResponse,
-  createRestError,
-} from "@azure-rest/core-client";
+  errorResponseDeserializer,
+  StandbyVirtualMachinePoolRuntimeViewResource,
+  standbyVirtualMachinePoolRuntimeViewResourceDeserializer,
+  _StandbyVirtualMachinePoolRuntimeViewResourceListResult,
+  _standbyVirtualMachinePoolRuntimeViewResourceListResultDeserializer,
+} from "../../models/models.js";
 import {
   PagedAsyncIterableIterator,
   buildPagedAsyncIterator,
 } from "../../static-helpers/pagingHelpers.js";
 import {
-  StandbyVirtualMachinePoolRuntimeViewsGetOptionalParams,
-  StandbyVirtualMachinePoolRuntimeViewsListByStandbyPoolOptionalParams,
-} from "../../models/options.js";
-
-export function _standbyVirtualMachinePoolRuntimeViewsGetSend(
-  context: Client,
-  subscriptionId: string,
-  resourceGroupName: string,
-  standbyVirtualMachinePoolName: string,
-  runtimeView: string,
-  options: StandbyVirtualMachinePoolRuntimeViewsGetOptionalParams = {
-    requestOptions: {},
-  },
-): StreamableMethod {
-  return context
-    .path(
-      "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.StandbyPool/standbyVirtualMachinePools/{standbyVirtualMachinePoolName}/runtimeViews/{runtimeView}",
-      subscriptionId,
-      resourceGroupName,
-      standbyVirtualMachinePoolName,
-      runtimeView,
-    )
-    .get({ ...operationOptionsToRequestParameters(options) });
-}
-
-export async function _standbyVirtualMachinePoolRuntimeViewsGetDeserialize(
-  result: PathUncheckedResponse,
-): Promise<StandbyVirtualMachinePoolRuntimeViewResource> {
-  const expectedStatuses = ["200"];
-  if (!expectedStatuses.includes(result.status)) {
-    throw createRestError(result);
-  }
-
-  return {
-    id: result.body["id"],
-    name: result.body["name"],
-    type: result.body["type"],
-    systemData: !result.body.systemData
-      ? undefined
-      : {
-          createdBy: result.body.systemData?.["createdBy"],
-          createdByType: result.body.systemData?.["createdByType"],
-          createdAt:
-            result.body.systemData?.["createdAt"] !== undefined
-              ? new Date(result.body.systemData?.["createdAt"])
-              : undefined,
-          lastModifiedBy: result.body.systemData?.["lastModifiedBy"],
-          lastModifiedByType: result.body.systemData?.["lastModifiedByType"],
-          lastModifiedAt:
-            result.body.systemData?.["lastModifiedAt"] !== undefined
-              ? new Date(result.body.systemData?.["lastModifiedAt"])
-              : undefined,
-        },
-    properties: !result.body.properties
-      ? undefined
-      : {
-          instanceCountSummary: result.body.properties?.["instanceCountSummary"].map((p: any) => {
-            return {
-              zone: p["zone"],
-              instanceCountsByState: p["instanceCountsByState"].map((p: any) => {
-                return { state: p["state"], count: p["count"] };
-              }),
-            };
-          }),
-          provisioningState: result.body.properties?.["provisioningState"],
-        },
-  };
-}
-
-/** Get a StandbyVirtualMachinePoolRuntimeViewResource */
-export async function standbyVirtualMachinePoolRuntimeViewsGet(
-  context: Client,
-  subscriptionId: string,
-  resourceGroupName: string,
-  standbyVirtualMachinePoolName: string,
-  runtimeView: string,
-  options: StandbyVirtualMachinePoolRuntimeViewsGetOptionalParams = {
-    requestOptions: {},
-  },
-): Promise<StandbyVirtualMachinePoolRuntimeViewResource> {
-  const result = await _standbyVirtualMachinePoolRuntimeViewsGetSend(
-    context,
-    subscriptionId,
-    resourceGroupName,
-    standbyVirtualMachinePoolName,
-    runtimeView,
-    options,
-  );
-  return _standbyVirtualMachinePoolRuntimeViewsGetDeserialize(result);
-}
+  StreamableMethod,
+  PathUncheckedResponse,
+  createRestError,
+  operationOptionsToRequestParameters,
+} from "@azure-rest/core-client";
 
 export function _standbyVirtualMachinePoolRuntimeViewsListByStandbyPoolSend(
   context: Client,
-  subscriptionId: string,
   resourceGroupName: string,
   standbyVirtualMachinePoolName: string,
   options: StandbyVirtualMachinePoolRuntimeViewsListByStandbyPoolOptionalParams = {
@@ -120,11 +35,18 @@ export function _standbyVirtualMachinePoolRuntimeViewsListByStandbyPoolSend(
   return context
     .path(
       "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.StandbyPool/standbyVirtualMachinePools/{standbyVirtualMachinePoolName}/runtimeViews",
-      subscriptionId,
+      context.subscriptionId,
       resourceGroupName,
       standbyVirtualMachinePoolName,
     )
-    .get({ ...operationOptionsToRequestParameters(options) });
+    .get({
+      ...operationOptionsToRequestParameters(options),
+      headers: {
+        accept: "application/json",
+        ...options.requestOptions?.headers,
+      },
+      queryParameters: { "api-version": context.apiVersion },
+    });
 }
 
 export async function _standbyVirtualMachinePoolRuntimeViewsListByStandbyPoolDeserialize(
@@ -132,54 +54,17 @@ export async function _standbyVirtualMachinePoolRuntimeViewsListByStandbyPoolDes
 ): Promise<_StandbyVirtualMachinePoolRuntimeViewResourceListResult> {
   const expectedStatuses = ["200"];
   if (!expectedStatuses.includes(result.status)) {
-    throw createRestError(result);
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
   }
 
-  return {
-    value: result.body["value"].map((p: any) => {
-      return {
-        id: p["id"],
-        name: p["name"],
-        type: p["type"],
-        systemData: !p.systemData
-          ? undefined
-          : {
-              createdBy: p.systemData?.["createdBy"],
-              createdByType: p.systemData?.["createdByType"],
-              createdAt:
-                p.systemData?.["createdAt"] !== undefined
-                  ? new Date(p.systemData?.["createdAt"])
-                  : undefined,
-              lastModifiedBy: p.systemData?.["lastModifiedBy"],
-              lastModifiedByType: p.systemData?.["lastModifiedByType"],
-              lastModifiedAt:
-                p.systemData?.["lastModifiedAt"] !== undefined
-                  ? new Date(p.systemData?.["lastModifiedAt"])
-                  : undefined,
-            },
-        properties: !p.properties
-          ? undefined
-          : {
-              instanceCountSummary: p.properties?.["instanceCountSummary"].map((p: any) => {
-                return {
-                  zone: p["zone"],
-                  instanceCountsByState: p["instanceCountsByState"].map((p: any) => {
-                    return { state: p["state"], count: p["count"] };
-                  }),
-                };
-              }),
-              provisioningState: p.properties?.["provisioningState"],
-            },
-      };
-    }),
-    nextLink: result.body["nextLink"],
-  };
+  return _standbyVirtualMachinePoolRuntimeViewResourceListResultDeserializer(result.body);
 }
 
 /** List StandbyVirtualMachinePoolRuntimeViewResource resources by StandbyVirtualMachinePoolResource */
 export function standbyVirtualMachinePoolRuntimeViewsListByStandbyPool(
   context: Client,
-  subscriptionId: string,
   resourceGroupName: string,
   standbyVirtualMachinePoolName: string,
   options: StandbyVirtualMachinePoolRuntimeViewsListByStandbyPoolOptionalParams = {
@@ -191,7 +76,6 @@ export function standbyVirtualMachinePoolRuntimeViewsListByStandbyPool(
     () =>
       _standbyVirtualMachinePoolRuntimeViewsListByStandbyPoolSend(
         context,
-        subscriptionId,
         resourceGroupName,
         standbyVirtualMachinePoolName,
         options,
@@ -200,4 +84,64 @@ export function standbyVirtualMachinePoolRuntimeViewsListByStandbyPool(
     ["200"],
     { itemName: "value", nextLinkName: "nextLink" },
   );
+}
+
+export function _standbyVirtualMachinePoolRuntimeViewsGetSend(
+  context: Client,
+  resourceGroupName: string,
+  standbyVirtualMachinePoolName: string,
+  runtimeView: string,
+  options: StandbyVirtualMachinePoolRuntimeViewsGetOptionalParams = {
+    requestOptions: {},
+  },
+): StreamableMethod {
+  return context
+    .path(
+      "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.StandbyPool/standbyVirtualMachinePools/{standbyVirtualMachinePoolName}/runtimeViews/{runtimeView}",
+      context.subscriptionId,
+      resourceGroupName,
+      standbyVirtualMachinePoolName,
+      runtimeView,
+    )
+    .get({
+      ...operationOptionsToRequestParameters(options),
+      headers: {
+        accept: "application/json",
+        ...options.requestOptions?.headers,
+      },
+      queryParameters: { "api-version": context.apiVersion },
+    });
+}
+
+export async function _standbyVirtualMachinePoolRuntimeViewsGetDeserialize(
+  result: PathUncheckedResponse,
+): Promise<StandbyVirtualMachinePoolRuntimeViewResource> {
+  const expectedStatuses = ["200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return standbyVirtualMachinePoolRuntimeViewResourceDeserializer(result.body);
+}
+
+/** Get a StandbyVirtualMachinePoolRuntimeViewResource */
+export async function standbyVirtualMachinePoolRuntimeViewsGet(
+  context: Client,
+  resourceGroupName: string,
+  standbyVirtualMachinePoolName: string,
+  runtimeView: string,
+  options: StandbyVirtualMachinePoolRuntimeViewsGetOptionalParams = {
+    requestOptions: {},
+  },
+): Promise<StandbyVirtualMachinePoolRuntimeViewResource> {
+  const result = await _standbyVirtualMachinePoolRuntimeViewsGetSend(
+    context,
+    resourceGroupName,
+    standbyVirtualMachinePoolName,
+    runtimeView,
+    options,
+  );
+  return _standbyVirtualMachinePoolRuntimeViewsGetDeserialize(result);
 }
