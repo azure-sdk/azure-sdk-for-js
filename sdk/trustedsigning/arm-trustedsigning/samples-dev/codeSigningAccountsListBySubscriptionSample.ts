e5d5f06be49ca10bf7e6f9cd4e720d0702a09cbd
SDKAuto <sdkautomation@microsoft.com>
2025-02-28 11:58:02 +0000

    CodeGen from PR 32910 in Azure/azure-rest-api-specs Merge a2a291b369646b289871e5a6e2f8703cebf5dd60 into 1e9d5bcecf240bef9640970b35ecdeee05359247

--- sdk/trustedsigning/arm-trustedsigning/samples-dev/codeSigningAccountsListBySubscriptionSample.ts
+++ sdk/trustedsigning/arm-trustedsigning/samples-dev/codeSigningAccountsListBySubscriptionSample.ts
@@ -8,14 +8,14 @@ import { DefaultAzureCredential } from "@azure/identity";
  * This sample demonstrates how to lists trusted signing accounts within a subscription.
  *
  * @summary lists trusted signing accounts within a subscription.
- * x-ms-original-file: 2024-02-05-preview/CodeSigningAccounts_ListBySubscription.json
+ * x-ms-original-file: 2025-03-30/CodeSigningAccounts_ListBySubscription.json
  */
 async function listsTrustedSigningAccountsWithinASubscription(): Promise<void> {
   const credential = new DefaultAzureCredential();
   const subscriptionId = "00000000-1111-2222-3333-444444444444";
   const client = new CodeSigningClient(credential, subscriptionId);
   const resArray = new Array();
-  for await (const item of client.codeSigningAccounts.listBySubscription()) {
+  for await (let item of client.codeSigningAccounts.listBySubscription()) {
     resArray.push(item);
   }
 
