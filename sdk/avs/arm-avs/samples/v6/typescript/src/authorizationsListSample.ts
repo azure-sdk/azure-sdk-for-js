99dbf059c0a6c44befc9cc822cc4644c197543ae
SDKAuto <sdkautomation@microsoft.com>
2025-03-06 09:00:22 +0000

    CodeGen from PR 33049 in Azure/azure-rest-api-specs Merge 5aca89cb316352f0d902d8232b536830e6058c08 into 2a8bab30928d37d0a750dbdb7a1f78c8cfe104f5

--- sdk/avs/arm-avs/samples/v6/typescript/src/authorizationsListSample.ts
+++ sdk/avs/arm-avs/samples/v6/typescript/src/authorizationsListSample.ts
@@ -15,7 +15,7 @@ import "dotenv/config";
  * @summary List ExpressRouteAuthorization resources by PrivateCloud
  * x-ms-original-file: specification/vmware/resource-manager/Microsoft.AVS/stable/2023-09-01/examples/Authorizations_List.json
  */
-async function authorizationsList() {
+async function authorizationsList(): Promise<void> {
   const subscriptionId =
     process.env["AVS_SUBSCRIPTION_ID"] ||
     "00000000-0000-0000-0000-000000000000";
@@ -33,7 +33,7 @@ async function authorizationsList() {
   console.log(resArray);
 }
 
-async function main() {
+async function main(): Promise<void> {
   authorizationsList();
 }
 
