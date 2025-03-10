f3e56aeb4de7a3428fe4d015a10cd3404dadd25f
SDKAuto <sdkautomation@microsoft.com>
2025-03-10 06:37:05 +0000

    CodeGen from PR 32939 in Azure/azure-rest-api-specs Merge 78d83f28834f55232952867d8d2cb80fdc3b17a3 into 0247110f15f6558fa2eeaff18a0515e883ed6517

--- sdk/eventgrid/eventgrid/test/public/utils/recordedClient.ts
+++ sdk/eventgrid/eventgrid/test/public/utils/recordedClient.ts
@@ -1,87 +1,23 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import type {
-  FindReplaceSanitizer,
-  RecorderStartOptions,
-  TestInfo,
-} from "@azure-tools/test-recorder";
-import { assertEnvironmentVariable, Recorder } from "@azure-tools/test-recorder";
-import type { InputSchema } from "../../../src/index.js";
-import { EventGridPublisherClient } from "../../../src/index.js";
-import { createTestCredential } from "@azure-tools/test-credential";
-import type { AdditionalPolicyConfig } from "@azure/core-client";
+import { Recorder, RecorderStartOptions, VitestTestContext } from "@azure-tools/test-recorder";
 
-export interface RecordedClient<T extends InputSchema> {
-  client: EventGridPublisherClient<T>;
-  recorder: Recorder;
-}
+const replaceableVariables: Record<string, string> = {
+  SUBSCRIPTION_ID: "azure_subscription_id",
+};
 
-const envSetupForPlayback: { [k: string]: string } = {
-  EVENT_GRID_EVENT_GRID_SCHEMA_ENDPOINT: "https://endpoint/api/events",
-  EVENT_GRID_CLOUD_EVENT_SCHEMA_ENDPOINT: "https://endpoint/api/events",
-  EVENT_GRID_CUSTOM_SCHEMA_ENDPOINT: "https://endpoint/api/events",
+const recorderEnvSetup: RecorderStartOptions = {
+  envSetupForPlayback: replaceableVariables,
 };
 
 /**
- * In some tests the endpoints appear without the /api/events suffix
- * so make sure to sanitize them in this case as well (it doesn't get
- * covered automatically by envSetupForPlayback).
+ * creates the recorder and reads the environment variables from the `.env` file.
+ * Should be called first in the test suite to make sure environment variables are
+ * read before they are being used.
  */
-const suffixlessEndpointSanitizer = (endpointEnv: string): FindReplaceSanitizer => ({
-  target: removeApiEventsSuffix(assertEnvironmentVariable(endpointEnv)),
-  value: "https://endpoint",
-});
-
-export const recorderOptions: RecorderStartOptions = {
-  envSetupForPlayback,
-  removeCentralSanitizers: [
-    "AZSDK3493", // .name in the body is not a secret and is listed below in the beforeEach section
-    "AZSDK3430", // .id in the body is not a secret and is listed below in the beforeEach section
-    "AZSDK4001",
-  ],
-};
-
-export async function createRecordedClient<T extends InputSchema>(
-  currentTest: TestInfo | undefined,
-  endpointEnv: string,
-  eventSchema: T,
-  options: {
-    removeApiEventsSuffixBool?: boolean;
-    additionalPolicies?: AdditionalPolicyConfig[];
-  } = {},
-): Promise<RecordedClient<T>> {
-  const recorder = new Recorder(currentTest);
-  await recorder.start(recorderOptions);
-  await recorder.addSanitizers({
-    generalSanitizers: [
-      suffixlessEndpointSanitizer("EVENT_GRID_EVENT_GRID_SCHEMA_ENDPOINT"),
-      suffixlessEndpointSanitizer("EVENT_GRID_CLOUD_EVENT_SCHEMA_ENDPOINT"),
-      suffixlessEndpointSanitizer("EVENT_GRID_CUSTOM_SCHEMA_ENDPOINT"),
-    ],
-  });
-
-  return {
-    client: new EventGridPublisherClient(
-      options.removeApiEventsSuffixBool
-        ? removeApiEventsSuffix(assertEnvironmentVariable(endpointEnv))
-        : assertEnvironmentVariable(endpointEnv),
-      eventSchema,
-      createTestCredential(),
-      recorder.configureClientOptions({
-        additionalPolicies: options.additionalPolicies,
-      }),
-    ),
-    recorder,
-  };
-}
-
-function removeApiEventsSuffix(endpoint: string): string {
-  const suffix = "/api/events";
-
-  if (!endpoint.endsWith(suffix)) {
-    throw new Error(`${endpoint} does not end with ${suffix}`);
-  }
-
-  return endpoint.substring(0, endpoint.length - suffix.length);
+export async function createRecorder(context: VitestTestContext): Promise<Recorder> {
+  const recorder = new Recorder(context);
+  await recorder.start(recorderEnvSetup);
+  return recorder;
 }
