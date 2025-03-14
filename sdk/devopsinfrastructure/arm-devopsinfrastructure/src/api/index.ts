1d80e73bf67f7ba5da49023b530f2deb4e090dc9
SDKAuto <sdkautomation@microsoft.com>
2025-03-14 09:05:55 +0000

    CodeGen from PR 33210 in Azure/azure-rest-api-specs Merge 1105d35d8e73a067d55ae2d11e8cc85b8ee2416d into 535377f6ef91e9d847bbee77d55871eb8b37b7b2

--- sdk/devopsinfrastructure/arm-devopsinfrastructure/src/api/index.ts
+++ sdk/devopsinfrastructure/arm-devopsinfrastructure/src/api/index.ts
@@ -7,28 +7,16 @@ export {
   DevOpsInfrastructureClientOptionalParams,
 } from "./devOpsInfrastructureContext.js";
 export {
-  OperationsListOptionalParams,
-  PoolsGetOptionalParams,
-  PoolsCreateOrUpdateOptionalParams,
-  PoolsUpdateOptionalParams,
-  PoolsDeleteOptionalParams,
-  PoolsListByResourceGroupOptionalParams,
-  PoolsListBySubscriptionOptionalParams,
-  ResourceDetailsListByPoolOptionalParams,
-  SkuListByLocationOptionalParams,
-  SubscriptionUsagesUsagesOptionalParams,
   ImageVersionsListByImageOptionalParams,
+  SubscriptionUsagesUsagesOptionalParams,
+  SkuListByLocationOptionalParams,
+  ResourceDetailsListByPoolOptionalParams,
+  PoolsCheckNameAvailabilityOptionalParams,
+  PoolsListBySubscriptionOptionalParams,
+  PoolsListByResourceGroupOptionalParams,
+  PoolsDeleteOptionalParams,
+  PoolsUpdateOptionalParams,
+  PoolsCreateOrUpdateOptionalParams,
+  PoolsGetOptionalParams,
+  OperationsListOptionalParams,
 } from "./options.js";
-export { imageVersionsListByImage } from "./imageVersions/index.js";
-export { operationsList } from "./operations/index.js";
-export {
-  poolsGet,
-  poolsCreateOrUpdate,
-  poolsUpdate,
-  poolsDelete,
-  poolsListByResourceGroup,
-  poolsListBySubscription,
-} from "./pools/index.js";
-export { resourceDetailsListByPool } from "./resourceDetails/index.js";
-export { skuListByLocation } from "./sku/index.js";
-export { subscriptionUsagesUsages } from "./subscriptionUsages/index.js";
--- sdk/devopsinfrastructure/arm-devopsinfrastructure/src/classic/operations/index.ts
+++ sdk/devopsinfrastructure/arm-devopsinfrastructure/src/classic/operations/index.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 import { DevOpsInfrastructureContext } from "../../api/devOpsInfrastructureContext.js";
-import { operationsList } from "../../api/operations/index.js";
+import { list } from "../../api/operations/index.js";
 import { OperationsListOptionalParams } from "../../api/options.js";
 import { Operation } from "../../models/models.js";
 import { PagedAsyncIterableIterator } from "../../static-helpers/pagingHelpers.js";
@@ -13,16 +13,16 @@ export interface OperationsOperations {
   list: (options?: OperationsListOptionalParams) => PagedAsyncIterableIterator<Operation>;
 }
 
-export function getOperations(context: DevOpsInfrastructureContext) {
+function _getOperations(context: DevOpsInfrastructureContext) {
   return {
-    list: (options?: OperationsListOptionalParams) => operationsList(context, options),
+    list: (options?: OperationsListOptionalParams) => list(context, options),
   };
 }
 
-export function getOperationsOperations(
+export function _getOperationsOperations(
   context: DevOpsInfrastructureContext,
 ): OperationsOperations {
   return {
-    ...getOperations(context),
+    ..._getOperations(context),
   };
 }
