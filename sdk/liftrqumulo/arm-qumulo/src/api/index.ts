f78ed5bb8dc7b5d3bddd3ce00775cd9e2fecc02f
SDKAuto <sdkautomation@microsoft.com>
2025-03-14 09:57:13 +0000

    CodeGen from PR 33210 in Azure/azure-rest-api-specs Merge 1105d35d8e73a067d55ae2d11e8cc85b8ee2416d into 535377f6ef91e9d847bbee77d55871eb8b37b7b2

---
+++ sdk/liftrqumulo/arm-qumulo/src/api/index.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  FileSystemsListBySubscriptionOptionalParams,
+  FileSystemsListByResourceGroupOptionalParams,
+  FileSystemsDeleteOptionalParams,
+  FileSystemsUpdateOptionalParams,
+  FileSystemsCreateOrUpdateOptionalParams,
+  FileSystemsGetOptionalParams,
+  OperationsListOptionalParams,
+} from "./options.js";
+export { createStorage, StorageContext, StorageClientOptionalParams } from "./storageContext.js";
