100cfbda3d72df6f0e6db89d2342a9132500c66e
SDKAuto <sdkautomation@microsoft.com>
2025-03-14 09:11:46 +0000

    CodeGen from PR 33210 in Azure/azure-rest-api-specs Merge 1105d35d8e73a067d55ae2d11e8cc85b8ee2416d into 535377f6ef91e9d847bbee77d55871eb8b37b7b2

---
+++ sdk/sitemanager/arm-sitemanager/src/api/index.ts
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export { createEdge, EdgeContext, EdgeClientOptionalParams } from "./edgeContext.js";
+export {
+  SitesBySubscriptionDeleteOptionalParams,
+  SitesBySubscriptionUpdateOptionalParams,
+  SitesBySubscriptionCreateOrUpdateOptionalParams,
+  SitesBySubscriptionGetOptionalParams,
+  SitesBySubscriptionListOptionalParams,
+  SitesListByResourceGroupOptionalParams,
+  SitesDeleteOptionalParams,
+  SitesUpdateOptionalParams,
+  SitesCreateOrUpdateOptionalParams,
+  SitesGetOptionalParams,
+} from "./options.js";
