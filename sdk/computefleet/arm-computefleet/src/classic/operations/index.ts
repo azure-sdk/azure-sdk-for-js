b6b240bf628037edcac3a2b1ea889ae276736699
SDKAuto <sdkautomation@microsoft.com>
2025-03-14 08:45:52 +0000

    CodeGen from PR 33210 in Azure/azure-rest-api-specs Merge 1105d35d8e73a067d55ae2d11e8cc85b8ee2416d into 535377f6ef91e9d847bbee77d55871eb8b37b7b2

--- sdk/computefleet/arm-computefleet/src/classic/operations/index.ts
+++ sdk/computefleet/arm-computefleet/src/classic/operations/index.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 import { AzureFleetContext } from "../../api/azureFleetContext.js";
-import { operationsList } from "../../api/operations/index.js";
+import { list } from "../../api/operations/index.js";
 import { OperationsListOptionalParams } from "../../api/options.js";
 import { Operation } from "../../models/models.js";
 import { PagedAsyncIterableIterator } from "../../static-helpers/pagingHelpers.js";
@@ -13,14 +13,14 @@ export interface OperationsOperations {
   list: (options?: OperationsListOptionalParams) => PagedAsyncIterableIterator<Operation>;
 }
 
-export function getOperations(context: AzureFleetContext) {
+function _getOperations(context: AzureFleetContext) {
   return {
-    list: (options?: OperationsListOptionalParams) => operationsList(context, options),
+    list: (options?: OperationsListOptionalParams) => list(context, options),
   };
 }
 
-export function getOperationsOperations(context: AzureFleetContext): OperationsOperations {
+export function _getOperationsOperations(context: AzureFleetContext): OperationsOperations {
   return {
-    ...getOperations(context),
+    ..._getOperations(context),
   };
 }
