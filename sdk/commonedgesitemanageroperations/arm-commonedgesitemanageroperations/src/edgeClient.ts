8029acfdba0a3af9ca5eba32302c96abaa96609c
SDKAuto <sdkautomation@microsoft.com>
2025-03-14 03:18:01 +0000

    CodeGen from PR 32938 in Azure/azure-rest-api-specs Merge 0a04a5454a0f5616f7cf7d6e728c8d0b7a8c12ba into 535377f6ef91e9d847bbee77d55871eb8b37b7b2

---
+++ sdk/commonedgesitemanageroperations/arm-commonedgesitemanageroperations/src/edgeClient.ts
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { _getOperationsOperations, OperationsOperations } from "./classic/operations/index.js";
+import { createEdge, EdgeContext, EdgeClientOptionalParams } from "./api/index.js";
+import { Pipeline } from "@azure/core-rest-pipeline";
+import { TokenCredential } from "@azure/core-auth";
+
+export { EdgeClientOptionalParams } from "./api/edgeContext.js";
+
+export class EdgeClient {
+  private _client: EdgeContext;
+  /** The pipeline used by this client to make requests */
+  public readonly pipeline: Pipeline;
+
+  constructor(credential: TokenCredential, options: EdgeClientOptionalParams = {}) {
+    const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
+    const userAgentPrefix = prefixFromOptions
+      ? `${prefixFromOptions} azsdk-js-client`
+      : `azsdk-js-client`;
+    this._client = createEdge(credential, {
+      ...options,
+      userAgentOptions: { userAgentPrefix },
+    });
+    this.pipeline = this._client.pipeline;
+    this.operations = _getOperationsOperations(this._client);
+  }
+
+  /** The operation groups for operations */
+  public readonly operations: OperationsOperations;
+}
