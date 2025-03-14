94a6e9ed27cb94b18c0ef77ebcbe1adfefe8c020
SDKAuto <sdkautomation@microsoft.com>
2025-03-14 09:08:00 +0000

    CodeGen from PR 33210 in Azure/azure-rest-api-specs Merge 1105d35d8e73a067d55ae2d11e8cc85b8ee2416d into 535377f6ef91e9d847bbee77d55871eb8b37b7b2

---
+++ sdk/durabletask/arm-durabletask/src/api/index.ts
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  createDurableTask,
+  DurableTaskContext,
+  DurableTaskClientOptionalParams,
+} from "./durableTaskContext.js";
+export {
+  TaskHubsListBySchedulerOptionalParams,
+  TaskHubsDeleteOptionalParams,
+  TaskHubsCreateOrUpdateOptionalParams,
+  TaskHubsGetOptionalParams,
+  SchedulersListBySubscriptionOptionalParams,
+  SchedulersListByResourceGroupOptionalParams,
+  SchedulersDeleteOptionalParams,
+  SchedulersUpdateOptionalParams,
+  SchedulersCreateOrUpdateOptionalParams,
+  SchedulersGetOptionalParams,
+  OperationsListOptionalParams,
+} from "./options.js";
