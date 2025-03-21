c2cbabd6ec8c870c5d5869b70c08eb83a737011f
SDKAuto <sdkautomation@microsoft.com>
2025-03-21 07:41:58 +0000

    CodeGen from PR 33344 in Azure/azure-rest-api-specs Merge bfafe9e2f8a8a553ae4a2758d2e2870200ed7024 into 10907bf313018df38cd1648f862455433567f277

---
+++ sdk/hybridconnectivity/arm-hybridconnectivity/src/api/inventory/index.ts
@@ -0,0 +1,150 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import {
+  HybridConnectivityManagementAPIContext as Client,
+  InventoryGetOptionalParams,
+  InventoryListBySolutionConfigurationOptionalParams,
+} from "../index.js";
+import {
+  errorResponseDeserializer,
+  InventoryResource,
+  inventoryResourceDeserializer,
+  _InventoryResourceListResult,
+  _inventoryResourceListResultDeserializer,
+} from "../../models/models.js";
+import {
+  PagedAsyncIterableIterator,
+  buildPagedAsyncIterator,
+} from "../../static-helpers/pagingHelpers.js";
+import { expandUrlTemplate } from "../../static-helpers/urlTemplate.js";
+import {
+  StreamableMethod,
+  PathUncheckedResponse,
+  createRestError,
+  operationOptionsToRequestParameters,
+} from "@azure-rest/core-client";
+
+export function _inventoryListBySolutionConfigurationSend(
+  context: Client,
+  resourceUri: string,
+  solutionConfiguration: string,
+  options: InventoryListBySolutionConfigurationOptionalParams = {
+    requestOptions: {},
+  },
+): StreamableMethod {
+  const path = expandUrlTemplate(
+    "/{+resourceUri}/providers/Microsoft.HybridConnectivity/solutionConfigurations/{solutionConfiguration}/inventory{?api-version}",
+    {
+      resourceUri: resourceUri,
+      solutionConfiguration: solutionConfiguration,
+      "api-version": context.apiVersion,
+    },
+    {
+      allowReserved: options?.requestOptions?.skipUrlEncoding,
+    },
+  );
+  return context.path(path).get({
+    ...operationOptionsToRequestParameters(options),
+    headers: {
+      accept: "application/json",
+      ...options.requestOptions?.headers,
+    },
+  });
+}
+
+export async function _inventoryListBySolutionConfigurationDeserialize(
+  result: PathUncheckedResponse,
+): Promise<_InventoryResourceListResult> {
+  const expectedStatuses = ["200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return _inventoryResourceListResultDeserializer(result.body);
+}
+
+/** List InventoryResource resources by SolutionConfiguration */
+export function inventoryListBySolutionConfiguration(
+  context: Client,
+  resourceUri: string,
+  solutionConfiguration: string,
+  options: InventoryListBySolutionConfigurationOptionalParams = {
+    requestOptions: {},
+  },
+): PagedAsyncIterableIterator<InventoryResource> {
+  return buildPagedAsyncIterator(
+    context,
+    () =>
+      _inventoryListBySolutionConfigurationSend(
+        context,
+        resourceUri,
+        solutionConfiguration,
+        options,
+      ),
+    _inventoryListBySolutionConfigurationDeserialize,
+    ["200"],
+    { itemName: "value", nextLinkName: "nextLink" },
+  );
+}
+
+export function _inventoryGetSend(
+  context: Client,
+  resourceUri: string,
+  solutionConfiguration: string,
+  inventoryId: string,
+  options: InventoryGetOptionalParams = { requestOptions: {} },
+): StreamableMethod {
+  const path = expandUrlTemplate(
+    "/{+resourceUri}/providers/Microsoft.HybridConnectivity/solutionConfigurations/{solutionConfiguration}/inventory/{inventoryId}{?api-version}",
+    {
+      resourceUri: resourceUri,
+      solutionConfiguration: solutionConfiguration,
+      inventoryId: inventoryId,
+      "api-version": context.apiVersion,
+    },
+    {
+      allowReserved: options?.requestOptions?.skipUrlEncoding,
+    },
+  );
+  return context.path(path).get({
+    ...operationOptionsToRequestParameters(options),
+    headers: {
+      accept: "application/json",
+      ...options.requestOptions?.headers,
+    },
+  });
+}
+
+export async function _inventoryGetDeserialize(
+  result: PathUncheckedResponse,
+): Promise<InventoryResource> {
+  const expectedStatuses = ["200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return inventoryResourceDeserializer(result.body);
+}
+
+/** Get a InventoryResource */
+export async function inventoryGet(
+  context: Client,
+  resourceUri: string,
+  solutionConfiguration: string,
+  inventoryId: string,
+  options: InventoryGetOptionalParams = { requestOptions: {} },
+): Promise<InventoryResource> {
+  const result = await _inventoryGetSend(
+    context,
+    resourceUri,
+    solutionConfiguration,
+    inventoryId,
+    options,
+  );
+  return _inventoryGetDeserialize(result);
+}
