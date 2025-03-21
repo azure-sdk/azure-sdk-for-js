1c06a80f2d52cf61cb0c56192cd103b4fe855799
SDKAuto <sdkautomation@microsoft.com>
2025-03-21 07:32:29 +0000

    CodeGen from PR 33344 in Azure/azure-rest-api-specs Merge bfafe9e2f8a8a553ae4a2758d2e2870200ed7024 into 10907bf313018df38cd1648f862455433567f277

--- sdk/trustedsigning/arm-trustedsigning/samples-dev/certificateProfilesGetSample.ts
+++ sdk/trustedsigning/arm-trustedsigning/samples-dev/certificateProfilesGetSample.ts
@@ -8,7 +8,7 @@ import { DefaultAzureCredential } from "@azure/identity";
  * This sample demonstrates how to get details of a certificate profile.
  *
  * @summary get details of a certificate profile.
- * x-ms-original-file: 2024-02-05-preview/CertificateProfiles_Get.json
+ * x-ms-original-file: 2024-09-30-preview/CertificateProfiles_Get.json
  */
 async function getDetailsOfACertificateProfile(): Promise<void> {
   const credential = new DefaultAzureCredential();
