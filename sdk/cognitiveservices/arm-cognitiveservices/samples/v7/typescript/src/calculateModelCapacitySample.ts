32545ac1789526880f7501c627ecb57bfd58631f
SDKAuto <sdkautomation@microsoft.com>
2025-03-21 21:31:39 +0000

    CodeGen from PR 33108 in Azure/azure-rest-api-specs Merge 3b48b4cb9d62580de4b82945103efe36780d4495 into c09d0a18ebabb763855e74038e1825ebee6a372a

--- sdk/cognitiveservices/arm-cognitiveservices/samples/v7/typescript/src/calculateModelCapacitySample.ts
+++ sdk/cognitiveservices/arm-cognitiveservices/samples/v7/typescript/src/calculateModelCapacitySample.ts
@@ -18,7 +18,7 @@ import "dotenv/config";
  * @summary Model capacity calculator.
  * x-ms-original-file: specification/cognitiveservices/resource-manager/Microsoft.CognitiveServices/stable/2024-10-01/examples/CalculateModelCapacity.json
  */
-async function calculateModelCapacity() {
+async function calculateModelCapacity(): Promise<void> {
   const subscriptionId =
     process.env["COGNITIVESERVICES_SUBSCRIPTION_ID"] ||
     "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
@@ -48,7 +48,7 @@ async function calculateModelCapacity() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   calculateModelCapacity();
 }
 
