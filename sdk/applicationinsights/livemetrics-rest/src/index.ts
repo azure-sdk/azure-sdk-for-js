62384badcf0635b3ac608083874a05fdff647f8c
SDKAuto <sdkautomation@microsoft.com>
2025-03-24 07:15:50 +0000

    CodeGen from PR 33441 in Azure/azure-rest-api-specs Merge 61986c0207f48566fd3f0238305e2ec097cb7ac1 into bc79b816272c7e12d79e8bc63d689327fc37d11f

---
+++ sdk/applicationinsights/livemetrics-rest/src/index.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import LiveMetricsClient from "./liveMetricsClient.js";
+
+export * from "./liveMetricsClient.js";
+export * from "./parameters.js";
+export * from "./responses.js";
+export * from "./clientDefinitions.js";
+export * from "./isUnexpected.js";
+export * from "./models.js";
+export * from "./outputModels.js";
+
+export default LiveMetricsClient;
