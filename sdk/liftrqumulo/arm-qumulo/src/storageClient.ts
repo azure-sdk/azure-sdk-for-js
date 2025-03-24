f1189b4154dff73b728aeb9e79ae441a35a41eb1
SDKAuto <sdkautomation@microsoft.com>
2025-03-24 08:54:00 +0000

    CodeGen from PR 33441 in Azure/azure-rest-api-specs Merge 61986c0207f48566fd3f0238305e2ec097cb7ac1 into bc79b816272c7e12d79e8bc63d689327fc37d11f

---
+++ sdk/liftrqumulo/arm-qumulo/src/storageClient.ts
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { _getFileSystemsOperations, FileSystemsOperations } from "./classic/fileSystems/index.js";
+import { _getOperationsOperations, OperationsOperations } from "./classic/operations/index.js";
+import { createStorage, StorageContext, StorageClientOptionalParams } from "./api/index.js";
+import { Pipeline } from "@azure/core-rest-pipeline";
+import { TokenCredential } from "@azure/core-auth";
+
+export { StorageClientOptionalParams } from "./api/storageContext.js";
+
+export class StorageClient {
+  private _client: StorageContext;
+  /** The pipeline used by this client to make requests */
+  public readonly pipeline: Pipeline;
+
+  constructor(
+    credential: TokenCredential,
+    subscriptionId: string,
+    options: StorageClientOptionalParams = {},
+  ) {
+    const prefixFromOptions = options?.userAgentOptions?.userAgentPrefix;
+    const userAgentPrefix = prefixFromOptions
+      ? `${prefixFromOptions} azsdk-js-client`
+      : `azsdk-js-client`;
+    this._client = createStorage(credential, subscriptionId, {
+      ...options,
+      userAgentOptions: { userAgentPrefix },
+    });
+    this.pipeline = this._client.pipeline;
+    this.fileSystems = _getFileSystemsOperations(this._client);
+    this.operations = _getOperationsOperations(this._client);
+  }
+
+  /** The operation groups for fileSystems */
+  public readonly fileSystems: FileSystemsOperations;
+  /** The operation groups for operations */
+  public readonly operations: OperationsOperations;
+}
