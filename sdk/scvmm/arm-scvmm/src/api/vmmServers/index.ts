68375c419e2508fb228152b8de6fb59b2c3b0910
SDKAuto <sdkautomation@microsoft.com>
2025-03-25 10:47:54 +0000

    CodeGen from PR 33210 in Azure/azure-rest-api-specs Merge 10196a6e832394cb261f333ebdd0cbb2c0bfbaaa into c56bcb994cdb222ae05e428096f0804d3c68f78c

---
+++ sdk/scvmm/arm-scvmm/src/api/vmmServers/index.ts
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  listBySubscription,
+  listByResourceGroup,
+  $delete,
+  update,
+  createOrUpdate,
+  get,
+} from "./operations.js";
+export {
+  VmmServersListBySubscriptionOptionalParams,
+  VmmServersListByResourceGroupOptionalParams,
+  VmmServersDeleteOptionalParams,
+  VmmServersUpdateOptionalParams,
+  VmmServersCreateOrUpdateOptionalParams,
+  VmmServersGetOptionalParams,
+} from "./options.js";
