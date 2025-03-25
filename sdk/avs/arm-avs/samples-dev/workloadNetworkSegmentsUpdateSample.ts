f7238565622c01ccde244b5c2efabe8391dd5df7
SDKAuto <sdkautomation@microsoft.com>
2025-03-25 11:02:43 +0000

    CodeGen from PR 33210 in Azure/azure-rest-api-specs Merge 10196a6e832394cb261f333ebdd0cbb2c0bfbaaa into c56bcb994cdb222ae05e428096f0804d3c68f78c

---
+++ sdk/avs/arm-avs/samples-dev/workloadNetworkSegmentsUpdateSample.ts
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { AzureVMwareSolutionAPIClient } from "@azure/arm-avs";
+import { DefaultAzureCredential } from "@azure/identity";
+
+/**
+ * This sample demonstrates how to update a WorkloadNetworkSegment
+ *
+ * @summary update a WorkloadNetworkSegment
+ * x-ms-original-file: 2023-09-01/WorkloadNetworks_UpdateSegments.json
+ */
+async function workloadNetworksUpdateSegments(): Promise<void> {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new AzureVMwareSolutionAPIClient(credential, subscriptionId);
+  const result = await client.workloadNetworkSegments.update("group1", "cloud1", "segment1");
+  console.log(result);
+}
+
+async function main(): Promise<void> {
+  await workloadNetworksUpdateSegments();
+}
+
+main().catch(console.error);
