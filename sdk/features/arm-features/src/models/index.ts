0494990773391b0b411ea3b89ee41a03ff398725
SDKAuto <sdkautomation@microsoft.com>
2025-03-26 14:56:57 +0000

    CodeGen from PR 33351 in Azure/azure-rest-api-specs Merge 416d88fcdd677ac65419c6af81ae14ff5cbeea1b into 45924e49834c4e01c0713e6b7ca21f94be17e396

--- sdk/features/arm-features/src/models/index.ts
+++ sdk/features/arm-features/src/models/index.ts
@@ -8,80 +8,6 @@
 
 import * as coreClient from "@azure/core-client";
 
-/** Result of the request to list Microsoft.Features operations. It contains a list of operations and a URL link to get the next set of results. */
-export interface OperationListResult {
-  /** List of Microsoft.Features operations. */
-  value?: Operation[];
-  /** URL to get the next set of operation list results if there are any. */
-  nextLink?: string;
-}
-
-/** Microsoft.Features operation */
-export interface Operation {
-  /** Operation name: {provider}/{resource}/{operation} */
-  name?: string;
-  /** The object that represents the operation. */
-  display?: OperationDisplay;
-}
-
-/** The object that represents the operation. */
-export interface OperationDisplay {
-  /** Service provider: Microsoft.Features */
-  provider?: string;
-  /** Resource on which the operation is performed: Profile, endpoint, etc. */
-  resource?: string;
-  /** Operation type: Read, write, delete, etc. */
-  operation?: string;
-}
-
-/** Error response indicates that the service is not able to process the incoming request. */
-export interface ErrorResponse {
-  /** The error details. */
-  error?: ErrorDefinition;
-}
-
-/** Error definition. */
-export interface ErrorDefinition {
-  /**
-   * Service specific error code which serves as the substatus for the HTTP error code.
-   * NOTE: This property will not be serialized. It can only be populated by the server.
-   */
-  readonly code?: string;
-  /**
-   * Description of the error.
-   * NOTE: This property will not be serialized. It can only be populated by the server.
-   */
-  readonly message?: string;
-  /** Internal error details. */
-  details?: ErrorDefinition[];
-}
-
-/** List of previewed features. */
-export interface FeatureOperationsListResult {
-  /** The array of features. */
-  value?: FeatureResult[];
-  /** The URL to use for getting the next set of results. */
-  nextLink?: string;
-}
-
-/** Previewed feature information. */
-export interface FeatureResult {
-  /** The name of the feature. */
-  name?: string;
-  /** Properties of the previewed feature. */
-  properties?: FeatureProperties;
-  /** The resource ID of the feature. */
-  id?: string;
-  /** The resource type of the feature. */
-  type?: string;
-}
-
-/** Information about feature. */
-export interface FeatureProperties {
-  /** The registration state of the feature for the subscription. */
-  state?: string;
-}
-
 export interface SubscriptionFeatureRegistrationProperties {
   /**
    * The tenantId.
@@ -188,6 +114,28 @@ export interface ProxyResource {
   readonly type?: string;
 }
 
+/** Error response indicates that the service is not able to process the incoming request. */
+export interface ErrorResponse {
+  /** The error details. */
+  error?: ErrorDefinition;
+}
+
+/** Error definition. */
+export interface ErrorDefinition {
+  /**
+   * Service specific error code which serves as the substatus for the HTTP error code.
+   * NOTE: This property will not be serialized. It can only be populated by the server.
+   */
+  readonly code?: string;
+  /**
+   * Description of the error.
+   * NOTE: This property will not be serialized. It can only be populated by the server.
+   */
+  readonly message?: string;
+  /** Internal error details. */
+  details?: ErrorDefinition[];
+}
+
 /** The list of subscription feature registrations. */
 export interface SubscriptionFeatureRegistrationList {
   /** The link used to get the next page of subscription feature registrations list. */
@@ -209,14 +157,10 @@ export enum KnownSubscriptionFeatureRegistrationState {
   NotRegistered = "NotRegistered",
   /** Pending */
   Pending = "Pending",
-  /** Registering */
-  Registering = "Registering",
   /** Registered */
   Registered = "Registered",
-  /** Unregistering */
-  Unregistering = "Unregistering",
   /** Unregistered */
-  Unregistered = "Unregistered"
+  Unregistered = "Unregistered",
 }
 
 /**
@@ -227,9 +171,7 @@ export enum KnownSubscriptionFeatureRegistrationState {
  * **NotSpecified** \
  * **NotRegistered** \
  * **Pending** \
- * **Registering** \
  * **Registered** \
- * **Unregistering** \
  * **Unregistered**
  */
 export type SubscriptionFeatureRegistrationState = string;
@@ -241,7 +183,7 @@ export enum KnownSubscriptionFeatureRegistrationApprovalType {
   /** ApprovalRequired */
   ApprovalRequired = "ApprovalRequired",
   /** AutoApproval */
-  AutoApproval = "AutoApproval"
+  AutoApproval = "AutoApproval",
 }
 
 /**
@@ -256,74 +198,12 @@ export enum KnownSubscriptionFeatureRegistrationApprovalType {
 export type SubscriptionFeatureRegistrationApprovalType = string;
 
 /** Optional parameters. */
-export interface ListOperationsOptionalParams
-  extends coreClient.OperationOptions {}
-
-/** Contains response data for the listOperations operation. */
-export type ListOperationsResponse = OperationListResult;
-
-/** Optional parameters. */
-export interface ListOperationsNextOptionalParams
-  extends coreClient.OperationOptions {}
-
-/** Contains response data for the listOperationsNext operation. */
-export type ListOperationsNextResponse = OperationListResult;
-
-/** Optional parameters. */
-export interface FeaturesListAllOptionalParams
-  extends coreClient.OperationOptions {}
-
-/** Contains response data for the listAll operation. */
-export type FeaturesListAllResponse = FeatureOperationsListResult;
-
-/** Optional parameters. */
-export interface FeaturesListOptionalParams
-  extends coreClient.OperationOptions {}
-
-/** Contains response data for the list operation. */
-export type FeaturesListResponse = FeatureOperationsListResult;
-
-/** Optional parameters. */
-export interface FeaturesGetOptionalParams
-  extends coreClient.OperationOptions {}
-
-/** Contains response data for the get operation. */
-export type FeaturesGetResponse = FeatureResult;
-
-/** Optional parameters. */
-export interface FeaturesRegisterOptionalParams
-  extends coreClient.OperationOptions {}
-
-/** Contains response data for the register operation. */
-export type FeaturesRegisterResponse = FeatureResult;
-
-/** Optional parameters. */
-export interface FeaturesUnregisterOptionalParams
-  extends coreClient.OperationOptions {}
-
-/** Contains response data for the unregister operation. */
-export type FeaturesUnregisterResponse = FeatureResult;
-
-/** Optional parameters. */
-export interface FeaturesListAllNextOptionalParams
-  extends coreClient.OperationOptions {}
-
-/** Contains response data for the listAllNext operation. */
-export type FeaturesListAllNextResponse = FeatureOperationsListResult;
-
-/** Optional parameters. */
-export interface FeaturesListNextOptionalParams
-  extends coreClient.OperationOptions {}
-
-/** Contains response data for the listNext operation. */
-export type FeaturesListNextResponse = FeatureOperationsListResult;
-
-/** Optional parameters. */
 export interface SubscriptionFeatureRegistrationsGetOptionalParams
   extends coreClient.OperationOptions {}
 
 /** Contains response data for the get operation. */
-export type SubscriptionFeatureRegistrationsGetResponse = SubscriptionFeatureRegistration;
+export type SubscriptionFeatureRegistrationsGetResponse =
+  SubscriptionFeatureRegistration;
 
 /** Optional parameters. */
 export interface SubscriptionFeatureRegistrationsCreateOrUpdateOptionalParams
@@ -333,7 +213,8 @@ export interface SubscriptionFeatureRegistrationsCreateOrUpdateOptionalParams
 }
 
 /** Contains response data for the createOrUpdate operation. */
-export type SubscriptionFeatureRegistrationsCreateOrUpdateResponse = SubscriptionFeatureRegistration;
+export type SubscriptionFeatureRegistrationsCreateOrUpdateResponse =
+  SubscriptionFeatureRegistration;
 
 /** Optional parameters. */
 export interface SubscriptionFeatureRegistrationsDeleteOptionalParams
@@ -344,28 +225,32 @@ export interface SubscriptionFeatureRegistrationsListBySubscriptionOptionalParam
   extends coreClient.OperationOptions {}
 
 /** Contains response data for the listBySubscription operation. */
-export type SubscriptionFeatureRegistrationsListBySubscriptionResponse = SubscriptionFeatureRegistrationList;
+export type SubscriptionFeatureRegistrationsListBySubscriptionResponse =
+  SubscriptionFeatureRegistrationList;
 
 /** Optional parameters. */
 export interface SubscriptionFeatureRegistrationsListAllBySubscriptionOptionalParams
   extends coreClient.OperationOptions {}
 
 /** Contains response data for the listAllBySubscription operation. */
-export type SubscriptionFeatureRegistrationsListAllBySubscriptionResponse = SubscriptionFeatureRegistrationList;
+export type SubscriptionFeatureRegistrationsListAllBySubscriptionResponse =
+  SubscriptionFeatureRegistrationList;
 
 /** Optional parameters. */
 export interface SubscriptionFeatureRegistrationsListBySubscriptionNextOptionalParams
   extends coreClient.OperationOptions {}
 
 /** Contains response data for the listBySubscriptionNext operation. */
-export type SubscriptionFeatureRegistrationsListBySubscriptionNextResponse = SubscriptionFeatureRegistrationList;
+export type SubscriptionFeatureRegistrationsListBySubscriptionNextResponse =
+  SubscriptionFeatureRegistrationList;
 
 /** Optional parameters. */
 export interface SubscriptionFeatureRegistrationsListAllBySubscriptionNextOptionalParams
   extends coreClient.OperationOptions {}
 
 /** Contains response data for the listAllBySubscriptionNext operation. */
-export type SubscriptionFeatureRegistrationsListAllBySubscriptionNextResponse = SubscriptionFeatureRegistrationList;
+export type SubscriptionFeatureRegistrationsListAllBySubscriptionNextResponse =
+  SubscriptionFeatureRegistrationList;
 
 /** Optional parameters. */
 export interface FeatureClientOptionalParams
