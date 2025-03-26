d3090eb6fbf85b47f5ebefc8b052efc72f18fbbe
SDKAuto <sdkautomation@microsoft.com>
2025-03-26 05:16:12 +0000

    CodeGen from PR 32282 in Azure/azure-rest-api-specs Merge 65f2e0b3ff80b07c858beaa13e834b54e2ecfd44 into b5838c4f5469766558f700d9a6d06ee04ac83750

---
+++ sdk/astro/arm-astro/src/api/organizations/index.ts
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  organizationsListBySubscription,
+  organizationsListByResourceGroup,
+  organizationsDelete,
+  organizationsUpdate,
+  organizationsCreateOrUpdate,
+  organizationsGet,
+} from "./operations.js";
+export {
+  OrganizationsListBySubscriptionOptionalParams,
+  OrganizationsListByResourceGroupOptionalParams,
+  OrganizationsDeleteOptionalParams,
+  OrganizationsUpdateOptionalParams,
+  OrganizationsCreateOrUpdateOptionalParams,
+  OrganizationsGetOptionalParams,
+} from "./options.js";
---
+++ sdk/astro/arm-astro/src/classic/organizations/index.ts
@@ -0,0 +1,102 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { AstroContext } from "../../api/astroContext.js";
+import { OrganizationResource, OrganizationResourceUpdate } from "../../models/models.js";
+import {
+  OrganizationsListBySubscriptionOptionalParams,
+  OrganizationsListByResourceGroupOptionalParams,
+  OrganizationsDeleteOptionalParams,
+  OrganizationsUpdateOptionalParams,
+  OrganizationsCreateOrUpdateOptionalParams,
+  OrganizationsGetOptionalParams,
+} from "../../api/organizations/options.js";
+import {
+  organizationsListBySubscription,
+  organizationsListByResourceGroup,
+  organizationsDelete,
+  organizationsUpdate,
+  organizationsCreateOrUpdate,
+  organizationsGet,
+} from "../../api/organizations/operations.js";
+import { PagedAsyncIterableIterator } from "../../static-helpers/pagingHelpers.js";
+import { PollerLike, OperationState } from "@azure/core-lro";
+
+/** Interface representing a Organizations operations. */
+export interface OrganizationsOperations {
+  /** List OrganizationResource resources by subscription ID */
+  listBySubscription: (
+    options?: OrganizationsListBySubscriptionOptionalParams,
+  ) => PagedAsyncIterableIterator<OrganizationResource>;
+  /** List OrganizationResource resources by resource group */
+  listByResourceGroup: (
+    resourceGroupName: string,
+    options?: OrganizationsListByResourceGroupOptionalParams,
+  ) => PagedAsyncIterableIterator<OrganizationResource>;
+  /** Delete a OrganizationResource */
+  delete: (
+    resourceGroupName: string,
+    organizationName: string,
+    options?: OrganizationsDeleteOptionalParams,
+  ) => PollerLike<OperationState<void>, void>;
+  /** Update a OrganizationResource */
+  update: (
+    resourceGroupName: string,
+    organizationName: string,
+    properties: OrganizationResourceUpdate,
+    options?: OrganizationsUpdateOptionalParams,
+  ) => PollerLike<OperationState<OrganizationResource>, OrganizationResource>;
+  /** Create a OrganizationResource */
+  createOrUpdate: (
+    resourceGroupName: string,
+    organizationName: string,
+    resource: OrganizationResource,
+    options?: OrganizationsCreateOrUpdateOptionalParams,
+  ) => PollerLike<OperationState<OrganizationResource>, OrganizationResource>;
+  /** Get a OrganizationResource */
+  get: (
+    resourceGroupName: string,
+    organizationName: string,
+    options?: OrganizationsGetOptionalParams,
+  ) => Promise<OrganizationResource>;
+}
+
+function _getOrganizations(context: AstroContext) {
+  return {
+    listBySubscription: (options?: OrganizationsListBySubscriptionOptionalParams) =>
+      organizationsListBySubscription(context, options),
+    listByResourceGroup: (
+      resourceGroupName: string,
+      options?: OrganizationsListByResourceGroupOptionalParams,
+    ) => organizationsListByResourceGroup(context, resourceGroupName, options),
+    delete: (
+      resourceGroupName: string,
+      organizationName: string,
+      options?: OrganizationsDeleteOptionalParams,
+    ) => organizationsDelete(context, resourceGroupName, organizationName, options),
+    update: (
+      resourceGroupName: string,
+      organizationName: string,
+      properties: OrganizationResourceUpdate,
+      options?: OrganizationsUpdateOptionalParams,
+    ) => organizationsUpdate(context, resourceGroupName, organizationName, properties, options),
+    createOrUpdate: (
+      resourceGroupName: string,
+      organizationName: string,
+      resource: OrganizationResource,
+      options?: OrganizationsCreateOrUpdateOptionalParams,
+    ) =>
+      organizationsCreateOrUpdate(context, resourceGroupName, organizationName, resource, options),
+    get: (
+      resourceGroupName: string,
+      organizationName: string,
+      options?: OrganizationsGetOptionalParams,
+    ) => organizationsGet(context, resourceGroupName, organizationName, options),
+  };
+}
+
+export function _getOrganizationsOperations(context: AstroContext): OrganizationsOperations {
+  return {
+    ..._getOrganizations(context),
+  };
+}
