5f1f3937fe656d1c813d4f081042691dcb453106
SDKAuto <sdkautomation@microsoft.com>
2025-03-28 11:12:01 +0000

    CodeGen from PR 33559 in Azure/azure-rest-api-specs Merge d10104af0f5db4e5f3c8483383840f9ef6f99490 into 7c1e8a2c1e18ac0dd65b88f00447992ed86fcc0f

--- sdk/resourcemover/arm-resourcemover/src/models/index.ts
+++ sdk/resourcemover/arm-resourcemover/src/models/index.ts
@@ -987,7 +987,7 @@ export enum KnownResourceIdentityType {
   /** SystemAssigned */
   SystemAssigned = "SystemAssigned",
   /** UserAssigned */
-  UserAssigned = "UserAssigned"
+  UserAssigned = "UserAssigned",
 }
 
 /**
@@ -1010,7 +1010,7 @@ export enum KnownProvisioningState {
   /** Creating */
   Creating = "Creating",
   /** Failed */
-  Failed = "Failed"
+  Failed = "Failed",
 }
 
 /**
@@ -1030,7 +1030,7 @@ export enum KnownMoveType {
   /** RegionToRegion */
   RegionToRegion = "RegionToRegion",
   /** RegionToZone */
-  RegionToZone = "RegionToZone"
+  RegionToZone = "RegionToZone",
 }
 
 /**
@@ -1052,7 +1052,7 @@ export enum KnownCreatedByType {
   /** ManagedIdentity */
   ManagedIdentity = "ManagedIdentity",
   /** Key */
-  Key = "Key"
+  Key = "Key",
 }
 
 /**
@@ -1072,7 +1072,7 @@ export enum KnownMoveResourceInputType {
   /** MoveResourceId */
   MoveResourceId = "MoveResourceId",
   /** MoveResourceSourceId */
-  MoveResourceSourceId = "MoveResourceSourceId"
+  MoveResourceSourceId = "MoveResourceSourceId",
 }
 
 /**
@@ -1116,7 +1116,7 @@ export enum KnownMoveState {
   /** DeleteSourcePending */
   DeleteSourcePending = "DeleteSourcePending",
   /** ResourceMoveCompleted */
-  ResourceMoveCompleted = "ResourceMoveCompleted"
+  ResourceMoveCompleted = "ResourceMoveCompleted",
 }
 
 /**
@@ -1145,7 +1145,7 @@ export type MoveState = string;
 /** Known values of {@link JobName} that the service accepts. */
 export enum KnownJobName {
   /** InitialSync */
-  InitialSync = "InitialSync"
+  InitialSync = "InitialSync",
 }
 
 /**
@@ -1162,7 +1162,7 @@ export enum KnownResolutionType {
   /** Manual */
   Manual = "Manual",
   /** Automatic */
-  Automatic = "Automatic"
+  Automatic = "Automatic",
 }
 
 /**
@@ -1180,7 +1180,7 @@ export enum KnownDependencyType {
   /** RequiredForPrepare */
   RequiredForPrepare = "RequiredForPrepare",
   /** RequiredForMove */
-  RequiredForMove = "RequiredForMove"
+  RequiredForMove = "RequiredForMove",
 }
 
 /**
@@ -1198,7 +1198,7 @@ export enum KnownDependencyLevel {
   /** Direct */
   Direct = "Direct",
   /** Descendant */
-  Descendant = "Descendant"
+  Descendant = "Descendant",
 }
 
 /**
@@ -1220,7 +1220,7 @@ export enum KnownTargetAvailabilityZone {
   /** Three */
   Three = "3",
   /** NA */
-  NA = "NA"
+  NA = "NA",
 }
 
 /**
@@ -1240,7 +1240,7 @@ export enum KnownZoneRedundant {
   /** Enable */
   Enable = "Enable",
   /** Disable */
-  Disable = "Disable"
+  Disable = "Disable",
 }
 
 /**
@@ -1379,35 +1379,40 @@ export interface MoveCollectionsListMoveCollectionsBySubscriptionOptionalParams
   extends coreClient.OperationOptions {}
 
 /** Contains response data for the listMoveCollectionsBySubscription operation. */
-export type MoveCollectionsListMoveCollectionsBySubscriptionResponse = MoveCollectionResultList;
+export type MoveCollectionsListMoveCollectionsBySubscriptionResponse =
+  MoveCollectionResultList;
 
 /** Optional parameters. */
 export interface MoveCollectionsListMoveCollectionsByResourceGroupOptionalParams
   extends coreClient.OperationOptions {}
 
 /** Contains response data for the listMoveCollectionsByResourceGroup operation. */
-export type MoveCollectionsListMoveCollectionsByResourceGroupResponse = MoveCollectionResultList;
+export type MoveCollectionsListMoveCollectionsByResourceGroupResponse =
+  MoveCollectionResultList;
 
 /** Optional parameters. */
 export interface MoveCollectionsListRequiredForOptionalParams
   extends coreClient.OperationOptions {}
 
 /** Contains response data for the listRequiredFor operation. */
-export type MoveCollectionsListRequiredForResponse = RequiredForResourcesCollection;
+export type MoveCollectionsListRequiredForResponse =
+  RequiredForResourcesCollection;
 
 /** Optional parameters. */
 export interface MoveCollectionsListMoveCollectionsBySubscriptionNextOptionalParams
   extends coreClient.OperationOptions {}
 
 /** Contains response data for the listMoveCollectionsBySubscriptionNext operation. */
-export type MoveCollectionsListMoveCollectionsBySubscriptionNextResponse = MoveCollectionResultList;
+export type MoveCollectionsListMoveCollectionsBySubscriptionNextResponse =
+  MoveCollectionResultList;
 
 /** Optional parameters. */
 export interface MoveCollectionsListMoveCollectionsByResourceGroupNextOptionalParams
   extends coreClient.OperationOptions {}
 
 /** Contains response data for the listMoveCollectionsByResourceGroupNext operation. */
-export type MoveCollectionsListMoveCollectionsByResourceGroupNextResponse = MoveCollectionResultList;
+export type MoveCollectionsListMoveCollectionsByResourceGroupNextResponse =
+  MoveCollectionResultList;
 
 /** Optional parameters. */
 export interface MoveResourcesListOptionalParams
@@ -1478,7 +1483,8 @@ export interface UnresolvedDependenciesGetNextOptionalParams
   extends coreClient.OperationOptions {}
 
 /** Contains response data for the getNext operation. */
-export type UnresolvedDependenciesGetNextResponse = UnresolvedDependencyCollection;
+export type UnresolvedDependenciesGetNextResponse =
+  UnresolvedDependencyCollection;
 
 /** Optional parameters. */
 export interface OperationsDiscoveryGetOptionalParams
