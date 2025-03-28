6a77761fcbcd4d709b0d1245065ae39553b0c8db
SDKAuto <sdkautomation@microsoft.com>
2025-03-28 05:53:40 +0000

    CodeGen from PR 33498 in Azure/azure-rest-api-specs Merge bcb8f3ff1191306833b68c931473314f720df85c into 763a27e7970d84740beef0bb6756375f77670c3a

---
+++ sdk/migrationdiscovery/arm-migrationdiscoverysap/src/api/serverInstances/index.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export { listBySapInstance, $delete, update, create, get } from "./operations.js";
+export {
+  ServerInstancesListBySapInstanceOptionalParams,
+  ServerInstancesDeleteOptionalParams,
+  ServerInstancesUpdateOptionalParams,
+  ServerInstancesCreateOptionalParams,
+  ServerInstancesGetOptionalParams,
+} from "./options.js";
