4d0e089229d3663d0bf6014fba3167c12f25646a
SDKAuto <sdkautomation@microsoft.com>
2025-03-31 10:53:26 +0000

    CodeGen from PR 33581 in Azure/azure-rest-api-specs Merge a125de51987277115941b197c6ff60b0055eafb6 into 1b47f62ee9da78db7e011c709c48ecd700095a5a

---
+++ sdk/oracledatabase/arm-oracledatabase/src/api/autonomousDatabaseBackups/index.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export { listByAutonomousDatabase, update, createOrUpdate, $delete, get } from "./operations.js";
+export {
+  AutonomousDatabaseBackupsListByAutonomousDatabaseOptionalParams,
+  AutonomousDatabaseBackupsUpdateOptionalParams,
+  AutonomousDatabaseBackupsCreateOrUpdateOptionalParams,
+  AutonomousDatabaseBackupsDeleteOptionalParams,
+  AutonomousDatabaseBackupsGetOptionalParams,
+} from "./options.js";
---
+++ sdk/oracledatabase/arm-oracledatabase/src/api/virtualNetworkAddresses/index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export { listByCloudVmCluster, createOrUpdate, $delete, get } from "./operations.js";
+export {
+  VirtualNetworkAddressesListByCloudVmClusterOptionalParams,
+  VirtualNetworkAddressesCreateOrUpdateOptionalParams,
+  VirtualNetworkAddressesDeleteOptionalParams,
+  VirtualNetworkAddressesGetOptionalParams,
+} from "./options.js";
