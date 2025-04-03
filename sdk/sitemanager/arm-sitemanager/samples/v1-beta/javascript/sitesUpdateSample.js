3baae28abf650dcb1607dadcac43ce13ab9787f2
SDKAuto <sdkautomation@microsoft.com>
2025-04-03 09:04:42 +0000

    CodeGen from PR 33698 in Azure/azure-rest-api-specs Merge c572a937b3c7818847d54d71c854e62969d75115 into 2ddcc49aa62928eff65f3864a88832fb4959166c

---
+++ sdk/sitemanager/arm-sitemanager/samples/v1-beta/javascript/sitesUpdateSample.js
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { EdgeClient } = require("@azure/arm-sitemanager");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to update a Site
+ *
+ * @summary update a Site
+ * x-ms-original-file: 2024-02-01-preview/Sites_Update.json
+ */
+async function updateSite() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "0154f7fe-df09-4981-bf82-7ad5c1f596eb";
+  const client = new EdgeClient(credential, subscriptionId);
+  const result = await client.sites.update("string", "string", {
+    properties: {
+      displayName: "string",
+      description: "string",
+      addressResourceId:
+        "/subscriptions/11111111-2222-3333-4444-55555555/resourceGroups/us-site-rg/providers/Microsoft.EdgeOrder/addresses/12343213",
+    },
+  });
+  console.log(result);
+}
+
+async function main() {
+  await updateSite();
+}
+
+main().catch(console.error);
