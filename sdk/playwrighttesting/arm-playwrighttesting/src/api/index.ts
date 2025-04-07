eaba1a2ebc2aefc9ecae88fc3e56e2a690200b78
SDKAuto <sdkautomation@microsoft.com>
2025-04-07 06:25:27 +0000

    CodeGen from PR 33754 in Azure/azure-rest-api-specs Merge cf3ec282f247a7a156a1b5c60197b211197772c2 into a67a8c166cdbcb357ac3e56cbc1c1e285b52706a

--- sdk/playwrighttesting/arm-playwrighttesting/src/api/index.ts
+++ sdk/playwrighttesting/arm-playwrighttesting/src/api/index.ts
@@ -6,29 +6,3 @@ export {
   AzurePlaywrightServiceContext,
   AzurePlaywrightServiceClientOptionalParams,
 } from "./azurePlaywrightServiceContext.js";
-export {
-  OperationsListOptionalParams,
-  AccountsGetOptionalParams,
-  AccountsCreateOrUpdateOptionalParams,
-  AccountsUpdateOptionalParams,
-  AccountsDeleteOptionalParams,
-  AccountsListByResourceGroupOptionalParams,
-  AccountsListBySubscriptionOptionalParams,
-  AccountsCheckNameAvailabilityOptionalParams,
-  QuotasGetOptionalParams,
-  QuotasListBySubscriptionOptionalParams,
-  AccountQuotasGetOptionalParams,
-  AccountQuotasListByAccountOptionalParams,
-} from "./options.js";
-export { accountQuotasGet, accountQuotasListByAccount } from "./accountQuotas/index.js";
-export {
-  accountsGet,
-  accountsCreateOrUpdate,
-  accountsUpdate,
-  accountsDelete,
-  accountsListByResourceGroup,
-  accountsListBySubscription,
-  accountsCheckNameAvailability,
-} from "./accounts/index.js";
-export { operationsList } from "./operations/index.js";
-export { quotasGet, quotasListBySubscription } from "./quotas/index.js";
