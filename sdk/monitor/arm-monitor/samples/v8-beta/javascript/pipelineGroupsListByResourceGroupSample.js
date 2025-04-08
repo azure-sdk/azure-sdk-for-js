42691a68a7f6132241f252a79aa827ab1ed7080a
SDKAuto <sdkautomation@microsoft.com>
2025-04-08 10:33:46 +0000

    CodeGen from PR 33698 in Azure/azure-rest-api-specs Merge 0426bf3308b9582c418b145b910798e0a53050cc into abb839b726184aa10f8d1eaac8e9fe590833619c

---
+++ sdk/monitor/arm-monitor/samples/v8-beta/javascript/pipelineGroupsListByResourceGroupSample.js
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { MonitorClient } = require("@azure/arm-monitor");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to lists all workspaces in the specified resource group
+ *
+ * @summary lists all workspaces in the specified resource group
+ * x-ms-original-file: 2023-10-01-preview/PipelineGroupListByResourceGroup.json
+ */
+async function listPipelineGroupsByResourceGroup() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new MonitorClient(credential, subscriptionId);
+  const resArray = new Array();
+  for await (const item of client.pipelineGroups.listByResourceGroup("myResourceGroup")) {
+    resArray.push(item);
+  }
+
+  console.log(resArray);
+}
+
+async function main() {
+  await listPipelineGroupsByResourceGroup();
+}
+
+main().catch(console.error);
