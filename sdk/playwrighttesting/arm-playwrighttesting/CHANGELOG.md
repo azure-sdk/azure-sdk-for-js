3a985522e2df915c8cbdfeffc00cbb051ae39eb5
SDKAuto <sdkautomation@microsoft.com>
2025-04-08 10:36:26 +0000

    CodeGen from PR 33698 in Azure/azure-rest-api-specs Merge 0426bf3308b9582c418b145b910798e0a53050cc into abb839b726184aa10f8d1eaac8e9fe590833619c

--- sdk/playwrighttesting/arm-playwrighttesting/CHANGELOG.md
+++ sdk/playwrighttesting/arm-playwrighttesting/CHANGELOG.md
@@ -1,5 +1,19 @@
 # Release History
     
+## 2.0.0 (2025-04-08)
+    
+### Features Added
+
+  - Added Interface ErrorAdditionalInfo
+  - Added Interface ErrorDetail
+  - Added Interface ErrorResponse
+  - Enum KnownVersions has a new value _20241201
+
+### Breaking Changes
+
+  - Enum KnownVersions no longer has value "V2024-12-01"
+    
+    
 ## 1.0.0 (2024-12-13)
 
 ### Features Added
--- sdk/playwrighttesting/arm-playwrighttesting/samples/v1/typescript/src/accountQuotasListByAccountSample.ts
+++ sdk/playwrighttesting/arm-playwrighttesting/samples/v1/typescript/src/accountQuotasListByAccountSample.ts
@@ -15,7 +15,7 @@ async function accountQuotasListByAccount(): Promise<void> {
   const subscriptionId = "00000000-0000-0000-0000-000000000000";
   const client = new AzurePlaywrightServiceClient(credential, subscriptionId);
   const resArray = new Array();
-  for await (let item of client.accountQuotas.listByAccount("dummyrg", "myPlaywrightAccount")) {
+  for await (const item of client.accountQuotas.listByAccount("dummyrg", "myPlaywrightAccount")) {
     resArray.push(item);
   }
 
@@ -23,7 +23,7 @@ async function accountQuotasListByAccount(): Promise<void> {
 }
 
 async function main(): Promise<void> {
-  accountQuotasListByAccount();
+  await accountQuotasListByAccount();
 }
 
 main().catch(console.error);
