cbbc62cf15e59bb8571eeaa9c00cf06d30ce29fc
SDKAuto <sdkautomation@microsoft.com>
2025-04-09 02:04:04 +0000

    CodeGen from PR 33784 in Azure/azure-rest-api-specs Merge 713d711228e1c5fb59251dd324be148a5c75dee8 into 7d694e709dcf22ad06006f3b6c664781f0f591d2

---
+++ sdk/dependencymap/arm-dependencymap/src/api/discoverySources/index.ts
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  discoverySourcesListByMapsResource,
+  discoverySourcesDelete,
+  discoverySourcesUpdate,
+  discoverySourcesCreateOrUpdate,
+  discoverySourcesGet,
+} from "./operations.js";
+export {
+  DiscoverySourcesListByMapsResourceOptionalParams,
+  DiscoverySourcesDeleteOptionalParams,
+  DiscoverySourcesUpdateOptionalParams,
+  DiscoverySourcesCreateOrUpdateOptionalParams,
+  DiscoverySourcesGetOptionalParams,
+} from "./options.js";
