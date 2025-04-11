0cd09d992bd8b92623ee8603abc637c1cbb3a6c4
SDKAuto <sdkautomation@microsoft.com>
2025-04-11 05:52:15 +0000

    CodeGen from PR 33881 in Azure/azure-rest-api-specs Merge 75eee67128e57fc00f25eca195679e8db7cf2e96 into 69ddfb5117d394bc756fa63a0bf7b872fcb6bae0

--- sdk/deviceregistry/arm-deviceregistry/src/classic/operations/index.ts
+++ sdk/deviceregistry/arm-deviceregistry/src/classic/operations/index.ts
@@ -2,9 +2,9 @@
 // Licensed under the MIT License.
 
 import { DeviceRegistryManagementContext } from "../../api/deviceRegistryManagementContext.js";
-import { operationsList } from "../../api/operations/index.js";
-import { OperationsListOptionalParams } from "../../api/options.js";
 import { Operation } from "../../models/models.js";
+import { OperationsListOptionalParams } from "../../api/operations/options.js";
+import { list } from "../../api/operations/operations.js";
 import { PagedAsyncIterableIterator } from "../../static-helpers/pagingHelpers.js";
 
 /** Interface representing a Operations operations. */
@@ -15,7 +15,7 @@ export interface OperationsOperations {
 
 function _getOperations(context: DeviceRegistryManagementContext) {
   return {
-    list: (options?: OperationsListOptionalParams) => operationsList(context, options),
+    list: (options?: OperationsListOptionalParams) => list(context, options),
   };
 }
 
