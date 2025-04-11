fbffbef85f658d51e2948d72b11ff6b741328628
SDKAuto <sdkautomation@microsoft.com>
2025-04-11 05:55:04 +0000

    CodeGen from PR 33881 in Azure/azure-rest-api-specs Merge 75eee67128e57fc00f25eca195679e8db7cf2e96 into 69ddfb5117d394bc756fa63a0bf7b872fcb6bae0

---
+++ kubernetesruntime/arm-containerorchestratorruntime/src/api/bgpPeers/index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export { list, $delete, createOrUpdate, get } from "./operations.js";
+export {
+  BgpPeersListOptionalParams,
+  BgpPeersDeleteOptionalParams,
+  BgpPeersCreateOrUpdateOptionalParams,
+  BgpPeersGetOptionalParams,
+} from "./options.js";
