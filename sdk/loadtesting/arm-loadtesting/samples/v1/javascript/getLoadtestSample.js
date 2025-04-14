b47a940d61d9a4e55076b8834576cd978cf2bc3d
SDKAuto <sdkautomation@microsoft.com>
2025-04-14 12:06:01 +0000

    CodeGen from PR 33925 in Azure/azure-rest-api-specs Merge 057778438fcc6462c8c026b068331c5c4b47780c into 640089eb48c9c32ee2627e910dfcbabbcbc3640b

---
+++ sdk/loadtesting/arm-loadtesting/samples/v1/javascript/getLoadtestSample.js
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { LoadTestMgmtClient } = require("@azure/arm-loadtesting");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to get a LoadTestResource
+ *
+ * @summary get a LoadTestResource
+ * x-ms-original-file: 2024-12-01-preview/LoadTests_Get.json
+ */
+async function getALoadTestResource() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new LoadTestMgmtClient(credential, subscriptionId);
+  const result = await client.getLoadtest("dummyrg", "myLoadTest");
+  console.log(result);
+}
+
+async function main() {
+  await getALoadTestResource();
+}
+
+main().catch(console.error);
