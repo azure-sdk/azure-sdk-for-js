730e3fc1ca3ef4017cc3a151b3b0a6626fef054a
SDKAuto <sdkautomation@microsoft.com>
2025-04-14 16:54:15 +0000

    CodeGen from PR 33833 in Azure/azure-rest-api-specs Merge f5353798e213b408cba770268c6f1ba3b6562c53 into 640089eb48c9c32ee2627e910dfcbabbcbc3640b

---
+++ sdk/servicefabricmanagedclusters/arm-servicefabricmanagedclusters/src/api/managedApplyMaintenanceWindow/index.ts
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export { managedApplyMaintenanceWindowPost } from "./operations.js";
+export { ManagedApplyMaintenanceWindowPostOptionalParams } from "./options.js";
