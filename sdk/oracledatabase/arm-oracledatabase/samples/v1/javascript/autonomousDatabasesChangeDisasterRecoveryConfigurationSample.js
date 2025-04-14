4cf276a60e52cc15030b2b4531555bbd3320c0e1
SDKAuto <sdkautomation@microsoft.com>
2025-04-14 21:23:41 +0000

    CodeGen from PR 33466 in Azure/azure-rest-api-specs Merge 62fc5e0e0a054e0830794e05f7938d1e61dad88a into ee2c80925c383e8efc4853837ef3adb56dcd6fea

---
+++ sdk/oracledatabase/arm-oracledatabase/samples/v1/javascript/autonomousDatabasesChangeDisasterRecoveryConfigurationSample.js
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { DatabaseClient } = require("@azure/arm-oracledatabase");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to perform ChangeDisasterRecoveryConfiguration action on Autonomous Database
+ *
+ * @summary perform ChangeDisasterRecoveryConfiguration action on Autonomous Database
+ * x-ms-original-file: 2025-03-01/autonomousDatabase_changeDisasterRecoveryConfiguration.json
+ */
+async function autonomousDatabasesChangeDisasterRecoveryConfiguration() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new DatabaseClient(credential, subscriptionId);
+  const result = await client.autonomousDatabases.changeDisasterRecoveryConfiguration(
+    "rg000",
+    "databasedb1",
+    { disasterRecoveryType: "Adg", isReplicateAutomaticBackups: false },
+  );
+  console.log(result);
+}
+
+async function main() {
+  await autonomousDatabasesChangeDisasterRecoveryConfiguration();
+}
+
+main().catch(console.error);
