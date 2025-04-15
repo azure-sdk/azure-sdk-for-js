fc2d3a893e7f85dc7c99d8a0611fb863956b7fbb
SDKAuto <sdkautomation@microsoft.com>
2025-04-15 06:58:09 +0000

    CodeGen from PR 33496 in Azure/azure-rest-api-specs Merge 80a27661250a05a8cec420ddd35120c4be60b1c1 into a730aea13a0bb551748ab41f5d36f00956f0a4f6

---
+++ kubernetesruntime/arm-containerorchestratorruntime/src/classic/bgpPeers/index.ts
@@ -0,0 +1,83 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { KubernetesRuntimeContext } from "../../api/kubernetesRuntimeContext.js";
+import { BgpPeer } from "../../models/models.js";
+import {
+  BgpPeersListOptionalParams,
+  BgpPeersDeleteOptionalParams,
+  BgpPeersCreateOrUpdateOptionalParams,
+  BgpPeersGetOptionalParams,
+} from "../../api/bgpPeers/options.js";
+import {
+  list,
+  $delete,
+  createOrUpdate,
+  get,
+} from "../../api/bgpPeers/operations.js";
+import { PagedAsyncIterableIterator } from "../../static-helpers/pagingHelpers.js";
+import { PollerLike, OperationState } from "@azure/core-lro";
+
+/** Interface representing a BgpPeers operations. */
+export interface BgpPeersOperations {
+  /** List BgpPeer resources by parent */
+  list: (
+    resourceUri: string,
+    options?: BgpPeersListOptionalParams,
+  ) => PagedAsyncIterableIterator<BgpPeer>;
+  /** Delete a BgpPeer */
+  /**
+   *  @fixme delete is a reserved word that cannot be used as an operation name.
+   *         Please add @clientName("clientName") or @clientName("<JS-Specific-Name>", "javascript")
+   *         to the operation to override the generated name.
+   */
+  delete: (
+    resourceUri: string,
+    bgpPeerName: string,
+    options?: BgpPeersDeleteOptionalParams,
+  ) => Promise<void>;
+  /** Create a BgpPeer */
+  createOrUpdate: (
+    resourceUri: string,
+    bgpPeerName: string,
+    resource: BgpPeer,
+    options?: BgpPeersCreateOrUpdateOptionalParams,
+  ) => PollerLike<OperationState<BgpPeer>, BgpPeer>;
+  /** Get a BgpPeer */
+  get: (
+    resourceUri: string,
+    bgpPeerName: string,
+    options?: BgpPeersGetOptionalParams,
+  ) => Promise<BgpPeer>;
+}
+
+function _getBgpPeers(context: KubernetesRuntimeContext) {
+  return {
+    list: (resourceUri: string, options?: BgpPeersListOptionalParams) =>
+      list(context, resourceUri, options),
+    delete: (
+      resourceUri: string,
+      bgpPeerName: string,
+      options?: BgpPeersDeleteOptionalParams,
+    ) => $delete(context, resourceUri, bgpPeerName, options),
+    createOrUpdate: (
+      resourceUri: string,
+      bgpPeerName: string,
+      resource: BgpPeer,
+      options?: BgpPeersCreateOrUpdateOptionalParams,
+    ) => createOrUpdate(context, resourceUri, bgpPeerName, resource, options),
+    get: (
+      resourceUri: string,
+      bgpPeerName: string,
+      options?: BgpPeersGetOptionalParams,
+    ) => get(context, resourceUri, bgpPeerName, options),
+  };
+}
+
+export function _getBgpPeersOperations(
+  context: KubernetesRuntimeContext,
+): BgpPeersOperations {
+  return {
+    ..._getBgpPeers(context),
+  };
+}
