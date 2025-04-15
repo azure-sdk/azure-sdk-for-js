b563bbcd00769b833220f3ac3e9a3eb795325e5f
SDKAuto <sdkautomation@microsoft.com>
2025-04-15 10:46:55 +0000

    CodeGen from PR 33581 in Azure/azure-rest-api-specs Merge efc5c09ac76420e669ba217b83c81ce263ad5453 into cb5ef7fc4cb443bd5f6b21d02cbce41051beb6ae

---
+++ sdk/scvmm/arm-scvmm/src/api/index.ts
@@ -0,0 +1,4 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export { createScVmm, ScVmmContext, ScVmmClientOptionalParams } from "./scVmmContext.js";
---
+++ sdk/scvmm/arm-scvmm/src/api/inventoryItems/operations.ts
@@ -0,0 +1,269 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { ScVmmContext as Client } from "../index.js";
+import {
+  errorResponseDeserializer,
+  InventoryItem,
+  inventoryItemSerializer,
+  inventoryItemDeserializer,
+  _InventoryItemListResult,
+  _inventoryItemListResultDeserializer,
+} from "../../models/models.js";
+import {
+  InventoryItemsListByVmmServerOptionalParams,
+  InventoryItemsDeleteOptionalParams,
+  InventoryItemsCreateOptionalParams,
+  InventoryItemsGetOptionalParams,
+} from "./options.js";
+import {
+  PagedAsyncIterableIterator,
+  buildPagedAsyncIterator,
+} from "../../static-helpers/pagingHelpers.js";
+import { expandUrlTemplate } from "../../static-helpers/urlTemplate.js";
+import {
+  StreamableMethod,
+  PathUncheckedResponse,
+  createRestError,
+  operationOptionsToRequestParameters,
+} from "@azure-rest/core-client";
+
+export function _listByVmmServerSend(
+  context: Client,
+  resourceGroupName: string,
+  vmmServerName: string,
+  options: InventoryItemsListByVmmServerOptionalParams = { requestOptions: {} },
+): StreamableMethod {
+  const path = expandUrlTemplate(
+    "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ScVmm/vmmServers/{vmmServerName}/inventoryItems{?api%2Dversion}",
+    {
+      subscriptionId: context.subscriptionId,
+      resourceGroupName: resourceGroupName,
+      vmmServerName: vmmServerName,
+      "api%2Dversion": context.apiVersion,
+    },
+    {
+      allowReserved: options?.requestOptions?.skipUrlEncoding,
+    },
+  );
+  return context.path(path).get({
+    ...operationOptionsToRequestParameters(options),
+    headers: {
+      accept: "application/json",
+      ...options.requestOptions?.headers,
+    },
+  });
+}
+
+export async function _listByVmmServerDeserialize(
+  result: PathUncheckedResponse,
+): Promise<_InventoryItemListResult> {
+  const expectedStatuses = ["200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return _inventoryItemListResultDeserializer(result.body);
+}
+
+/** Returns the list of inventoryItems in the given VmmServer. */
+export function listByVmmServer(
+  context: Client,
+  resourceGroupName: string,
+  vmmServerName: string,
+  options: InventoryItemsListByVmmServerOptionalParams = { requestOptions: {} },
+): PagedAsyncIterableIterator<InventoryItem> {
+  return buildPagedAsyncIterator(
+    context,
+    () => _listByVmmServerSend(context, resourceGroupName, vmmServerName, options),
+    _listByVmmServerDeserialize,
+    ["200"],
+    { itemName: "value", nextLinkName: "nextLink" },
+  );
+}
+
+export function _$deleteSend(
+  context: Client,
+  resourceGroupName: string,
+  vmmServerName: string,
+  inventoryItemResourceName: string,
+  options: InventoryItemsDeleteOptionalParams = { requestOptions: {} },
+): StreamableMethod {
+  const path = expandUrlTemplate(
+    "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ScVmm/vmmServers/{vmmServerName}/inventoryItems/{inventoryItemResourceName}{?api%2Dversion}",
+    {
+      subscriptionId: context.subscriptionId,
+      resourceGroupName: resourceGroupName,
+      vmmServerName: vmmServerName,
+      inventoryItemResourceName: inventoryItemResourceName,
+      "api%2Dversion": context.apiVersion,
+    },
+    {
+      allowReserved: options?.requestOptions?.skipUrlEncoding,
+    },
+  );
+  return context.path(path).delete({
+    ...operationOptionsToRequestParameters(options),
+    headers: {
+      accept: "application/json",
+      ...options.requestOptions?.headers,
+    },
+  });
+}
+
+export async function _$deleteDeserialize(result: PathUncheckedResponse): Promise<void> {
+  const expectedStatuses = ["200", "204"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return;
+}
+
+/** Deletes an inventoryItem. */
+/**
+ *  @fixme delete is a reserved word that cannot be used as an operation name.
+ *         Please add @clientName("clientName") or @clientName("<JS-Specific-Name>", "javascript")
+ *         to the operation to override the generated name.
+ */
+export async function $delete(
+  context: Client,
+  resourceGroupName: string,
+  vmmServerName: string,
+  inventoryItemResourceName: string,
+  options: InventoryItemsDeleteOptionalParams = { requestOptions: {} },
+): Promise<void> {
+  const result = await _$deleteSend(
+    context,
+    resourceGroupName,
+    vmmServerName,
+    inventoryItemResourceName,
+    options,
+  );
+  return _$deleteDeserialize(result);
+}
+
+export function _createSend(
+  context: Client,
+  resourceGroupName: string,
+  vmmServerName: string,
+  inventoryItemResourceName: string,
+  resource: InventoryItem,
+  options: InventoryItemsCreateOptionalParams = { requestOptions: {} },
+): StreamableMethod {
+  const path = expandUrlTemplate(
+    "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ScVmm/vmmServers/{vmmServerName}/inventoryItems/{inventoryItemResourceName}{?api%2Dversion}",
+    {
+      subscriptionId: context.subscriptionId,
+      resourceGroupName: resourceGroupName,
+      vmmServerName: vmmServerName,
+      inventoryItemResourceName: inventoryItemResourceName,
+      "api%2Dversion": context.apiVersion,
+    },
+    {
+      allowReserved: options?.requestOptions?.skipUrlEncoding,
+    },
+  );
+  return context.path(path).put({
+    ...operationOptionsToRequestParameters(options),
+    contentType: "application/json",
+    headers: {
+      accept: "application/json",
+      ...options.requestOptions?.headers,
+    },
+    body: inventoryItemSerializer(resource),
+  });
+}
+
+export async function _createDeserialize(result: PathUncheckedResponse): Promise<InventoryItem> {
+  const expectedStatuses = ["200", "201"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return inventoryItemDeserializer(result.body);
+}
+
+/** Create Or Update InventoryItem. */
+export async function create(
+  context: Client,
+  resourceGroupName: string,
+  vmmServerName: string,
+  inventoryItemResourceName: string,
+  resource: InventoryItem,
+  options: InventoryItemsCreateOptionalParams = { requestOptions: {} },
+): Promise<InventoryItem> {
+  const result = await _createSend(
+    context,
+    resourceGroupName,
+    vmmServerName,
+    inventoryItemResourceName,
+    resource,
+    options,
+  );
+  return _createDeserialize(result);
+}
+
+export function _getSend(
+  context: Client,
+  resourceGroupName: string,
+  vmmServerName: string,
+  inventoryItemResourceName: string,
+  options: InventoryItemsGetOptionalParams = { requestOptions: {} },
+): StreamableMethod {
+  const path = expandUrlTemplate(
+    "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ScVmm/vmmServers/{vmmServerName}/inventoryItems/{inventoryItemResourceName}{?api%2Dversion}",
+    {
+      subscriptionId: context.subscriptionId,
+      resourceGroupName: resourceGroupName,
+      vmmServerName: vmmServerName,
+      inventoryItemResourceName: inventoryItemResourceName,
+      "api%2Dversion": context.apiVersion,
+    },
+    {
+      allowReserved: options?.requestOptions?.skipUrlEncoding,
+    },
+  );
+  return context.path(path).get({
+    ...operationOptionsToRequestParameters(options),
+    headers: {
+      accept: "application/json",
+      ...options.requestOptions?.headers,
+    },
+  });
+}
+
+export async function _getDeserialize(result: PathUncheckedResponse): Promise<InventoryItem> {
+  const expectedStatuses = ["200"];
+  if (!expectedStatuses.includes(result.status)) {
+    const error = createRestError(result);
+    error.details = errorResponseDeserializer(result.body);
+    throw error;
+  }
+
+  return inventoryItemDeserializer(result.body);
+}
+
+/** Shows an inventory item. */
+export async function get(
+  context: Client,
+  resourceGroupName: string,
+  vmmServerName: string,
+  inventoryItemResourceName: string,
+  options: InventoryItemsGetOptionalParams = { requestOptions: {} },
+): Promise<InventoryItem> {
+  const result = await _getSend(
+    context,
+    resourceGroupName,
+    vmmServerName,
+    inventoryItemResourceName,
+    options,
+  );
+  return _getDeserialize(result);
+}
