e4facadda0e6e17824fa0afddc09333e8e62a312
SDKAuto <sdkautomation@microsoft.com>
2025-04-15 10:00:07 +0000

    CodeGen from PR 33581 in Azure/azure-rest-api-specs Merge efc5c09ac76420e669ba217b83c81ce263ad5453 into cb5ef7fc4cb443bd5f6b21d02cbce41051beb6ae

---
+++ sdk/databasefleetmanager/arm-databasefleetmanager/samples/v1-beta/javascript/firewallRulesCreateOrUpdateSample.js
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { DatabaseFleetManagerClient } = require("@azure/arm-databasefleetmanager");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to creates or updates a firewall rule.
+ *
+ * @summary creates or updates a firewall rule.
+ * x-ms-original-file: 2025-02-01-preview/FirewallRules_CreateOrUpdate_MaximumSet_Gen.json
+ */
+async function createOrUpdateFirewallRuleGeneratedByPolicy() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "a2b3c4d5-6789-0123-4567-89abcdef1234";
+  const client = new DatabaseFleetManagerClient(credential, subscriptionId);
+  const result = await client.firewallRules.createOrUpdate(
+    "rg-networking-operations",
+    "data-fleet-01",
+    "prod-environment",
+    "allow-10-0-0-0-24-to-10-1-0-0-24",
+    { properties: { startIpAddress: "10.0.0.0", endIpAddress: "10.0.0.255" } },
+  );
+  console.log(result);
+}
+
+async function main() {
+  await createOrUpdateFirewallRuleGeneratedByPolicy();
+}
+
+main().catch(console.error);
