54666eb504cca9da38ee99dd4b6653f499ed7c9e
SDKAuto <sdkautomation@microsoft.com>
2025-04-16 08:26:51 +0000

    CodeGen from PR 33986 in Azure/azure-rest-api-specs Merge c0d9c680f240997b7a7d0de1eaebd9986988bc1a into 4f6ba43a83ed4f80863e1fe698ac10b89e8c48a0

--- sdk/cognitiveservices/arm-cognitiveservices/src/operations/accounts.ts
+++ sdk/cognitiveservices/arm-cognitiveservices/src/operations/accounts.ts
@@ -7,18 +7,18 @@
  */
 
 import { PagedAsyncIterableIterator, PageSettings } from "@azure/core-paging";
-import { setContinuationToken } from "../pagingHelper.js";
-import { Accounts } from "../operationsInterfaces/index.js";
+import { setContinuationToken } from "../pagingHelper";
+import { Accounts } from "../operationsInterfaces";
 import * as coreClient from "@azure/core-client";
-import * as Mappers from "../models/mappers.js";
-import * as Parameters from "../models/parameters.js";
-import { CognitiveServicesManagementClient } from "../cognitiveServicesManagementClient.js";
+import * as Mappers from "../models/mappers";
+import * as Parameters from "../models/parameters";
+import { CognitiveServicesManagementClient } from "../cognitiveServicesManagementClient";
 import {
   SimplePollerLike,
   OperationState,
   createHttpPoller,
 } from "@azure/core-lro";
-import { createLroSpec } from "../lroImpl.js";
+import { createLroSpec } from "../lroImpl";
 import {
   Account,
   AccountsListByResourceGroupNextOptionalParams,
@@ -50,7 +50,7 @@ import {
   AccountsListByResourceGroupNextResponse,
   AccountsListNextResponse,
   AccountsListModelsNextResponse,
-} from "../models/index.js";
+} from "../models";
 
 /// <reference lib="esnext.asynciterable" />
 /** Class containing Accounts operations. */
