b792a7e82b159ce90c1f4a0b544db9656dfe44b0
SDKAuto <sdkautomation@microsoft.com>
2025-04-17 20:18:22 +0000

    CodeGen from PR 33742 in Azure/azure-rest-api-specs Merge c1c9c1acce03e996f0e09ccd936a90fb35e9c676 into ee1e1c4a8150828b854859ab164f069fab01aa18

---
+++ sdk/computeschedule/arm-computeschedule/samples/v1/javascript/scheduledActionsVirtualMachinesExecuteCreateSample.js
@@ -0,0 +1,63 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { ComputeScheduleClient } = require("@azure/arm-computeschedule");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+/**
+ * This sample demonstrates how to virtualMachinesExecuteCreate: Execute create operation for a batch of virtual machines, this operation is triggered as soon as Computeschedule receives it.
+ *
+ * @summary virtualMachinesExecuteCreate: Execute create operation for a batch of virtual machines, this operation is triggered as soon as Computeschedule receives it.
+ * x-ms-original-file: 2025-05-01/ScheduledActions_VirtualMachinesExecuteCreate_MaximumSet_Gen.json
+ */
+async function scheduledActionsVirtualMachinesExecuteCreateMaximumSetGenGeneratedByMaximumSetRuleGeneratedByMaximumSetRule() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "0505D8E4-D41A-48FB-9CA5-4AF8D93BE75F";
+  const client = new ComputeScheduleClient(credential, subscriptionId);
+  const result = await client.scheduledActions.virtualMachinesExecuteCreate("egiw", {
+    resourceConfigParameters: {
+      baseProfile: "tfgrulcuneopmbdjydzofmhpa",
+      resourceOverrides: ["hsqtjgsobjaffl"],
+      vmExtensions: [
+        {
+          name: "xnymtvrxgddnppywidgkxcsrcthx",
+          location: "daubzmbqetankmfvcc",
+          properties: {},
+          tags: {},
+        },
+      ],
+      resourceCount: 28,
+      resourcePrefix: "rqlxavviucrxfjj",
+    },
+    executionParameters: {
+      optimizationPreference: "Cost",
+      retryPolicy: { retryCount: 25, retryWindowInMinutes: 4 },
+    },
+    correlationid: "erpswvxajdpqgxfpgmzy",
+  });
+  console.log(result);
+}
+
+/**
+ * This sample demonstrates how to virtualMachinesExecuteCreate: Execute create operation for a batch of virtual machines, this operation is triggered as soon as Computeschedule receives it.
+ *
+ * @summary virtualMachinesExecuteCreate: Execute create operation for a batch of virtual machines, this operation is triggered as soon as Computeschedule receives it.
+ * x-ms-original-file: 2025-05-01/ScheduledActions_VirtualMachinesExecuteCreate_MinimumSet_Gen.json
+ */
+async function scheduledActionsVirtualMachinesExecuteCreateMaximumSetGenGeneratedByMaximumSetRuleGeneratedByMinimumSetRule() {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "0505D8E4-D41A-48FB-9CA5-4AF8D93BE75F";
+  const client = new ComputeScheduleClient(credential, subscriptionId);
+  const result = await client.scheduledActions.virtualMachinesExecuteCreate("nrpuchctpj", {
+    resourceConfigParameters: { resourceCount: 28 },
+    executionParameters: {},
+  });
+  console.log(result);
+}
+
+async function main() {
+  await scheduledActionsVirtualMachinesExecuteCreateMaximumSetGenGeneratedByMaximumSetRuleGeneratedByMaximumSetRule();
+  await scheduledActionsVirtualMachinesExecuteCreateMaximumSetGenGeneratedByMaximumSetRuleGeneratedByMinimumSetRule();
+}
+
+main().catch(console.error);
