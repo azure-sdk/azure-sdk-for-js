83df1fa522007829431ce0ef74a71246ce9cb497
SDKAuto <sdkautomation@microsoft.com>
2025-04-18 02:30:22 +0000

    CodeGen from PR 34006 in Azure/azure-rest-api-specs Merge 6cc659de67576bdedf53855ec4dcad2ed42a03ed into 877f166d9819e61625338a9ebb9922e1fc646636

--- sdk/appplatform/arm-appplatform/samples/v3/typescript/src/servicesEnableApmGloballySample.ts
+++ sdk/appplatform/arm-appplatform/samples/v3/typescript/src/servicesEnableApmGloballySample.ts
@@ -18,7 +18,7 @@ import "dotenv/config";
  * @summary Enable an APM globally.
  * x-ms-original-file: specification/appplatform/resource-manager/Microsoft.AppPlatform/stable/2023-12-01/examples/Services_EnableApmGlobally.json
  */
-async function servicesEnableApmGlobally() {
+async function servicesEnableApmGlobally(): Promise<void> {
   const subscriptionId =
     process.env["APPPLATFORM_SUBSCRIPTION_ID"] ||
     "00000000-0000-0000-0000-000000000000";
@@ -39,7 +39,7 @@ async function servicesEnableApmGlobally() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   servicesEnableApmGlobally();
 }
 
--- sdk/appplatform/arm-appplatform/samples/v3/typescript/src/storagesGetSample.ts
+++ sdk/appplatform/arm-appplatform/samples/v3/typescript/src/storagesGetSample.ts
@@ -15,7 +15,7 @@ import "dotenv/config";
  * @summary Get the storage resource.
  * x-ms-original-file: specification/appplatform/resource-manager/Microsoft.AppPlatform/stable/2023-12-01/examples/Storages_Get.json
  */
-async function storagesGet() {
+async function storagesGet(): Promise<void> {
   const subscriptionId =
     process.env["APPPLATFORM_SUBSCRIPTION_ID"] ||
     "00000000-0000-0000-0000-000000000000";
@@ -33,7 +33,7 @@ async function storagesGet() {
   console.log(result);
 }
 
-async function main() {
+async function main(): Promise<void> {
   storagesGet();
 }
 
