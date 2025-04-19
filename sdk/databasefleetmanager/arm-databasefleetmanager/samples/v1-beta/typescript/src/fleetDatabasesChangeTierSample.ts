2791afe14c6a43258a76525e5cf65c475707dfb1
SDKAuto <sdkautomation@microsoft.com>
2025-04-19 01:23:34 +0000

    CodeGen from PR 33973 in Azure/azure-rest-api-specs Merge c5980e617228448f4c3624b42ce6dc93388995ce into abe6b63e998a3995d1c28d2a1f8c0a6950a79b72

---
+++ sdk/databasefleetmanager/arm-databasefleetmanager/samples/v1-beta/typescript/src/fleetDatabasesChangeTierSample.ts
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { DatabaseFleetManagerClient } from "@azure/arm-databasefleetmanager";
+import { DefaultAzureCredential } from "@azure/identity";
+
+/**
+ * This sample demonstrates how to moves database to a different tier.
+ *
+ * @summary moves database to a different tier.
+ * x-ms-original-file: 2025-02-01-preview/FleetDatabases_ChangeTier_MaximumSet_Gen.json
+ */
+async function changeDatabaseTierGeneratedByPolicy(): Promise<void> {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "C3897315-3847-4D8A-B2FC-7307B066AD63";
+  const client = new DatabaseFleetManagerClient(credential, subscriptionId);
+  await client.fleetDatabases.changeTier(
+    "rg-database-operations",
+    "data-fleet-01",
+    "prod-environment",
+    "customer-db-prod",
+    { targetTierName: "Standard" },
+  );
+}
+
+async function main(): Promise<void> {
+  await changeDatabaseTierGeneratedByPolicy();
+}
+
+main().catch(console.error);
