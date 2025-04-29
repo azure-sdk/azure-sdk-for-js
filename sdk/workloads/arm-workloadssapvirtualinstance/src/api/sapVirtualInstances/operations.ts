a9423a08daa89883be4092ea23a2ecbb58fba6fb
SDKAuto <sdkautomation@microsoft.com>
2025-04-29 05:27:20 +0000

    CodeGen from PR 34285 in Azure/azure-rest-api-specs Merge 7727cd634fd8544c300b04ae03881e7a920d51a7 into c5c1929b24a2d40130a10058757763ab3568c40f

--- sdk/workloads/arm-workloadssapvirtualinstance/src/api/sapVirtualInstances/operations.ts
+++ sdk/workloads/arm-workloadssapvirtualinstance/src/api/sapVirtualInstances/operations.ts
@@ -46,11 +46,11 @@ import {
   SAPVirtualInstancesCreateOptionalParams,
   SAPVirtualInstancesGetOptionalParams,
 } from "./options.js";
-import { getLongRunningPoller } from "../../static-helpers/pollingHelpers.js";
 import {
   PagedAsyncIterableIterator,
   buildPagedAsyncIterator,
 } from "../../static-helpers/pagingHelpers.js";
+import { getLongRunningPoller } from "../../static-helpers/pollingHelpers.js";
 import { expandUrlTemplate } from "../../static-helpers/urlTemplate.js";
 import {
   StreamableMethod,
