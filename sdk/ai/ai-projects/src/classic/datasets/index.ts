21b9904a75d751c21da43b27aa7b6e5b7fef9f2c
SDKAuto <sdkautomation@microsoft.com>
2025-05-05 18:56:06 +0000

    CodeGen from PR 34444 in Azure/azure-rest-api-specs Merge d2b8d735f113e008454c702a7176dcb6de90576d into 57e9831f27b627823c986784b43640c30c4f9a7d

---
+++ sdk/ai/ai-projects/src/classic/datasets/index.ts
@@ -0,0 +1,123 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { AIProjectContext } from "../../api/aiProjectContext.js";
+import {
+  DatasetVersionUnion,
+  PendingUploadRequest,
+  PendingUploadResponse,
+  AssetCredentialResponse,
+} from "../../models/models.js";
+import {
+  DatasetsGetCredentialsOptionalParams,
+  DatasetsPendingUploadOptionalParams,
+  DatasetsCreateOrUpdateOptionalParams,
+  DatasetsDeleteOptionalParams,
+  DatasetsGetOptionalParams,
+  DatasetsListOptionalParams,
+  DatasetsListVersionsOptionalParams,
+} from "../../api/datasets/options.js";
+import {
+  getCredentials,
+  pendingUpload,
+  createOrUpdate,
+  $delete,
+  get,
+  list,
+  listVersions,
+} from "../../api/datasets/operations.js";
+import { PagedAsyncIterableIterator } from "../../static-helpers/pagingHelpers.js";
+
+/** Interface representing a Datasets operations. */
+export interface DatasetsOperations {
+  /** Get the SAS credential to access the storage account associated with a Dataset version. */
+  getCredentials: (
+    name: string,
+    body: Record<string, any>,
+    version: string,
+    options?: DatasetsGetCredentialsOptionalParams,
+  ) => Promise<AssetCredentialResponse>;
+  /** Start a new or get an existing pending upload of a dataset for a specific version. */
+  pendingUpload: (
+    name: string,
+    body: PendingUploadRequest,
+    version: string,
+    options?: DatasetsPendingUploadOptionalParams,
+  ) => Promise<PendingUploadResponse>;
+  /** Create a new or update an existing DatasetVersion with the given version id */
+  createOrUpdate: (
+    name: string,
+    body: DatasetVersionUnion,
+    version: string,
+    options?: DatasetsCreateOrUpdateOptionalParams,
+  ) => Promise<DatasetVersionUnion>;
+  /** Delete the specific version of the DatasetVersion */
+  /**
+   *  @fixme delete is a reserved word that cannot be used as an operation name.
+   *         Please add @clientName("clientName") or @clientName("<JS-Specific-Name>", "javascript")
+   *         to the operation to override the generated name.
+   */
+  delete: (
+    name: string,
+    version: string,
+    options?: DatasetsDeleteOptionalParams,
+  ) => Promise<void>;
+  /** Get the specific version of the DatasetVersion */
+  get: (
+    name: string,
+    version: string,
+    options?: DatasetsGetOptionalParams,
+  ) => Promise<DatasetVersionUnion>;
+  /** List the latest version of each DatasetVersion */
+  list: (
+    options?: DatasetsListOptionalParams,
+  ) => PagedAsyncIterableIterator<DatasetVersionUnion>;
+  /** List all versions of the given DatasetVersion */
+  listVersions: (
+    name: string,
+    options?: DatasetsListVersionsOptionalParams,
+  ) => PagedAsyncIterableIterator<DatasetVersionUnion>;
+}
+
+function _getDatasets(context: AIProjectContext) {
+  return {
+    getCredentials: (
+      name: string,
+      body: Record<string, any>,
+      version: string,
+      options?: DatasetsGetCredentialsOptionalParams,
+    ) => getCredentials(context, name, body, version, options),
+    pendingUpload: (
+      name: string,
+      body: PendingUploadRequest,
+      version: string,
+      options?: DatasetsPendingUploadOptionalParams,
+    ) => pendingUpload(context, name, body, version, options),
+    createOrUpdate: (
+      name: string,
+      body: DatasetVersionUnion,
+      version: string,
+      options?: DatasetsCreateOrUpdateOptionalParams,
+    ) => createOrUpdate(context, name, body, version, options),
+    delete: (
+      name: string,
+      version: string,
+      options?: DatasetsDeleteOptionalParams,
+    ) => $delete(context, name, version, options),
+    get: (name: string, version: string, options?: DatasetsGetOptionalParams) =>
+      get(context, name, version, options),
+    list: (options?: DatasetsListOptionalParams) => list(context, options),
+    listVersions: (
+      name: string,
+      options?: DatasetsListVersionsOptionalParams,
+    ) => listVersions(context, name, options),
+  };
+}
+
+export function _getDatasetsOperations(
+  context: AIProjectContext,
+): DatasetsOperations {
+  return {
+    ..._getDatasets(context),
+  };
+}
