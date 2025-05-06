52914cdedbaae71329f77deda42510b99e6a6830
SDKAuto <sdkautomation@microsoft.com>
2025-05-06 18:56:58 +0000

    CodeGen from PR 34402 in Azure/azure-rest-api-specs Merge 54af2f1008357beea13b36cb6cc1b77ef8d057d7 into 71b435a5443536ac202440b417e66433468dcaba

---
+++ sdk/informatica/arm-informaticadatamanagement/src/classic/index.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export { OperationsOperations } from "./operations/index.js";
+export { OrganizationsOperations } from "./organizations/index.js";
+export { ServerlessRuntimesOperations } from "./serverlessRuntimes/index.js";
