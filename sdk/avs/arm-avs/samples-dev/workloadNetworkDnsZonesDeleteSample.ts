177871c524106b2610eed93c2d7be292b3bcae6f
SDKAuto <sdkautomation@microsoft.com>
2025-05-06 17:46:10 +0000

    CodeGen from PR 34469 in Azure/azure-rest-api-specs Merge 204e8ffea4887d2145464c08667a929c8b42078a into 7608c3db81c82c6fa9cf449adc77f36db9b9a26c

---
+++ sdk/avs/arm-avs/samples-dev/workloadNetworkDnsZonesDeleteSample.ts
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { AzureVMwareSolutionAPIClient } from "@azure/arm-avs";
+import { DefaultAzureCredential } from "@azure/identity";
+
+/**
+ * This sample demonstrates how to delete a WorkloadNetworkDnsZone
+ *
+ * @summary delete a WorkloadNetworkDnsZone
+ * x-ms-original-file: 2023-09-01/WorkloadNetworks_DeleteDnsZone.json
+ */
+async function workloadNetworksDeleteDnsZone(): Promise<void> {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new AzureVMwareSolutionAPIClient(credential, subscriptionId);
+  await client.workloadNetworkDnsZones.delete("group1", "dnsZone1", "cloud1");
+}
+
+async function main(): Promise<void> {
+  await workloadNetworksDeleteDnsZone();
+}
+
+main().catch(console.error);
---
+++ sdk/avs/arm-avs/samples-dev/workloadNetworkPortMirroringProfilesCreateSample.ts
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { AzureVMwareSolutionAPIClient } from "@azure/arm-avs";
+import { DefaultAzureCredential } from "@azure/identity";
+
+/**
+ * This sample demonstrates how to create a WorkloadNetworkPortMirroring
+ *
+ * @summary create a WorkloadNetworkPortMirroring
+ * x-ms-original-file: 2023-09-01/WorkloadNetworks_CreatePortMirroring.json
+ */
+async function workloadNetworksCreatePortMirroring(): Promise<void> {
+  const credential = new DefaultAzureCredential();
+  const subscriptionId = "00000000-0000-0000-0000-000000000000";
+  const client = new AzureVMwareSolutionAPIClient(credential, subscriptionId);
+  const result = await client.workloadNetworkPortMirroringProfiles.create(
+    "group1",
+    "cloud1",
+    "portMirroring1",
+  );
+  console.log(result);
+}
+
+async function main(): Promise<void> {
+  await workloadNetworksCreatePortMirroring();
+}
+
+main().catch(console.error);
