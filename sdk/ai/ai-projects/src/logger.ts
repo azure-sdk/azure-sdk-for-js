65325d6023121b9af91f6344177ad58af3186fbd
SDKAuto <sdkautomation@microsoft.com>
2025-05-07 03:48:33 +0000

    CodeGen from PR 34486 in Azure/azure-rest-api-specs Merge 46ce90da0e24d277b7c1cb984ee77116138233e4 into 04f19c9287ab3f1fd78d03848caef6149a066738

--- sdk/ai/ai-projects/src/logger.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-import { createClientLogger } from "@azure/logger";
-import { PACKAGE_NAME } from "./constants.js";
-export const logger = createClientLogger(PACKAGE_NAME);
