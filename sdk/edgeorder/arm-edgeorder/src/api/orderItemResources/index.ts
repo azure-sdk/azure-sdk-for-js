de01d80b5e2c6e929c6f8f3ac9ff117d524df0c4
SDKAuto <sdkautomation@microsoft.com>
2025-05-10 02:11:07 +0000

    CodeGen from PR 34238 in Azure/azure-rest-api-specs Merge ae78f7f18efaecb7e30d206c3a2bcf6495e005d2 into 4e8d16d3793228046ac6171eadda4b8d26ad2b4f

---
+++ sdk/edgeorder/arm-edgeorder/src/api/orderItemResources/index.ts
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export {
+  returnOrderItem,
+  cancel,
+  listBySubscription,
+  listByResourceGroup,
+  $delete,
+  update,
+  create,
+  get,
+} from "./operations.js";
+export {
+  OrderItemResourcesReturnOrderItemOptionalParams,
+  OrderItemResourcesCancelOptionalParams,
+  OrderItemResourcesListBySubscriptionOptionalParams,
+  OrderItemResourcesListByResourceGroupOptionalParams,
+  OrderItemResourcesDeleteOptionalParams,
+  OrderItemResourcesUpdateOptionalParams,
+  OrderItemResourcesCreateOptionalParams,
+  OrderItemResourcesGetOptionalParams,
+} from "./options.js";
