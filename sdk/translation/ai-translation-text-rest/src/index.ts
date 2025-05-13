8c37a4d8d374f0f96034c05e748779776dd69892
SDKAuto <sdkautomation@microsoft.com>
2025-05-13 07:00:04 +0000

    CodeGen from PR 33410 in Azure/azure-rest-api-specs Merge 1b94ded8fbc174386df38e49fdeacaa4a057677a into ba0c086df0ebe03a61579485c1c10de0d17804b2

--- sdk/translation/ai-translation-text-rest/src/index.ts
+++ sdk/translation/ai-translation-text-rest/src/index.ts
@@ -1,17 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import TextTranslationClient from "./customClient.js";
+import TextTranslationClient from "./textTranslationClient.js";
 
-export * from "./customClient.js";
+export * from "./textTranslationClient.js";
 export * from "./parameters.js";
 export * from "./responses.js";
 export * from "./clientDefinitions.js";
 export * from "./isUnexpected.js";
 export * from "./models.js";
 export * from "./outputModels.js";
-export * from "./serializeHelper.js";
-export { TranslatorCredential, TranslatorTokenCredential } from "./authenticationCustomized.js";
 
-// eslint-disable-next-line @azure/azure-sdk/ts-modules-only-named
 export default TextTranslationClient;
