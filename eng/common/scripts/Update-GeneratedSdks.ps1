58f0e244b30135d28205144139f6be4d4eafa891
Kyle Zhang <v-zhanh@microsoft.com>
2025-05-20 14:42:37 +0800

    test

--- eng/common/scripts/Update-GeneratedSdks.ps1
+++ eng/common/scripts/Update-GeneratedSdks.ps1
@@ -7,7 +7,7 @@ param(
 . $PSScriptRoot/common.ps1
 . $PSScriptRoot/Helpers/CommandInvocation-Helpers.ps1
 
-$ErrorActionPreference = 'Stop'
+#$ErrorActionPreference = 'Stop'
 
 if (Test-Path "Function:$UpdateGeneratedSdksFn") {
     &$UpdateGeneratedSdksFn $PackageDirectoriesFile
