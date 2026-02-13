05a21ca82341bcf3f46fcf39dc5f4743ca50519d
kazrael2119 <98569699+kazrael2119@users.noreply.github.com>
2026-02-13 16:24:27 +0800

    format

--- sdk/disconnectedoperations/arm-disconnectedoperations/src/api/disconnectedOperations/options.ts
+++ sdk/disconnectedoperations/arm-disconnectedoperations/src/api/disconnectedOperations/options.ts
@@ -4,8 +4,7 @@
 import type { OperationOptions } from "@azure-rest/core-client";
 
 /** Optional parameters. */
-export interface DisconnectedOperationsListDeploymentManifestOptionalParams
-  extends OperationOptions {}
+export interface DisconnectedOperationsListDeploymentManifestOptionalParams extends OperationOptions {}
 
 /** Optional parameters. */
 export interface DisconnectedOperationsListBySubscriptionOptionalParams extends OperationOptions {}
